b9bcda6fe6ae42afae5b20ccf5c3963d939aef28
Tom Smyth <tom@sassafras.coop>
2015-07-23 22:32:06 -0400

    2702: Added user abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,5 +2,17 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    return if user.nil?
+
+    # Anyone can read any other user's info.
+    can :read, User
+
+    # Only user can edit own profile.
+    can :edit_profile, User, id: user.id
+
+    if user.admin?
+      # Admins can edit any user
+      can :manage, User
+    end
   end
 end

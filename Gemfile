ebfbcf035ff241dbb72fd8d8c320378d97591026
Tom Smyth <tom@sassafras.coop>
2015-07-25 22:35:44 -0400

    2702: Added user validation

--- Gemfile
+++ Gemfile
@@ -41,6 +41,9 @@ gem 'cancancan', '~> 1.10'
 
 gem 'bootstrap-sass', '~> 3.3.5'
 
+gem 'attribute_normalizer'
+gem 'phony_rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,20 @@ class User < ActiveRecord::Base
 
   belongs_to :household
 
+  %w(home_phone work_phone mobile_phone).each do |p|
+    phony_normalize p, default_country_code: 'US'
+    validates_plausible_phone p, normalized_country_code: 'US'
+  end
+
+  normalize_attributes :email, :google_email, :first_name, :last_name
+
+  validates :email, format: Devise.email_regexp, presence: true, uniqueness: true
+  validates :google_email, format: Devise.email_regexp, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :household, presence: true
+  validate :at_least_one_phone
+
   def self.from_omniauth(auth)
     # Find user
     if user = where(google_email: auth.info[:email]).first
@@ -16,5 +30,15 @@ class User < ActiveRecord::Base
 
   def name
     "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def at_least_one_phone
+    errors.add(:base, "You must enter at least one phone number") if no_phones?
+  end
+
+  def no_phones?
+    [home_phone, mobile_phone, work_phone].compact.empty?
   end
 end

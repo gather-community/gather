108c255df5124cd41a17f4eb80df691cb39113eb
Tom Smyth <tom@sassafras.coop>
2015-08-12 23:30:30 -0400

    2840: Fixed migration

--- db/migrate/20150811025818_add_location_id_to_meals.rb
+++ db/migrate/20150811025818_add_location_id_to_meals.rb
@@ -2,7 +2,7 @@ class AddLocationIdToMeals < ActiveRecord::Migration
   def change
     add_reference :meals, :location, index: true, foreign_key: true
     loc = Location.first
-    raise "No locations available" if loc.nil?
+    raise "No locations available" if loc.nil? && Meal.count > 0
     Meal.all.each{ |m| m.update_attribute(:location_id, loc.id) }
     change_column_null :meals, :location_id, true
   end

225a1bb8aef6557eea83028d9c71d285df222397
Tom Smyth <tom@sassafras.coop>
2015-08-24 23:44:23 -0400

    Removed success message

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Google")
     else
       set_flash_message(:error, :failure, kind: "Google", reason: "UID not found. Please contact meal biller.")
       redirect_to root_path

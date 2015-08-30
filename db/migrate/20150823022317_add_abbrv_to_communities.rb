94db98f0f136c3b681ebc5642226f58e80403611
Tom Smyth <tom@sassafras.coop>
2015-08-29 21:41:44 -0400

    Tweaked yet another migration

--- db/migrate/20150823022317_add_abbrv_to_communities.rb
+++ db/migrate/20150823022317_add_abbrv_to_communities.rb
@@ -1,9 +1,11 @@
 class AddAbbrvToCommunities < ActiveRecord::Migration
   def change
-    add_column :communities, :abbrv, :string
-    Community.where(name: 'Touchstone').first.update_attribute(:abbrv, 'TS')
-    Community.where(name: 'Great Oak').first.update_attribute(:abbrv, 'GO')
-    Community.where(name: 'Sunward').first.update_attribute(:abbrv, 'SW')
-    change_column_null :communities, :abbrv, false
+    unless column_exists?(:communities, :abbrv)
+      add_column :communities, :abbrv, :string
+      Community.where(name: 'Touchstone').first.update_attribute(:abbrv, 'TS')
+      Community.where(name: 'Great Oak').first.update_attribute(:abbrv, 'GO')
+      Community.where(name: 'Sunward').first.update_attribute(:abbrv, 'SW')
+      change_column_null :communities, :abbrv, false
+    end
   end
 end

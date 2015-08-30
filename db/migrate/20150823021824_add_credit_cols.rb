0e63bea92cb166ffac5bdd929c1fa8fdcc7bd0c6
Tom Smyth <tom@sassafras.coop>
2015-08-29 21:39:08 -0400

    Tweaked migration

--- db/migrate/20150823021824_add_credit_cols.rb
+++ db/migrate/20150823021824_add_credit_cols.rb
@@ -1,6 +1,8 @@
 class AddCreditCols < ActiveRecord::Migration
   def change
-    add_column :households, :credit_limit, :integer, null: false, default: 50
+    unless column_exists?(:households, :credit_limit)
+      add_column :households, :credit_limit, :integer, null: false, default: 50
+    end
     add_column :households, :over_limit, :boolean, null: false, default: false
   end
 end

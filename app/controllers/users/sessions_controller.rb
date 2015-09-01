78459691b954695a762c36bb15ef6cca49fd0f1f
Tom Smyth <tom@sassafras.coop>
2015-08-31 23:00:00 -0400

    Fixed bug with log in

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,5 @@
+class Users::SessionsController < Devise::SessionsController
+  def new
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks",
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    sessions: "users/sessions"
   }
 
   resources :users do

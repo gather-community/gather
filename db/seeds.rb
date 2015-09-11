ade1b9a8a90d108dd807fc60ea49840780766650
Tom Smyth <tom@sassafras.coop>
2015-09-10 20:39:30 -0400

    3022: Seeded formulas

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,51 @@ if Household.all.empty?
   h6 = Household.create!(name: "Brown/Walsh", unit_num: 502, community: go)
 end
 
-Location.find_or_create_by!(name: "Touchstone Common House", abbrv: "TS CH")
-Location.find_or_create_by!(name: "Touchstone Courtyard", abbrv: "TS Courtyard")
-Location.find_or_create_by!(name: "Great Oak Common House", abbrv: "GO CH")
-Location.find_or_create_by!(name: "Sunward Common House", abbrv: "SW CH")
+Location.find_or_create_by!(name: "Touchstone Common House") do |loc|
+  loc.abbrv = "TS CH"
+end
+Location.find_or_create_by!(name: "Touchstone Courtyard") do |loc|
+  loc.abbrv = "TS Courtyard"
+end
+Location.find_or_create_by!(name: "Great Oak Common House") do |loc|
+  loc.abbrv = "GO CH"
+end
+Location.find_or_create_by!(name: "Sunward Common House") do |loc|
+  loc.abbrv = "SW CH"
+end
+
+Formula.find_or_create_by(community_id: ts.id) do |f|
+  f.effective_on = Date.civil(2015,1,1)
+  f.senior_meat = 4
+  f.adult_meat = 6
+  f.teen_meat = 4
+  f.big_kid_meat = 3
+  f.little_kid_meat = 0
+  f.senior_veg = 4
+  f.adult_veg = 6
+  f.teen_veg = 4
+  f.big_kid_veg = 3
+  f.little_kid_veg = 0
+  f.pantry_fee = 0.5
+  f.meal_calc_type = "fixed"
+  f.pantry_calc_type = "fixed"
+end
+
+[go, sw].each do |community|
+  Formula.find_or_create_by(community_id: community.id) do |f|
+    f.effective_on = Date.civil(2015,1,1)
+    f.senior_meat = nil
+    f.adult_meat = 1
+    f.teen_meat = 0.75
+    f.big_kid_meat = 0.5
+    f.little_kid_meat = 0
+    f.senior_veg = nil
+    f.adult_veg = 1
+    f.teen_veg = 0.75
+    f.big_kid_veg = 0.5
+    f.little_kid_veg = 0
+    f.pantry_fee = 0.1
+    f.meal_calc_type = "share"
+    f.pantry_calc_type = "share"
+  end
+end

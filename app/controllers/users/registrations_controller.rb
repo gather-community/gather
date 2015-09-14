5d9418930371506c8d95f70e0a8cca65b7f205cb
Tom Smyth <tom@sassafras.coop>
2015-09-13 22:51:41 -0400

    3099: Removed unused devise override

--- app/controllers/users/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-  # Override destroy to allow for soft delete.
-  def destroy
-    resource.deactivate!
-    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-    set_flash_message :notice, :destroyed if is_navigational_format?
-    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
-  end
-end

31d0ab6a4b2e18575c454362500208191c315e67
Tom Smyth <tom@sassafras.coop>
2015-09-15 14:18:11 -0400

    3116: Fixed oauth bug

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,27 +3,36 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     auth = request.env["omniauth.auth"]
 
     # If invite token is present, try to find user by that.
-    if (t = session[:invite_token]) && @user = User.with_reset_password_token(t)
+    if (t = session[:invite_token]) && by_token = User.with_reset_password_token(t)
 
-      if !@user.google_email.nil? && @user.google_email != auth.info[:email]
+      # If we find them but they are logging in with the wrong google_email, notify them.
+      if !by_token.google_email.nil? && by_token.google_email != auth.info[:email]
         set_flash_message(:error, :failure, kind: "Google",
-          reason: "you must login with the Google ID #{@user.google_email}")
+          reason: "you must login with the Google ID #{by_token.google_email}")
         redirect_to root_path
-      elsif @user.reset_password_period_valid?
-        @user.update_for_oauth!(auth)
-        @user.send(:clear_reset_password_token)
-        sign_in_and_redirect @user, event: :authentication
+
+      # If there is already a user with that google_email, log them in
+      # INSTEAD of the one with the token. Shouldn't happen often.
+      elsif by_email = User.from_omniauth(auth)
+        login_and_clear_token(by_email, auth)
+
+      # Else if the token is still valid, log them in and grab their google_email
+      elsif by_token.reset_password_period_valid?
+        login_and_clear_token(by_token, auth)
+
       else
         set_flash_message(:error, :failure, kind: "Google",
           reason: "your invitation has expired")
         redirect_to root_path
       end
+
       session[:invite_token] = nil
 
-    elsif @user = User.from_omniauth(auth)
+    # if no invite, try to find by google_email
+    elsif by_email = User.from_omniauth(auth)
 
-      @user.update_for_oauth!(auth)
-      sign_in_and_redirect @user, event: :authentication
+      by_email.update_for_oauth!(auth)
+      sign_in_and_redirect by_email, event: :authentication
 
     else
 
@@ -32,5 +41,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to root_path
 
     end
+  end
+
+  private
+
+  def login_and_clear_token(user, auth)
+    user.update_for_oauth!(auth)
+    user.send(:clear_reset_password_token)
+    sign_in_and_redirect user, event: :authentication
   end
 end

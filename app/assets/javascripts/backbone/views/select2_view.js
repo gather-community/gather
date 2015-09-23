3b61d4ff95e847edfb5871f4a4bb63e6cb4968bc
Tom Smyth <tom@sassafras.coop>
2015-09-22 21:41:57 -0400

    3080: Change household select2 to full_name

--- app/assets/javascripts/backbone/views/select2_view.js
+++ app/assets/javascripts/backbone/views/select2_view.js
@@ -15,6 +15,8 @@ Mess.Views.Select2View = Backbone.View.extend({
 
   setup_select2: function(el) {
     var src = el.data("select2-src");
+    var label_attr = el.data("select2-label-attr") || "name";
+    var self = this;
 
     el.select2({
       ajax: {
@@ -30,7 +32,7 @@ Mess.Views.Select2View = Backbone.View.extend({
         processResults: function (data, page) {
           console.log(data);
           return {
-            results: data[src + "s"].map(function(u){ return {id: u.id, text: u.name}; }),
+            results: data[src + "s"].map(function(u){ return {id: u.id, text: u[label_attr]}; }),
             pagination: { more: data.meta.more }
           }
         },
---
+++ app/serializers/household_serializer.rb
@@ -0,0 +1,3 @@
+class HouseholdSerializer < ActiveModel::Serializer
+  attributes :id, :full_name
+end

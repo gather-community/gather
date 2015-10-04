e002af68a1e975fdbd38913fe955a424498ec9e5
Tom Smyth <tom@sassafras.coop>
2015-10-04 01:58:27 -0400

    Added whenever env vars

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,6 @@
 set :output, "#{path}/log/cron_log.log"
+env :PATH, ENV['PATH']
+env :GEM_HOME, ENV['GEM_HOME']
 
 job_type :enqueue,  "cd :path && RAILS_ENV=:environment bundle exec djc-enqueue :task :output"
 

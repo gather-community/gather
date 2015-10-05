bcc0ed17f4ff5c1f13226fe3246751e0b01b6082
Tom Smyth <tom@sassafras.coop>
2015-10-04 21:19:33 -0400

    Re-enabled cron

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ gem 'inline_svg'
 gem 'delayed_job_active_record'
 gem "daemons"
 gem 'exception_notification'
+gem "whenever"
 gem 'dj_cron', github: 'sassafrastech/dj_cron'
 
 group :development, :test do
--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,6 @@ env :GEM_HOME, ENV['GEM_HOME']
 
 job_type :enqueue,  "cd :path && RAILS_ENV=:environment bundle exec djc-enqueue :task :output"
 
-# every 1.minute do
-#   enqueue "MealReminderJob"
-# end
+every 1.minute do
+  enqueue "MealReminderJob"
+end

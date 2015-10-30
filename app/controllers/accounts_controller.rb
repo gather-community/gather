2df62e5d09c9325349e75871ca50d96412004279
Tom Smyth <tom@sassafras.coop>
2015-10-29 21:32:09 -0400

    2875: Accounts index now works with new account model

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,7 +2,9 @@ class AccountsController < ApplicationController
   authorize_resource
 
   def index
-    households = Household.in_community(current_user.community).by_active_and_name
-    @accounts = Account.for_households(households)
+    @households = Household.joins(:account).includes(:account).in_community(current_user.community).
+      where("deactivated_at IS NULL OR
+        COALESCE(accounts.due_last_invoice,0) - total_new_credits + total_new_charges > 0.01").
+      by_name.page(params[:page]).per(20)
   end
 end

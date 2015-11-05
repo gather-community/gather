5f773fb24ee0c88de5a25772346d0d7338ae6d9e
Tom Smyth <tom@sassafras.coop>
2015-11-04 21:29:33 -0500

    2875: Added LateFeeApplier

---
+++ spec/models/late_fee_applier_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe LateFeeApplier, type: :model do
+  describe "apply" do
+    let(:community){ create(:community, settings: { "late_fee_policy" => policy }) }
+    let(:accounts) do
+      [-5, 0, 5, 10, 15].map do |b|
+        create(:account, community: community, due_last_statement: b, total_new_credits: 0)
+      end
+    end
+
+    context "threshold with fixed fee" do
+      let(:policy){ { "threshold" => 5, "fee_type" => "fixed", "fee_amount" => 3 } }
+
+      it "should apply appropriate fees" do
+        expect_fees(0, 0, 0, 3, 3)
+      end
+    end
+
+    context "percent fee" do
+      let(:policy){ { "fee_type" => "percent", "fee_pct" => 3 } }
+
+      it "should apply appropriate fees" do
+        expect_fees(0, 0, 0.15, 0.30, 0.45)
+      end
+    end
+
+    context "no policy" do
+      let(:policy){ nil }
+
+      it "should apply no fees" do
+        expect_fees(0, 0, 0, 0, 0)
+      end
+    end
+
+    def expect_fees(*amounts)
+      accounts
+      LateFeeApplier.new(community).apply!
+      amounts.each_with_index do |amt, i|
+        if amt == 0
+          expect(accounts[i].line_items).to be_empty
+        else
+          expect(accounts[i].line_items.last.incurred_on).to eq Date.today
+          expect(accounts[i].line_items.last.code).to eq "late_fee"
+          expect(accounts[i].line_items.last.amount).to eq amt
+        end
+      end
+    end
+  end
+end

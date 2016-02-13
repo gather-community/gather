50edc0cbf3ef15e5743968b7534f61d316cf75cc
Tom Smyth <tom@sassafras.coop>
2016-02-13 16:00:45 -0500

    3610: Added household policy

---
+++ app/policies/household_policy.rb
@@ -0,0 +1,51 @@
+class HouseholdPolicy < ApplicationPolicy
+  alias_method :household, :record
+
+  class Scope < Scope
+    def resolve
+      user.admin? ? scope : scope.none
+    end
+  end
+
+  def index?
+    admin? || biller?
+  end
+
+  def show?
+    admin? || biller?
+  end
+
+  def create?
+    admin?
+  end
+
+  def update?
+    admin?
+  end
+
+  def accounts?
+    admin? || biller? || household == user.household
+  end
+
+  def destroy?
+    admin? && !record.any_users? && !record.any_assignments? && !record.any_signups? && !record.any_accounts?
+  end
+
+  def permitted_attributes
+    [:name, :community_id, :unit_num, :old_id, :old_name]
+  end
+
+  private
+
+  def self?
+    record == user
+  end
+
+  def admin?
+    user.admin?
+  end
+
+  def biller?
+    user.biller?
+  end
+end
---
+++ spec/policies/household_policy_spec.rb
@@ -0,0 +1,106 @@
+require 'rails_helper'
+
+describe HouseholdPolicy do
+  describe "permissions" do
+    include_context "policy objs"
+
+    shared_examples_for "admins but not regular users" do
+      it "grants access to admins" do
+        expect(subject).to permit(admin, household)
+      end
+
+      it "denies access to regular users" do
+        expect(subject).not_to permit(user, household)
+      end
+    end
+
+    shared_examples_for "admins only" do
+      it_behaves_like "admins but not regular users"
+
+      it "denies access to billers" do
+        expect(subject).not_to permit(biller, household)
+      end
+    end
+
+    permissions :index?, :show? do
+      it_behaves_like "admins but not regular users"
+
+      it "grants access to billers" do
+        expect(subject).to permit(biller, household)
+      end
+    end
+
+    permissions :new?, :create?, :edit?, :update? do
+      it_behaves_like "admins only"
+    end
+
+    permissions :accounts? do
+      it "grants access for regular users to own household" do
+        expect(subject).to permit(user, user.household)
+      end
+
+      it "denies access for regular users to other households" do
+        expect(subject).not_to permit(user, Household.new)
+      end
+    end
+
+    permissions :destroy? do
+      shared_examples_for "full denial" do
+        it "denies access to admins" do
+          expect(subject).not_to permit(admin, household)
+        end
+
+        it "denies access to billers" do
+          expect(subject).not_to permit(biller, household)
+        end
+      end
+
+      context "with user" do
+        it_behaves_like "full denial"
+      end
+
+      context "with assignment" do
+        before { household.users.first.assignments.build }
+        it_behaves_like "full denial"
+      end
+
+      context "with signup" do
+        before { household.signups.build }
+        it_behaves_like "full denial"
+      end
+
+      context "with account" do
+        before { household.accounts.build }
+        it_behaves_like "full denial"
+      end
+
+      context "without any of the above" do
+        before { household.users = [] }
+        it_behaves_like "admins only"
+      end
+    end
+  end
+
+  describe "scope" do
+    let!(:admin) { create(:user, admin: true) }
+    let!(:household2) { create(:household) }
+
+    it "returns all households for admins" do
+      permitted = HouseholdPolicy::Scope.new(admin, Household.all).resolve
+      expect(permitted).to contain_exactly(admin.household, household2)
+    end
+
+    it "returns no households for regular users" do
+      permitted = HouseholdPolicy::Scope.new(User.new, Household.all).resolve
+      expect(permitted).to eq([])
+    end
+  end
+
+  describe "permitted attributes" do
+    subject { HouseholdPolicy.new(User.new, Household.new).permitted_attributes }
+
+    it "should allow basic attribs" do
+      expect(subject).to contain_exactly(:name, :community_id, :unit_num, :old_id, :old_name)
+    end
+  end
+end

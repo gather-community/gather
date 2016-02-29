6f4191f6c6fd9c3b88fc648fdee68aa5adca8bb8
Tom Smyth <tom@sassafras.coop>
2016-02-29 08:28:25 -0500

    Fixed authz bug with Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   # Verify that controller actions are authorized.
-  after_action :verify_authorized,  except: :index
+  after_action :verify_authorized,  except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index
 
   def set_validation_error_notice

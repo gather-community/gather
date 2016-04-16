25d933e8c8b744a07d7eba2680baa056a2b29801
Tom Smyth <tom@sassafras.coop>
2016-04-16 15:50:19 -0400

    4242: Adjusted styling of base error

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -2,6 +2,8 @@ module FormHelper
   def base_error(f)
     return unless f.object.errors[:base].any?
     # Mimics the way it works for fields
-    content_tag(:div, f.error(:base), class: "has-error")
+    content_tag(:div, class: "row") do
+      content_tag(:div, f.error(:base), class: "has-error base-error col-sm-6 col-sm-offset-2")
+    end
   end
 end

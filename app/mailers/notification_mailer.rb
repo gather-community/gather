de0a6cd3b4905d24eb1c94c2cf9b70ff19c7c972
Tom Smyth <tom@sassafras.coop>
2016-04-23 09:22:34 -0400

    4186: Added cook menu reminder

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -36,4 +36,14 @@ class NotificationMailer < ActionMailer::Base
 
     mail(to: recips.compact.uniq, subject: "Meal Work Assignment Change Notice")
   end
+
+  def cook_menu_reminder(assignment)
+    @assignment = assignment
+    @user = assignment.user
+    @meal = assignment.meal
+    @type = assignment.reminder_count == 0 ? :first : :second
+
+    mail(to: @user.email, subject:
+      "Menu Reminder: Please post menu for #{@meal.served_at.to_s(:short_date)}")
+  end
 end

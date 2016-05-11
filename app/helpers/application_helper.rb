baedb7054136d0e2ae7149a984bb8c62ba601602
Tom Smyth <tom@sassafras.coop>
2016-05-11 09:40:56 -0400

    Revert "Temporarily removed reservation link"
    This reverts commit 211172a9b632ed0fe85ad7917281fb7742f9b3c1.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,7 +26,7 @@ module ApplicationHelper
   end
 
   def nav_links
-    [Meal, [Meal, "work"], User, Household, Account].map do |item|
+    [Meal, [Meal, "work"], Reservation::Reservation, User, Household, Account].map do |item|
       controller = item.is_a?(Class) ? item.table_name : item[0].to_s.tableize
       action = item.is_a?(Class) ? "index" : item[1]
       route_key = item.is_a?(Class) ? controller : "#{action}_#{controller}"

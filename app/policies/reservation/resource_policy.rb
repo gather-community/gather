90800a4b7583932ffb81e39a5e7db5a4285ce75f
Tom Smyth <tom@sassafras.coop>
2016-08-15 21:18:27 -0400

    Changed visibility of resources for admins

--- app/policies/reservation/resource_policy.rb
+++ app/policies/reservation/resource_policy.rb
@@ -4,9 +4,12 @@ module Reservation
 
     class Scope < Scope
       def resolve
+        # Only show visible resources unless admin.
+        scope_with_visibility = user.admin? ? scope : scope.visible
+
         # Need to load the resources because access_level is computed by RuleSet which can't be computed
         # at database level.
-        ids = scope.visible.select do |resource|
+        ids = scope_with_visibility.select do |resource|
           sample_reservation = Reservation.new(reserver: user, resource: resource)
           sample_reservation.access_level != "forbidden"
         end.map(&:id)

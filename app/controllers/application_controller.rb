5f07368b6d42d905ff6e4afe76685ff344bf3c61
Tom Smyth <tom@sassafras.coop>
2016-09-08 09:25:54 -0400

    5078: Fixed redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :store_current_location
   before_action :authenticate_user!
 
   # Verify that controller actions are authorized.
@@ -25,10 +26,6 @@ class ApplicationController < ActionController::Base
     current_user.try(:inactive?) ? inactive_path : root_path
   end
 
-  def after_sign_in_path_for(resource)
-    home_path
-  end
-
   def set_no_cache
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
@@ -41,5 +38,17 @@ class ApplicationController < ActionController::Base
 
   def default_serializer_options
     { root: false }
+  end
+
+  private
+
+  # Saves the location before loading each page so we can return to the right page after sign in.
+  def store_current_location
+    # If we're on a devise page, we don't want to store that as the
+    # place to return to (for example, we don't want to return to the sign in page
+    # after signing in).
+    # Also since the root path is shown if authentication fails, we don't want to store that either.
+    return if devise_controller? || request.fullpath == '/'
+    store_location_for(:user, request.url)
   end
 end

d6f902d5819b1273a7718b2d1866cf90a218fe7a
Tom Smyth <tom@sassafras.coop>
2016-11-11 08:30:38 -0500

    3606: Refactored policy spec objs

--- spec/support/policy_spec_context.rb
+++ spec/support/policy_spec_context.rb
@@ -1,33 +1,34 @@
 shared_context "policy objs" do
   subject { described_class }
-  let(:cluster) { Cluster.new }
-  let(:community) { Community.new }
-  let(:user) { User.new }
-  let(:other_user) { User.new }
-  let(:inactive_user) { User.new(deactivated_at: Time.now) }
+  let(:cluster) { Cluster.new(name: "Main Cluster") }
+  let(:community) { Community.new(name: "Community A", cluster: cluster) }
+  let(:communityB) { Community.new(name: "Community B", cluster: cluster) }
+  let(:user) { new_user_from(community) }
+  let(:other_user) { new_user_from(community) }
+  let(:inactive_user) { new_user_from(community, deactivated_at: Time.now) }
   let(:household) { Household.new(users: [user], community: community) }
-  let(:admin) { User.new(household: Household.new(community: community)) }
-  let(:cluster_admin) { User.new(household: Household.new(community: community)) }
-  let(:super_admin) { User.new(household: Household.new(community: community)) }
-  let(:biller) { User.new(household: Household.new(community: community)) }
-  let(:account) { Billing::Account.new }
+  let(:account) { Billing::Account.new(household: Household.new(community: community)) }
 
-  let(:outside_admin) { User.new(household: Household.new) }
-  let(:outside_biller) { User.new(household: Household.new) }
+  let(:admin) { new_user_from(community) }
+  let(:cluster_admin) { new_user_from(community) }
+  let(:super_admin) { new_user_from(community) }
+  let(:outside_admin) { new_user_from(communityB) }
+
+  let(:biller) { new_user_from(community) }
+  let(:outside_biller) { new_user_from(communityB) }
 
   before do
-    allow(community).to receive(:cluster).and_return(cluster)
-    allow(user).to receive(:community).and_return(community)
-    allow(inactive_user).to receive(:community).and_return(community)
-    allow(admin).to receive(:community).and_return(community)
+    allow(user).to receive(:has_role?) { false }
+    allow(other_user).to receive(:has_role?) { false }
     allow(admin).to receive(:has_role?) { |r| r == :admin }
-    allow(cluster_admin).to receive(:community).and_return(community)
     allow(cluster_admin).to receive(:has_role?) { |r| r == :cluster_admin }
     allow(super_admin).to receive(:has_role?) { |r| r == :super_admin }
-    allow(biller).to receive(:community).and_return(community)
     allow(biller).to receive(:has_role?) { |r| r == :biller }
-    allow(account).to receive(:community).and_return(community)
     allow(outside_admin).to receive(:has_role?) { |r| r == :admin }
     allow(outside_biller).to receive(:has_role?) { |r| r == :biller }
+  end
+
+  def new_user_from(community, attribs = {})
+    User.new(attribs.merge(household: Household.new(community: community)))
   end
 end

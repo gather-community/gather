513d15198447589b1bea17a4ff828e436dc51a7c
Tom Smyth <tom@sassafras.coop>
2016-11-27 21:26:14 -0500

    5372: Added delete button for deleting temporary uploads

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -3,17 +3,36 @@ class UploadsController < ApplicationController
 
   def create
     skip_authorization
-    uploadable_attrs = UPLOADABLES[params[:model].to_sym]
-    if uploadable_attrs.nil?
-      return render plain: 'Invalid model', status: 403
-    elsif !uploadable_attrs.include?(params[:attribute].to_sym)
-      return render plain: 'Invalid attribute', status: 403
-    else
-      object = params[:model].camelize.constantize.new
+    if object = build_object
       object.send("#{params[:attribute]}=", params[:file])
-      object.send("#{params[:attribute]}_tmp_id=", params[:tmp_id])
       object.send(params[:attribute]).save_tmp
       render nothing: true
+    end
+  end
+
+  def destroy
+    skip_authorization
+    params[:tmp_id] = params[:id]
+    if object = build_object
+      object.send(params[:attribute]).destroy
+      render nothing: true
+    end
+  end
+
+  private
+
+  def build_object
+    uploadable_attrs = UPLOADABLES[params[:model].to_sym]
+    if uploadable_attrs.nil?
+      render plain: 'Invalid model', status: 403
+      return nil
+    elsif !uploadable_attrs.include?(params[:attribute].to_sym)
+      render plain: 'Invalid attribute', status: 403
+      return nil
+    else
+      params[:model].camelize.constantize.new.tap do |object|
+        object.send("#{params[:attribute]}_tmp_id=", params[:tmp_id])
+      end
     end
   end
 end

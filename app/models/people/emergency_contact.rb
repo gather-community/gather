62a6df8ee11c1e656c498bfeb515c941988cfcc8
Tom Smyth <tom@sassafras.coop>
2016-12-23 23:30:58 -0500

    5374: Added new fields to household show

--- app/models/people/emergency_contact.rb
+++ app/models/people/emergency_contact.rb
@@ -2,9 +2,12 @@ class People::EmergencyContact < ActiveRecord::Base
   include Phoneable
 
   belongs_to :household
-
   normalize_attributes :name, :email, :location, :relationship
   handle_phone_types :main, :alt
-
   validates :name, :main_phone, :location, :relationship, presence: true
+
+  def to_s
+    "#{name} (#{relationship}) - #{location}, " <<
+      [format_phone(:main), format_phone(:alt), email].compact.join(", ")
+  end
 end
--- app/models/people/vehicle.rb
+++ app/models/people/vehicle.rb
@@ -1,4 +1,8 @@
 class People::Vehicle < ActiveRecord::Base
   normalize_attributes :make, :model, :color
   validates :make, :model, :color, presence: true
+
+  def to_s
+    "#{color} #{make} #{model}"
+  end
 end

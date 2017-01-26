98d1402d1cc1f33fc349844fc6b49c4b5dfc1d1f
Tom Smyth <tom@sassafras.coop>
2017-01-25 21:51:38 -0500

    3703: Added community column to meal summary

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -26,7 +26,8 @@ class Signup < ActiveRecord::Base
   validates :comments, length: {maximum: MAX_COMMENT_LENGTH}
   validate :max_signups_per_type, :dont_exceed_spots, :nonzero_signups_if_new
 
-  delegate :full_name, :users, :adults, to: :household, prefix: true
+  delegate :name, :full_name, :users, :adults, to: :household, prefix: true
+  delegate :community_abbrv, to: :household
   delegate :communities, to: :meal
 
   def self.for(user, meal)

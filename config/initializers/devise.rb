73b8ed0e70eb77eb2e5eebef4e8d6f186d91958e
Tom Smyth <tom@sassafras.coop>
2017-01-30 10:39:23 -0500

    Fixed devise mailer sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "MESS <mess@aacoho.org>"
+  config.mailer_sender = Settings.email.from
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'DeviseMailer'

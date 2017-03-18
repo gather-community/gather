2ce0631158d01ddef36210557b01fd1e8fbd3b7f
Tom Smyth <tom@sassafras.coop>
2017-03-18 14:33:00 -0400

    5320: Moved validation specs into own file

---
+++ spec/models/custom_fields/validation_spec.rb
@@ -0,0 +1,178 @@
+require "rails_helper"
+
+describe "custom field validation" do
+  let(:spec) { CustomFields::Spec.new(spec_data) }
+  let(:instance) { CustomFields::Instance.new(
+    spec: spec,
+    instance_data: instance_data,
+    class_name: "mod",
+    attrib_name: "att"
+  ) }
+
+  describe "i18n_key" do
+    let(:spec_data) { [
+      {key: "alpha", type: "string", validation: {length: {maximum: 5}}},
+      {key: "bravo", type: "group", fields: [
+        {key: "charlie", type: "string", validation: {length: {minimum: 5}}}
+      ]}
+    ] }
+    let(:instance_data) { {"fruit" => "peach", "info" => {"complete" => true, "comment" => "hi!"}} }
+
+    it "should be correct at all levels" do
+      expect(instance.entries[0].i18n_key(:errors)).to eq "custom_fields.errors.mod.att.alpha"
+      expect(instance.entries[1].i18n_key(:errors)).to eq "custom_fields.errors.mod.att.bravo._self"
+      expect(instance.entries[1].entries[0].i18n_key(:errors)).to eq(
+        "custom_fields.errors.mod.att.bravo.charlie")
+    end
+  end
+
+  describe "nestedness" do
+    let(:spec_data) { [
+      {key: "alpha", type: "string", validation: {length: {maximum: 5}}},
+      {key: "bravo", type: "group", fields: [
+        {key: "charlie", type: "string", validation: {length: {minimum: 5}}}
+      ]}
+    ] }
+
+    context "with all valid entries" do
+      let(:instance_data) { {alpha: "xxx", bravo: {charlie: "xxxxxx"}} }
+
+      it "shouldn't be invalid" do
+        expect(instance.valid?).to be true
+        expect(instance.errors[:alpha]).to eq []
+        expect(instance.errors[:bravo]).to eq []
+        expect(instance.bravo.errors[:charlie]).to eq []
+      end
+    end
+
+    context "with multiple invalid entries" do
+      let(:instance_data) { {alpha: "xxxxxx", bravo: {charlie: "xxx"}} }
+
+      it "should set error on all invalid fields" do
+        expect(instance.valid?).to be false
+        expect(instance.errors[:alpha]).to eq ["is too long (maximum is 5 characters)"]
+        expect(instance.errors[:bravo]).to eq ["is invalid"]
+        expect(instance.bravo.errors[:charlie]).to eq ["is too short (minimum is 5 characters)"]
+      end
+    end
+
+    context "with an invalid leaf entry only" do
+      let(:instance_data) { {alpha: "xxx", bravo: {charlie: "xxx"}} }
+
+      it "should set error on appropriate fields" do
+        expect(instance.valid?).to be false
+        expect(instance.errors[:alpha]).to eq []
+        expect(instance.errors[:bravo]).to eq ["is invalid"]
+        expect(instance.bravo.errors[:charlie]).to eq ["is too short (minimum is 5 characters)"]
+      end
+    end
+  end
+
+  describe "required fields" do
+    let(:spec_data) { [
+      {key: "alpha", type: "string", required: true}
+    ] }
+
+    context "with blank entry" do
+      let(:instance_data) { {alpha: ""} }
+
+      it "should set error" do
+        expect(instance.valid?).to be false
+        expect(instance.errors[:alpha]).to eq ["can't be blank"]
+      end
+    end
+
+    context "with nil entry" do
+      let(:instance_data) { {alpha: nil} }
+
+      it "should set error" do
+        expect(instance.valid?).to be false
+        expect(instance.errors[:alpha]).to eq ["can't be blank"]
+      end
+    end
+
+    context "with missing entry" do
+      let(:instance_data) { {} }
+
+      it "should set error" do
+        expect(instance.valid?).to be false
+        expect(instance.errors[:alpha]).to eq ["can't be blank"]
+      end
+    end
+  end
+
+  describe "multiple validations on one field and custom messages" do
+    let(:spec_data) { [
+      {key: "alpha", type: "string", validation: {
+        length: {minimum: 5, message: "too short!"},
+        format: {with: /\Ax/, message: "doesn't start with x!"}}
+      }
+    ] }
+    let(:instance_data) { {alpha: "hi!"} }
+
+    it "should set multiple error messages" do
+      expect(instance.valid?).to be false
+      expect(instance.errors[:alpha]).to eq ["too short!", "doesn't start with x!"]
+    end
+  end
+
+  describe "enum inclusion" do
+    let(:spec_data) { [
+      {key: "alpha", type: "enum", options: %w(blue yellow)}
+    ] }
+
+    context "with value from list" do
+      let(:instance_data) { {alpha: "blue"} }
+
+      it "should not set error" do
+        expect(instance.valid?).to be true
+      end
+    end
+
+    context "with value not from list" do
+      let(:instance_data) { {alpha: "green"} }
+
+      it "should set error" do
+        expect(instance.valid?).to be false
+        expect(instance.errors[:alpha]).to eq ["is not included in the list"]
+      end
+    end
+  end
+
+  describe "with string keys" do
+    let(:spec_data) { [
+      {key: "alpha", type: "string", validation: {"length" => {"maximum" => 5}}}
+    ] }
+    let(:instance_data) { {alpha: "xxxxxx"} }
+
+    it "should still work" do
+      expect(instance.valid?).to be false
+      expect(instance.errors[:alpha]).to eq ["is too long (maximum is 5 characters)"]
+    end
+  end
+
+  describe "with custom i18n'd message" do
+    let(:spec_data) { [
+      {key: "alpha", type: "string", validation: {length: {maximum: 5, message: :foo}}},
+    ] }
+    let(:instance_data) { {alpha: "xxxxxx"} }
+
+    it "should look up message in expected place" do
+      original_translate = I18n.method(:translate)
+      allow(I18n).to receive(:translate) do |key, options|
+        if key == "custom_fields.errors.mod.att.alpha"
+          expect(options[:default]).to eq %i(
+            activemodel.errors.messages.foo
+            activerecord.errors.messages.foo
+            errors.messages.foo
+          )
+          "Teh error msg"
+        else
+          original_translate.call(key, options)
+        end
+      end
+      expect(instance.valid?).to be false
+      expect(instance.errors[:alpha]).to eq ["Teh error msg"]
+    end
+  end
+end

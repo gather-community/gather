a5a5b34667b36da9b3c081f99c1f0580eac00278
Tom Smyth <tom@sassafras.coop>
2017-03-29 21:25:17 -0400

    5320: Fixed photo max size setting

--- spec/features/user_form_spec.rb
+++ spec/features/user_form_spec.rb
@@ -8,12 +8,12 @@ feature "user form" do
   let!(:household2) { create(:household, name: "Potatoheads") }
 
   before do
-    @old_max_size = Settings.photos.max_size
-    Settings.photos.max_size = 1.megabyte
+    @old_max_size = Settings.photos.max_size_mb
+    Settings.photos.max_size_mb = 1
   end
 
   after do
-    Settings.photos.max_size = @old_max_size
+    Settings.photos.max_size_mb = @old_max_size
   end
 
   shared_examples_for "editing user" do

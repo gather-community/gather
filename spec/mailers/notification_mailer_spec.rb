739ae1778be2f8092c314819d7cbfd3d8e597430
Tom Smyth <tom@sassafras.coop>
2017-03-31 22:43:19 -0400

    6178: Fixed spec

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 describe NotificationMailer do
   describe "shift_reminder" do
     let(:resource) { create(:resource, name: "Place", meal_abbrv: "FOO") }
+    let(:ca) { resource.community.abbrv }
     let(:meal) { create(:meal, served_at: "2017-01-01 12:00", resources: [resource]) }
     let(:assignment) { create(:assignment, meal: meal, role: "asst_cook") }
     let(:mail) { described_class.shift_reminder(assignment).deliver_now }
@@ -14,11 +15,10 @@ describe NotificationMailer do
 
     it "renders the subject" do
       expect(mail.subject).to eq(
-        "Job Reminder: You are Assistant Cook for a meal at Sun Jan 01 11:00am at C1 FOO")
+        "Job Reminder: You are Assistant Cook for a meal at Sun Jan 01 11:00am at #{ca} FOO")
     end
 
     it "renders the correct times in the body" do
-      ca = resource.community.abbrv
       expect(mail.body.encoded).to match "Your shift is on Sun Jan 01 from 11:00am-11:55am at #{ca} Place."
       expect(mail.body.encoded).to match "The meal is scheduled to be served at 12:00pm."
     end

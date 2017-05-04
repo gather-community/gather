692151d1c579c40469d018b551ccef5c83dfeb45
Tom Smyth <tom@sassafras.coop>
2017-05-03 22:20:32 -0400

    4564: Refactored close past meal job to use settings

--- app/jobs/meals/close_past_meals_job.rb
+++ app/jobs/meals/close_past_meals_job.rb
@@ -1,10 +1,8 @@
 # Closes past meals after a certain delay period.
 module Meals
   class ClosePastMealsJob
-    CLOSE_CUTOFF_AGE = 2.hours
-
     def perform
-      Meal.open.with_min_age(CLOSE_CUTOFF_AGE).each { |m| m.close! }
+      Meal.open.with_min_age(Settings.meals.close_cutoff_age.hours).each { |m| m.close! }
     end
   end
 end

55ea710420b29e9abaae19ab2851ee42592ec1bc
Tom Smyth <tom@sassafras.coop>
2017-05-22 20:32:02 -0400

    Fixed migrations to set without_tenant

--- db/migrate/20170329232743_copy_community_settings_to_new_json_field.rb
+++ db/migrate/20170329232743_copy_community_settings_to_new_json_field.rb
@@ -4,46 +4,48 @@ end
 
 class CopyCommunitySettingsToNewJsonField < ActiveRecord::Migration
   def up
-    Community.all.each do |community|
-      old_settings = community.settings
-      community.send(:write_attribute, :config, {
-        meals: {
-          reimb_instructions: "Place " << old_settings[:meal_reimb_dropoff_loc],
-          admin_email: old_settings[:meals_ctte_email],
-          extra_roles: "asst_cook, cleaner" << (old_settings[:has_table_setters] ? ", table_setter" : ""),
-          default_shift_times: {
-            start: {
-              head_cook: -195,
-              asst_cook: -135,
-              table_setter: -60,
-              cleaner: 45,
-            },
-            end: {
-              head_cook: 0,
-              asst_cook: 0,
-              table_setter: 0,
-              cleaner: 165,
+    ActsAsTenant.without_tenant do
+      Community.all.each do |community|
+        old_settings = community.settings
+        community.send(:write_attribute, :config, {
+          meals: {
+            reimb_instructions: "Place " << old_settings[:meal_reimb_dropoff_loc],
+            admin_email: old_settings[:meals_ctte_email],
+            extra_roles: "asst_cook, cleaner" << (old_settings[:has_table_setters] ? ", table_setter" : ""),
+            default_shift_times: {
+              start: {
+                head_cook: -195,
+                asst_cook: -135,
+                table_setter: -60,
+                cleaner: 45,
+              },
+              end: {
+                head_cook: 0,
+                asst_cook: 0,
+                table_setter: 0,
+                cleaner: 165,
+              }
+            }
+          },
+          reservations: {
+            kinds: old_settings[:reservation_kinds].try(:join, ", "),
+            meals: {
+              default_total_time: 330,
+              default_prep_time: 180
+            }
+          },
+          billing: {
+            payment_instructions: old_settings[:payment_instructions],
+            statement_terms: old_settings[:statement_terms],
+            late_fee_policy: {
+              fee_type: old_settings[:late_fee_policy].try(:[], :fee_type),
+              threshold: old_settings[:late_fee_policy].try(:[], :threshold),
+              amount: old_settings[:late_fee_policy].try(:[], :fee_amount)
             }
           }
-        },
-        reservations: {
-          kinds: old_settings[:reservation_kinds].try(:join, ", "),
-          meals: {
-            default_total_time: 330,
-            default_prep_time: 180
-          }
-        },
-        billing: {
-          payment_instructions: old_settings[:payment_instructions],
-          statement_terms: old_settings[:statement_terms],
-          late_fee_policy: {
-            fee_type: old_settings[:late_fee_policy].try(:[], :fee_type),
-            threshold: old_settings[:late_fee_policy].try(:[], :threshold),
-            amount: old_settings[:late_fee_policy].try(:[], :fee_amount)
-          }
-        }
-      })
-      community.save!
+        })
+        community.save!
+      end
     end
   end
 end
--- db/migrate/20170417131246_add_slug_to_communities.rb
+++ db/migrate/20170417131246_add_slug_to_communities.rb
@@ -1,7 +1,9 @@
 class AddSlugToCommunities < ActiveRecord::Migration
   def up
     add_column :communities, :slug, :string, unique: true
-    Community.all.each { |c| c.update_column(:slug, c.name.downcase.gsub(" ", "")) }
+    ActsAsTenant.without_tenant do
+      Community.all.each { |c| c.update_column(:slug, c.name.downcase.gsub(" ", "")) }
+    end
     change_column_null :communities, :slug, false
   end
 end

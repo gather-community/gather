f23c309face1706c553f30b345bcbf67110dc142
Tom Smyth <tom@sassafras.coop>
2017-06-04 10:01:50 -0400

    6139: Simplified exporter code

--- app/decorators/csv/user_decorator.rb
+++ app/decorators/csv/user_decorator.rb
@@ -1,6 +1,5 @@
 module Csv
   class UserDecorator < ::UserDecorator
-    include Utils
     delegate_all
 
     def birthdate
@@ -13,6 +12,29 @@ module Csv
 
     def child
       bool(object.child?)
+    end
+
+    def vehicles
+      adult? && household.vehicles.any? ? household.vehicles.map(&:to_s).join("; ") : nil
+    end
+
+    def unit_num
+      household.unit_num
+    end
+
+    def garage_nums
+      household.garage_nums
+    end
+
+    private
+
+    def l(date_or_time)
+      return nil if date_or_time.nil?
+      I18n.l(date_or_time, format: :csv_full)
+    end
+
+    def bool(val)
+      I18n.t("common.#{val ? 'true' : 'false'}")
     end
   end
 end

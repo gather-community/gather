dccd28062e072ad58c467962dcd3fae43cba348e
Tom Smyth <tom@sassafras.coop>
2017-06-15 09:32:46 -0400

    4563: Finished fake meals

--- app/models/utils/fake_data_generator.rb
+++ app/models/utils/fake_data_generator.rb
@@ -3,7 +3,9 @@ module Utils
   class FakeDataGenerator
     include FactoryGirl::Syntax::Methods
 
-    attr_accessor :community, :households, :users, :photos
+    attr_accessor :community, :households, :users, :photos, :meal_loc
+
+    MONTHS = 5
 
     def initialize(community, photos: false)
       self.community = community
@@ -11,19 +13,26 @@ module Utils
     end
 
     def generate
+      tz = Time.zone
+      Time.zone = community.settings.time_zone
       ActiveRecord::Base.transaction do
-        create(:meals_formula)
-        create_households_and_users
-        deactivate_households
+        begin
+          create(:meals_formula)
+          create_households_and_users
+          deactivate_households
+          create_resources
+          create_meals
 
-        # Assigns randomly chosen adults to admin, photographer, and meal biller roles.
-        # Creates 10 meals with assignments, menus, signups, some of them finalized.
-        # Creates 5 reservable resources with lorem ipsum guidelines.
-        # Creates one shared guideline with heading to indicate same and attaches to a random subset of resources.
-        # Creates 3 resource protocols and attaches to several resources each.
-        # Creates a random assortment of reservations on the various resources.
-        # Runs statements for all users.
+          # Creates 5 reservable resources with lorem ipsum guidelines.
+          # Creates one shared guideline with heading to indicate same and attaches to a random subset of resources.
+          # Creates 3 resource protocols and attaches to several resources each.
+          # Creates a random assortment of reservations on the various resources.
+          # Runs statements for all users.
+        ensure
+          users.each { |u| u.photo.destroy }
+        end
       end
+      Time.zone = tz
     end
 
     private
@@ -31,9 +40,10 @@ module Utils
     # Creates 24 households (3 inactive) with random vehicles and emergency contacts.
     def create_households_and_users
       self.users = []
+      adults = []
       garages = ((1..20).to_a + [nil] * 4).shuffle
 
-      last_names = unique_set(24) { Faker::Name.last_name }
+      last_names = unique_set(60) { Faker::Name.last_name }
 
       self.households = 24.times.map do |i|
         dir = Rails.root.join("lib/random_data/households/#{i.to_s.rjust(2, "0")}/*.jpg")
@@ -43,15 +53,14 @@ module Utils
         adults = Dir[dir].map do |path|
           age = File.basename(path, ".jpg").to_i
           next if age < 16
-          bday_format = "%b %d #{bool_prob(50) ? '%Y' : ''}"
-          bday = Faker::Date.birthday(age, age + 1).to_time.strftime(bday_format)
+          bday = Faker::Date.birthday(age, age + 1)
           first_name = Faker::Name.unisex_name
 
           build(:user,
             fake: true,
             first_name: first_name,
-            last_name: bool_prob(70) ? last_name : Faker::Name.last_name,
-            birthdate_str: bday,
+            last_name: bool_prob(70) ? last_name : last_names.pop,
+            birthdate: bday,
             email: "#{first_name}@#{Faker::Internet.domain_name}",
             mobile_phone: Faker::PhoneNumber.simple,
             home_phone: bool_prob(50) ? Faker::PhoneNumber.simple : nil,
@@ -69,7 +78,7 @@ module Utils
           build(:user, :child,
             fake: true,
             first_name: Faker::Name.unisex_name,
-            last_name: last_name,
+            last_name: bool_prob(90) ? last_name : last_names.pop,
             email: nil,
             mobile_phone: nil,
             guardians: adults,
@@ -98,6 +107,56 @@ module Utils
         joined = h.users.map(&:joined_on).max
         Timecop.travel(joined + rand((Date.today - joined).to_i)) do
           h.deactivate!
+        end
+      end
+    end
+
+    def create_resources
+      self.meal_loc = create(:resource, meal_abbrv: "CH")
+    end
+
+    def create_meals
+      samples = YAML.load_file(Rails.root.join("lib/random_data/meals.yml"))
+      creator = User.adults.first
+      all_households = Household.all
+
+      # All but 1 month of meals should be in past.
+      Timecop.travel(((MONTHS - 1) * 30).days.ago.beginning_of_week.midnight) do
+
+        # Create all meals and signups at once.
+        MONTHS.times do |month|
+          4.times do |week|
+            [0, 4].each do |day|
+              sample = samples.pop
+              served_at = Time.zone.now + month.months + week.weeks + day.days + 18.hours
+              staff = User.adults.active.shuffle
+
+              meal = create(:meal, sample.merge(
+                resources: [meal_loc],
+                community: community,
+                head_cook: staff[0],
+                asst_cooks: staff[1..2],
+                cleaners: staff[3..4],
+                served_at: served_at,
+                creator: creator
+              ))
+
+              num_households = 5 + rand(all_households.size - 5)
+              all_households.shuffle[0...num_households].each do |household|
+                members = household.users.to_a
+                type = bool_prob(70) ? "meat" : "veg"
+                create(:signup,
+                  meal: meal,
+                  household: household,
+                  "senior_#{type}" => members.count { |u| u.age && u.age >= 65 },
+                  "adult_#{type}" => members.count { |u| u.age.nil? || (u.age < 65 && u.age >= 18) },
+                  "big_kid_#{type}" => members.count { |u| u.age && u.age < 18 && u.age >= 5 },
+                  "little_kid_#{type}" => members.count { |u| u.age && u.age < 5 }
+                )
+                break if meal.reload.full?
+              end
+            end
+          end
         end
       end
     end
--- spec/factories/meals/formula_factory.rb
+++ spec/factories/meals/formula_factory.rb
@@ -2,10 +2,14 @@ FactoryGirl.define do
   factory :meals_formula, class: "Meals::Formula" do
     community { default_community }
     effective_on "2015-01-01"
+    senior_meat 0.75
+    senior_veg 0.6
     adult_meat 1.0
     adult_veg 0.9
     big_kid_meat 0.75
     big_kid_veg 0.6
+    little_kid_meat 0
+    little_kid_veg 0
     pantry_fee 0.10
     meal_calc_type "share"
     pantry_calc_type "percent"

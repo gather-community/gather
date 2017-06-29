6a0229562f109bdaf67298f82a1d8da12744c05a
Tom Smyth <tom@sassafras.coop>
2017-06-29 15:24:06 -0700

    6731: Removed unused controller override

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    omniauth_callbacks: "users/omniauth_callbacks",
-    registrations: "users/registrations"
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   resources :users do

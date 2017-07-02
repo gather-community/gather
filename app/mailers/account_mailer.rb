92b6fc396b8ddec4ed520aa17ba0dd8a4795075c
Tom Smyth <tom@sassafras.coop>
2017-07-02 10:17:26 -0600

    6409: Refactored mailer to know about users and households and remove refs to adults

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,21 +1,17 @@
 class AccountMailer < ApplicationMailer
+  attr_reader :community
+
   def statement_notice(statement)
     load_statement_vars(statement)
-    mail(to: household_emails, subject: "New Account Statement for #{@community.name}")
+    mail(to: @household, subject: "New Account Statement for #{community.name}")
   end
 
   def statement_reminder(statement)
     load_statement_vars(statement)
-    mail(to: household_emails, subject: "Payment Reminder for #{@community.name} Account")
+    mail(to: @household, subject: "Payment Reminder for #{community.name} Account")
   end
 
   private
-
-  def mail(*args)
-    with_community_subdomain(@community) do
-      super
-    end
-  end
 
   def load_statement_vars(statement)
     @statement = statement

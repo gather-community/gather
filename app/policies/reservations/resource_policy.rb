6aef334ec7b392c2e82756a103f52ea8d1bddf3a
Tom Smyth <tom@sassafras.coop>
2017-07-06 17:27:54 -0600

    4243: Added permitted attribs to resource policy

--- app/policies/reservations/resource_policy.rb
+++ app/policies/reservations/resource_policy.rb
@@ -2,6 +2,21 @@ module Reservations
   class ResourcePolicy < ApplicationPolicy
     alias_method :resource, :record
 
+    class Scope < Scope
+      def resolve
+        # Only show visible resources unless admin.
+        scope_with_visibility = active_admin? ? scope : scope.visible
+
+        # Need to load the resources because access_level is computed by RuleSet which can't be computed
+        # at database level.
+        ids = scope_with_visibility.select do |resource|
+          sample_reservation = Reservation.new(reserver: user, resource: resource)
+          sample_reservation.access_level != "forbidden"
+        end.map(&:id)
+        scope.where(id: ids)
+      end
+    end
+
     def index?
       active_admin?
     end
@@ -22,19 +37,9 @@ module Reservations
       active_admin?
     end
 
-    class Scope < Scope
-      def resolve
-        # Only show visible resources unless admin.
-        scope_with_visibility = active_admin? ? scope : scope.visible
-
-        # Need to load the resources because access_level is computed by RuleSet which can't be computed
-        # at database level.
-        ids = scope_with_visibility.select do |resource|
-          sample_reservation = Reservation.new(reserver: user, resource: resource)
-          sample_reservation.access_level != "forbidden"
-        end.map(&:id)
-        scope.where(id: ids)
-      end
+    def permitted_attributes
+      [:default_calendar_view, :guidelines, :hidden, :meal_abbrv, :name,
+        :photo, :photo_tmp_id, :photo_destroy]
     end
   end
 end

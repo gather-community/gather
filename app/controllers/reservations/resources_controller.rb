88d7903ed7117b11d3db8bf1b6e43321405c26cc
Tom Smyth <tom@sassafras.coop>
2017-07-16 20:07:29 -0400

    4243: Added new/create

--- app/controllers/reservations/resources_controller.rb
+++ app/controllers/reservations/resources_controller.rb
@@ -1,6 +1,7 @@
 module Reservations
   class ResourcesController < ApplicationController
     decorates_assigned :resource, :resources
+    helper_method :sample_resource
 
     before_action -> { nav_context(:reservations, :resources) }
 
@@ -10,9 +11,27 @@ module Reservations
         with_reservation_counts.where(community: current_community).by_name
     end
 
+    def new
+      @resource = sample_resource
+      authorize @resource
+    end
+
     def edit
       @resource = Resource.find(params[:id])
       authorize @resource
+    end
+
+    def create
+      @resource = sample_resource
+      @resource.assign_attributes(resource_params)
+      authorize @resource
+      if @resource.save
+        flash[:success] = "Resource created successfully."
+        redirect_to reservations_resources_path
+      else
+        set_validation_error_notice
+        render :new
+      end
     end
 
     def update
@@ -54,7 +73,7 @@ module Reservations
     private
 
     def sample_resource
-      @sample_resource ||= Reservations::Resource.new(community: current_community)
+      @sample_resource ||= Resource.new(community: current_community)
     end
 
     # Pundit built-in helper doesn't work due to namespacing

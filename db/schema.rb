2e42045429201ebf7aff8ea1a1ac6404ac636af5
Tom Smyth <tom@sassafras.coop>
2017-08-06 11:43:46 -0400

    4560: Repaired pantry calc type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806150932) do
+ActiveRecord::Schema.define(version: 20170806154142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

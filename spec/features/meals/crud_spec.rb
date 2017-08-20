ee49cb4f4979e1669f1d958b25fe6d1a22cf4878
Tom Smyth <tom@sassafras.coop>
2017-08-19 21:43:48 -0400

    4560: Added head cook and regular user scenarios

--- spec/features/meals/crud_spec.rb
+++ spec/features/meals/crud_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 feature "meal crud", js: true do
-  let!(:actor) { create(:admin) }
   let!(:users) { create_list(:user, 2) }
   let!(:location) { create(:resource, name: "Dining Room", abbrv: "DR", meal_hostable: true) }
   let!(:formula) { create(:meal_formula, is_default: true) }
-  let!(:meals) { create_list(:meal, 5, :with_menu, formula: formula, resources: [location]) }
+  let!(:meals) { create_list(:meal, 5, formula: formula, resources: [location]) }
+  let!(:meal) { meals.first }
 
   around do |example|
     with_user_home_subdomain(actor) { example.run }
@@ -15,23 +15,96 @@ feature "meal crud", js: true do
     login_as(actor, scope: :user)
   end
 
-  scenario do
-    # Index
+  context "as meals coordinator" do
+    let!(:actor) { create(:meals_coordinator) }
+
+    scenario do
+      test_index
+
+      # Create with no menu
+      click_on("Create Meal")
+      select2(location.name, from: ".meal_resource_ids", type: :inline)
+      select2(users[0].name, from: "#meal_head_cook_assign_attributes_user_id")
+      select2(users[1].name, from: "#meal_asst_cook_assigns_attributes_0_user_id")
+      click_on("Create Meal")
+      expect_success
+
+      find("tr", text: users[0].name).find("a", text: "[No Title]").click
+      click_link("Edit")
+      fill_in_menu
+
+      # Show
+      find("a", text: "Southern Beans").click
+      expect(page).to have_content("Southern Beans and Rice")
+      expect(page).to have_content("Chocolate")
+
+      summary_close_reopen
+
+      # Delete
+      find("a", text: "Southern Beans").click
+      click_link("Edit")
+      click_on("Delete Meal")
+      expect_success
+      expect(page).not_to have_content("Southern Beans and Rice")
+    end
+  end
+
+  context "as head cook" do
+    let!(:actor) { meal.head_cook }
+
+    scenario do
+      test_index
+      expect(page).not_to have_content("Create Meal")
+
+      # Update to add menu
+      find("tr", text: actor.name).find("a", text: "[No Title]").click
+      click_link("Edit")
+      expect(page).not_to have_content("Delete Meal")
+      fill_in_menu
+
+      # Show
+      find("a", text: "Southern Beans").click
+      expect(page).to have_content("Southern Beans and Rice")
+      expect(page).to have_content("Chocolate")
+
+      summary_close_reopen
+    end
+  end
+
+  context "as regular user" do
+    let!(:actor) { create(:user) }
+
+    scenario do
+      test_index
+      expect(page).not_to have_content("Create Meal")
+
+      # Update to change assignment
+      find("tr", text: meals[4].head_cook.name).find("a", text: "[No Title]").click
+      click_link("Edit")
+      expect(page).not_to have_content("Delete Meal")
+      select2(actor.name, from: "#meal_asst_cook_assigns_attributes_0_user_id")
+      click_on("Update Meal")
+      expect_success
+
+      # Show
+      find("tr", text: meals[4].head_cook.name).find("a", text: "[No Title]").click
+      expect(page).to have_css("#meal-menu", text: actor.name)
+
+      # Summary
+      click_link("Summary")
+      expect(page).to have_content("This meal will require")
+      expect(page).to have_css("#meal-menu", text: actor.name)
+      expect(page).not_to have_css("a", text: "Close")
+    end
+  end
+
+  def test_index
     visit("/meals")
-    expect(page).to have_content(meals[0].title)
-    expect(page).to have_content(meals[4].title)
+    expect(page).to have_css("tr", text: meals[0].head_cook.name)
+    expect(page).to have_css("tr", text: meals[4].head_cook.name)
+  end
 
-    # Create with no menu
-    click_on("Create Meal")
-    select2(location.name, from: ".meal_resource_ids", type: :inline)
-    select2(users[0].name, from: "#meal_head_cook_assign_attributes_user_id")
-    select2(users[1].name, from: "#meal_asst_cook_assigns_attributes_0_user_id")
-    click_on("Create Meal")
-    expect_success
-
-    # Update to add menu
-    find("tr", text: users[0].name).find("a", text: "[No Title]").click
-    click_link("Edit")
+  def fill_in_menu
     fill_in("Title", with: "Southern Beans and Rice")
     fill_in("Entrees", with: "Beans, rice, sausage")
     fill_in("Side", with: "Collards")
@@ -41,18 +114,13 @@ feature "meal crud", js: true do
     check("Dairy")
     click_on("Update Meal")
     expect_success
+  end
 
-    # Show
-    find("a", text: "Southern Beans").click
-    expect(page).to have_content(users[0].name)
-    expect(page).to have_content(users[1].name)
-    expect(page).to have_content("Southern Beans and Rice")
-    expect(page).to have_content("Chocolate")
-
+  def summary_close_reopen
     # Summary
     click_link("Summary")
     expect(page).to have_content("This meal will require")
-    expect(page).to have_content(users[0].name)
+    expect(page).to have_content("Southern Beans and Rice")
 
     # Close/reopen
     click_link("Close")
@@ -60,16 +128,5 @@ feature "meal crud", js: true do
     find("a", text: "Southern Beans").click
     click_link("Reopen")
     expect_success
-
-    # Delete
-    find("a", text: "Southern Beans").click
-    click_link("Edit")
-    click_on("Delete Meal")
-    expect_success
-    expect(page).not_to have_content("Southern Beans and Rice")
-
-
-
-
   end
 end

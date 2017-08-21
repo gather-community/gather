629b99c28bded296eff7a1338b8e5a541b951c3c
Tom Smyth <tom@sassafras.coop>
2017-08-21 09:59:15 -0400

    Fixed migration

--- db/migrate/20170805130000_add_formula_id_to_meals.rb
+++ db/migrate/20170805130000_add_formula_id_to_meals.rb
@@ -2,6 +2,6 @@ class AddFormulaIdToMeals < ActiveRecord::Migration
   def change
     add_column :meals, :formula_id, :integer
     add_index :meals, :formula_id
-    add_foreign_key :meals, :meal_formulas, column: :formula_id
+    add_foreign_key :meals, :meals_formulas, column: :formula_id
   end
 end

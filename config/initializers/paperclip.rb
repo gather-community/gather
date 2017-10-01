7b0022eff3cc4990d18b7da297b9c0504a11cf8e
Tom Smyth <tom@sassafras.coop>
2017-09-30 21:49:39 -0400

    7239: Added clean destruction

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,5 @@
 prefix = Rails.env.test? ? "/test" : ""
 Paperclip::Attachment.default_options.merge!(
   path: ":rails_root/public/system#{prefix}/:class/:attachment/:id_partition/:style/:filename",
+  url: "/system#{prefix}/:class/:attachment/:id_partition/:style/:filename"
 )

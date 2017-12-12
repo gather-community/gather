7bafb04e9c98ec002bc71dfd5101f6217cc90f21
Tom Smyth <tom@sassafras.coop>
2017-12-11 22:46:46 -0500

    7266: Fixed page versions

--- app/models/wiki/page.rb
+++ app/models/wiki/page.rb
@@ -1,16 +1,38 @@
 module Wiki
   class Page < ActiveRecord::Base
     acts_as_tenant :cluster
-    acts_as_wiki_page
+
+    attr_accessor :comment
 
     belongs_to :community
     belongs_to :creator, class_name: "User"
     belongs_to :updator, class_name: "User"
-    has_many :versions, class_name: "Wiki::PageVersion", dependent: :destroy
+    has_many :versions, -> { order('number DESC') }, class_name: "Wiki::PageVersion", dependent: :destroy
 
     scope :in_community, ->(c) { where(community_id: c.id) }
     scope :by_title, -> { order("LOWER(title)") }
 
     validates :title, presence: true
+
+    after_save :create_new_version
+
+    def self.find_by_path_or_new(path)
+      find_by(path: path) || new(path: path, title: CGI.unescape(path))
+    end
+
+    def last_version_number
+      versions.maximum(:number) || 0
+    end
+
+    private
+
+    def create_new_version
+      n = last_version_number
+      v = versions.build
+      v.attributes = attributes.slice(*(v.attribute_names - ['id']))
+      v.comment = comment
+      v.number = n + 1
+      v.save!
+    end
   end
 end

dc3251423614a2839eb54cf4960862f841af9389
Tom Smyth <tom@sassafras.coop>
2017-12-17 21:59:52 -0500

    7533: Got rid of last irwi dependencies

--- app/models/wiki/page_version.rb
+++ app/models/wiki/page_version.rb
@@ -1,6 +1,31 @@
 module Wiki
   class PageVersion < ActiveRecord::Base
     acts_as_tenant :cluster
-    acts_as_wiki_page_version
+
+    belongs_to :page, class_name: "Wiki::Page"
+    belongs_to :updator, class_name: "User"
+
+    before_update :raise_on_update
+
+    def self.between(first, last)
+      first = first.to_i
+      last = last.to_i
+      first, last = last, first if last < first
+      where('number >= ? AND number <= ?', first, last)
+    end
+
+    def next
+      self.class.first conditions: ["id > ? AND page_id = ?", id, page_id], order: 'id ASC'
+    end
+
+    def previous
+      self.class.first conditions: ["id < ? AND page_id = ?", id, page_id], order: 'id DESC'
+    end
+
+    private
+
+    def raise_on_update
+      raise ActiveRecordError, "Can't modify existing version"
+    end
   end
 end

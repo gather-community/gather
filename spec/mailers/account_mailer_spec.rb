a136524d20d0fe8ca17d8ecde8bd6638a652ae56
Tom Smyth <tom@sassafras.coop>
2018-01-01 11:25:53 -0500

    7157: Added biller emails as statement email reply-to

--- spec/mailers/account_mailer_spec.rb
+++ spec/mailers/account_mailer_spec.rb
@@ -1,51 +1,58 @@
 require "rails_helper"
 
 describe AccountMailer do
+  # Make separate community in cluster to test cross-community billing
+  let(:community) { create(:community) }
+
   let(:users) { create_list(:user, 2) }
   let(:household) { create(:household, users: users) }
   let(:account) { create(:account, household: household, community: community) }
   let(:statement) { create(:statement, account: account, total_due: 9.99) }
+  let!(:billers) { create_list(:biller, 2, community: community) }
 
-  # Make separate community in cluster to test cross-community billing
-  let(:community) { create(:community) }
-
-  describe "statement_notice" do
-    let(:mail) { described_class.statement_notice(statement).deliver_now }
-
+  shared_examples_for "statement email" do
     it "sets the right recipients" do
       expect(mail.to).to match_array(users.map(&:email))
     end
 
-    it "renders the subject" do
-      expect(mail.subject).to eq("New Account Statement for #{community.name}")
+    it "sets the right reply to" do
+      expect(mail.reply_to).to match_array(billers.map(&:email))
     end
 
     it "renders the correct name and URL in the body" do
       expect(mail.body.encoded).to match("Dear #{household.name} Household,")
       expect(mail.body.encoded).to include("The amount due is $9.99.")
-      expect(mail.body.encoded).to have_correct_statement_url(statement)
+      expect(mail.body.encoded).to contain_community_url(statement.community, "/statements/#{statement.id}")
+    end
+  end
+
+  describe "statement_notice" do
+    let(:mail) { described_class.statement_notice(statement).deliver_now }
+
+    context "with billers" do
+      it_behaves_like "statement email"
+
+      it "renders the subject" do
+        expect(mail.subject).to eq("New Account Statement for #{community.name}")
+      end
+    end
+
+    context "with no billers" do
+      let(:billers) { [] }
+
+      it "sets the right reply to" do
+        expect(mail.reply_to).to be_empty
+      end
     end
   end
 
   describe "statement_reminder" do
     let(:mail) { described_class.statement_reminder(statement).deliver_now }
 
-    it "sets the right recipients" do
-      expect(mail.to).to match_array(users.map(&:email))
-    end
+    it_behaves_like "statement email"
 
     it "renders the subject" do
       expect(mail.subject).to eq("Payment Reminder for #{community.name} Account")
     end
-
-    it "renders the correct name and URL in the body" do
-      expect(mail.body.encoded).to match("Dear #{household.name} Household,")
-      expect(mail.body.encoded).to include("The amount due is $9.99.")
-      expect(mail.body.encoded).to have_correct_statement_url(statement)
-    end
-  end
-
-  def have_correct_statement_url(statement)
-    contain_community_url(statement.community, "/statements/#{statement.id}")
   end
 end

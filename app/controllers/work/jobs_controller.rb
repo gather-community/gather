67fd4e4e50c9d02ca992dda4ba71bfb4d76b3097
Tom Smyth <tom@sassafras.coop>
2018-01-27 23:48:33 -0500

    7835: Added create action

--- app/controllers/work/jobs_controller.rb
+++ app/controllers/work/jobs_controller.rb
@@ -10,12 +10,35 @@ module Work
       authorize @job
     end
 
+    def create
+      @job = Job.new(community: current_community)
+      @job.assign_attributes(job_params)
+      authorize @job
+      if @job.save
+        flash[:success] = "Job created successfully."
+        redirect_to work_jobs_path
+      else
+        set_validation_error_notice(@job)
+        prep_form_vars
+        render :new
+      end
+    end
+
     private
+
+    def sample_job
+      Job.new(community: current_community)
+    end
 
     def prep_form_vars
       @periods = Period.for_community(current_community).active
       @period = @periods.first # This will change to use lens.
       @requesters = People::Group.for_community(current_community).by_name
+    end
+
+    # Pundit built-in helper doesn't work due to namespacing
+    def job_params
+      params.require(:work_job).permit(policy(@job).permitted_attributes)
     end
   end
 end

c72f2eec70c9d1d531706fc93340aad67a305b5c
Tom Smyth <tom@sassafras.coop>
2018-02-10 14:16:22 -0500

    7956: Set null false for reservation start and end times

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208003035) do
+ActiveRecord::Schema.define(version: 20180210191418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -334,7 +334,7 @@ ActiveRecord::Schema.define(version: 20180208003035) do
   create_table "reservations", id: :serial, force: :cascade do |t|
     t.integer "cluster_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "ends_at"
+    t.datetime "ends_at", null: false
     t.string "kind"
     t.integer "meal_id"
     t.string "name", limit: 24, null: false
@@ -342,7 +342,7 @@ ActiveRecord::Schema.define(version: 20180208003035) do
     t.integer "reserver_id", null: false
     t.integer "resource_id", null: false
     t.integer "sponsor_id"
-    t.datetime "starts_at"
+    t.datetime "starts_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cluster_id"], name: "index_reservations_on_cluster_id"
     t.index ["meal_id"], name: "index_reservations_on_meal_id"

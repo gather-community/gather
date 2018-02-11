f9685149760786af13b7e05c8629fab6fa84a60b
Tom Smyth <tom@sassafras.coop>
2018-02-11 10:09:33 -0500

    Fixed bug in serializer

--- app/serializers/reservation_rule_set_serializer.rb
+++ app/serializers/reservation_rule_set_serializer.rb
@@ -2,10 +2,10 @@ class ReservationRuleSetSerializer < ApplicationSerializer
   attributes :fixed_start_time, :fixed_end_time, :access_level
 
   def fixed_start_time
-    I18n.l(object.fixed_start_time, format: :machine_readable)
+    object.fixed_start_time && I18n.l(object.fixed_start_time, format: :machine_readable)
   end
 
   def fixed_end_time
-    I18n.l(object.fixed_end_time, format: :machine_readable)
+    object.fixed_end_time && I18n.l(object.fixed_end_time, format: :machine_readable)
   end
 end

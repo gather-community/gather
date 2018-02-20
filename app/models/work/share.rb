e5ae20c320a72c507c1c9bddefe98399ff894152
Tom Smyth <tom@sassafras.coop>
2018-02-19 22:10:23 -0500

    7825: Added share policy

--- app/models/work/share.rb
+++ app/models/work/share.rb
@@ -4,5 +4,9 @@ module Work
 
     belongs_to :period
     belongs_to :user
+
+    scope :for_community, ->(c) { joins(:period).where("work_periods.community_id": c.id) }
+
+    delegate :community, to: :period
   end
 end

c9209e44b6c82eee6b1a7439f114aa4e66bf8eb4
Tom Smyth <tom@sassafras.coop>
2018-02-23 08:29:23 -0500

    7825: First draft of shares form

--- app/controllers/work/shares_controller.rb
+++ app/controllers/work/shares_controller.rb
@@ -10,7 +10,13 @@ module Work
         skip_policy_scope
         lenses.hide!
       else
-        @shares = policy_scope(Share).where(period: @period)
+        users = policy_scope(User).active.by_name.in_community(current_community).decorate
+        users = users.reject { |c| c.age.try(:<, Share::MIN_AGE) }
+        @users_by_kind = users.group_by(&:kind)
+
+        shares = policy_scope(Share).where(period: @period)
+        @shares_by_user = shares.index_by(&:user)
+        users.each { |u| @shares_by_user[u] ||= Share.new(user: u, period: @period) }
       end
     end
 
--- app/models/work/share.rb
+++ app/models/work/share.rb
@@ -1,8 +1,11 @@
 module Work
   class Share < ApplicationRecord
+    # At some point it becomes ridiculous to show very small children in the work share system.
+    MIN_AGE = 5
+
     acts_as_tenant(:cluster)
 
-    belongs_to :period
+    belongs_to :period, inverse_of: :shares
     belongs_to :user
 
     scope :for_community, ->(c) { joins(:period).where("work_periods.community_id": c.id) }

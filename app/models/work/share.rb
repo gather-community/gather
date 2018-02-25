6e686ee254ab395d60e3df7247dd3e5aee3de426
Tom Smyth <tom@sassafras.coop>
2018-02-25 11:11:15 -0500

    7825: Added for_period scope to Share

--- app/models/work/share.rb
+++ app/models/work/share.rb
@@ -9,6 +9,7 @@ module Work
     belongs_to :user
 
     scope :for_community, ->(c) { joins(:period).where("work_periods.community_id": c.id) }
+    scope :for_period, ->(p) { joins(:user).where(period_id: p.id, "users.deactivated_at": nil) }
 
     delegate :community, to: :period
   end

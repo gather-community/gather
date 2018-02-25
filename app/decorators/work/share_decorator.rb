5a89e1be03a6e0ba40cb442dc157d8dd0c0e2827
Tom Smyth <tom@sassafras.coop>
2018-02-24 19:25:17 -0500

    7825: Finished main part of form and styled

--- app/decorators/work/share_decorator.rb
+++ app/decorators/work/share_decorator.rb
@@ -2,8 +2,8 @@ module Work
   class ShareDecorator < ApplicationDecorator
     delegate_all
 
-    def self.select_options
-      {full: 1.0, half: 0.5, none: 0.0}.map { |n, p| [I18n.t("work/shifts.portion_options.#{n}"), p.to_s] }
+    def select_options
+      {full: 1.0, half: 0.5, none: 0.0}.map { |n, p| [I18n.t("work/shares.portion_options.#{n}"), p.to_s] }
     end
   end
 end

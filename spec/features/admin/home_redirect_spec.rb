91d451fd49c4ee148052183e06bbba68a3052e43
dtdimond <dtdimond@gmail.com>
2018-02-25 18:39:42 -0600

    Refactor default landing page to use en.yml translations

--- spec/features/admin/home_redirect_spec.rb
+++ spec/features/admin/home_redirect_spec.rb
@@ -13,40 +13,17 @@ feature "home redirect" do
     visit "/"
     expect(page).to have_title("Directory")
 
-    ["Meals", "Directory", "Reservations", "Wiki"].each { |new_default|
+    %w[Meals Directory Reservations Wiki].each do |new_default|
       change_default_home(new_default)
       visit "/"
       expect(page).to have_title(new_default)
-    }
+    end
   end
 end
 
 def change_default_home(new_default)
     visit "/admin/settings/community"
-    select(new_default, from: "Default landing page")
+    select(new_default, from: "Default Landing Page")
     click_on("Save Settings")
     expect(page).to have_content("Settings updated successfully.")
 end
-
-#TODO - create feature to test moving the default home page and then logging out/in
-#feature "home redirect2" do
-#  let(:actor) { create(:admin) }
-#
-#  around { |ex| with_user_home_subdomain(actor) { ex.run } }
-#
-#  before do
-#  end
-#
-#  scenario "admin changes default home and logs out then logs in" do
-#    puts page.body
-#    login_as(actor, scope: :user)
-#    expect(page).to have_title("Directory")
-#    visit "/admin/settings/community"
-#    select("Reservations", from: "Default landing page")
-#    click_on("Save Settings")
-#    expect(page).to have_content("Settings updated successfully.")
-#    log_out(actor)
-#    login_as(actor, scope: :user)
-#    expect(page).to have_title("Reservations")
-#  end
-#end

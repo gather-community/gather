a27f249bb0870710ea5fca079cfd30bf4d69d494
Tom Smyth <tom@sassafras.coop>
2018-02-27 12:32:16 -0500

    8062: Stopped emails from going to fake users

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,8 +1,9 @@
 class DeviseMailer < Devise::Mailer
   include SubdomainSettable
 
-  def reset_password_instructions(record, token, opts = {})
-    with_community_subdomain(record.community) do
+  def reset_password_instructions(user, token, opts = {})
+    with_community_subdomain(user.community) do
+      return if user.fake?
       super.tap do |mail|
         mail.subject = "Welcome to Gather!"
       end

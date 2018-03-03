f208a0b3663c27597a470f82c113eb6296514efd
Tom Smyth <tom@sassafras.coop>
2018-03-02 20:37:35 -0500

    7827: Added shares to period form

--- app/controllers/work/periods_controller.rb
+++ app/controllers/work/periods_controller.rb
@@ -16,6 +16,12 @@ module Work
       prep_form_vars
     end
 
+    def edit
+      @period = Period.find(params[:id])
+      authorize @period
+      prep_form_vars
+    end
+
     def create
       @period = Period.new(community: current_community)
       @period.assign_attributes(period_params)
@@ -27,6 +33,19 @@ module Work
         set_validation_error_notice(@period)
         prep_form_vars
         render :new
+      end
+    end
+
+    def update
+      @period = Period.find(params[:id])
+      authorize @period
+      if @period.update_attributes(period_params)
+        flash[:success] = "Period updated successfully."
+        redirect_to work_periods_path
+      else
+        set_validation_error_notice(@period)
+        prep_form_vars
+        render :edit
       end
     end
 
@@ -43,6 +62,24 @@ module Work
     end
 
     def prep_form_vars
+      build_users_by_kind_table
+      build_and_populate_shares_by_user_table
+    end
+
+    def users
+      @users ||= policy_scope(User).active.by_name.in_community(current_community).decorate.tap do |users|
+        users.reject! { |c| c.age.try(:<, Share::MIN_AGE) }
+      end
+    end
+
+    def build_users_by_kind_table
+      @users_by_kind = users.group_by(&:kind)
+    end
+
+    def build_and_populate_shares_by_user_table
+      shares = policy_scope(Share).for_period(@period)
+      @shares_by_user = shares.index_by(&:user_id)
+      users.each { |u| @shares_by_user[u.id] ||= Share.new(user: u, period: @period, portion: nil) }
     end
 
     # Pundit built-in helper doesn't work due to namespacing

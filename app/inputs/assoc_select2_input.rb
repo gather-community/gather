bea368af2f932cf8247d8584f4252d4b729f16b7
Tom Smyth <tom@sassafras.coop>
2018-03-20 22:03:16 -0400

    8123: Added ability to clear

--- app/inputs/assoc_select2_input.rb
+++ app/inputs/assoc_select2_input.rb
@@ -1,15 +1,29 @@
 class AssocSelect2Input < SimpleForm::Inputs::CollectionSelectInput
   def input(wrapper_options)
-    assoc_name = attribute_name.to_s.sub(/_id\z/, "")
-    assoc_class = object.class.reflect_on_association(assoc_name).klass
+    options[:collection] ||= initial_options
 
-    options[:collection] ||= [object.send(assoc_name)].compact
     input_html_options[:data] ||= {}
     input_html_options[:data][:"select2-src"] = assoc_class.model_name.route_key
     input_html_options[:data][:"select2-label-attr"] = options[:label_method] if options[:label_method]
-    input_html_options[:data][:"select2-prompt"] = I18n.t("select2_prompts.#{assoc_name}")
+    input_html_options[:data][:"select2-prompt"] = I18n.t("select2.prompts.#{assoc_name}")
     input_html_options[:data][:"select2-context"] = options.delete(:context)
+    input_html_options[:data][:"select2-placeholder"] = I18n.t("select2.placeholders.#{assoc_name}")
+    input_html_options[:data][:"select2-allow-clear"] = options.delete(:allow_clear)
 
     super(wrapper_options)
+  end
+
+  private
+
+  def assoc_name
+    @assoc_name ||= attribute_name.to_s.sub(/_id\z/, "")
+  end
+
+  def assoc_class
+    @assoc_class ||= object.class.reflect_on_association(assoc_name).klass
+  end
+
+  def initial_options
+    [object.send(assoc_name)].compact
   end
 end
--- app/models/work/shift.rb
+++ app/models/work/shift.rb
@@ -132,7 +132,7 @@ module Work
         self.slots = UNLIMITED_SLOTS
       else
         # Remove any extra assignments beyond the permitted slots.
-        assignments.delete(*assignments.reject(&:marked_for_destruction)[slots..-1])
+        assignments.delete(*assignments.reject(&:marked_for_destruction?)[slots..-1])
       end
 
       if job_full_period?

34053e928d73f4ffe45f0161bdf56676e96d5fe0
Tom Smyth <tom@sassafras.coop>
2018-03-27 16:42:44 -0500

    8180: Added Redis to ping page

---
+++ app/models/system_status.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# Checks system status for use in the ping page.
+class SystemStatus
+  def ok?
+    delayed_job_up? && redis_up?
+  end
+
+  def delayed_job_up?
+    return @delayed_job_up if defined?(@delayed_job_up)
+    @delayed_job_up =
+      begin
+        Process.kill(0, delayed_job_pid) && true
+      rescue Errno::ESRCH
+        false
+      end
+  end
+
+  def redis_up?
+    return @redis_up if defined?(@redis_up)
+    @redis_up =
+      begin
+        Rails.cache.stats && true
+      rescue Redis::CannotConnectError
+        false
+      end
+  end
+
+  private
+
+  def delayed_job_pid
+    File.read(Rails.root.join("tmp", "pids", "delayed_job.pid")).to_i
+  rescue Errno::ENOENT
+    nil
+  end
+end

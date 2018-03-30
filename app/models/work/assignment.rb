020e84915ed032249632591cd8c75b8eb083f45e
Tom Smyth <tom@sassafras.coop>
2018-03-30 07:32:16 -0500

    Added touch: true to assignment shift association

--- app/models/work/assignment.rb
+++ app/models/work/assignment.rb
@@ -5,7 +5,7 @@ module Work
   class Assignment < ApplicationRecord
     acts_as_tenant :cluster
 
-    belongs_to :shift, inverse_of: :assignments, counter_cache: true
+    belongs_to :shift, inverse_of: :assignments, counter_cache: true, touch: true
     belongs_to :user
 
     scope :for_community, ->(c) { joins(shift: {job: :period}).where("work_periods.community_id": c.id) }

67d1709639495ebe64c823db4e987dde1ab2ee5a
dtdimond <dtdimond@gmail.com>
2018-04-10 21:30:35 -0500

    set nav context

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,4 +1,7 @@
 class RolesController < ApplicationController
+
+  before_action -> { nav_context(:people, :roles) }, except: :accounts
+
   def index
     authorize Role
     @roles = policy_scope(Role)

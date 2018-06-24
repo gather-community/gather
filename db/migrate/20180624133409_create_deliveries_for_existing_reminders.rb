71525cc9bc9840d10ce09d7cdd343819d0e69372
Tom Smyth <tom@sassafras.coop>
2018-06-24 09:45:21 -0400

    7848: Added migration to create deliveries for existing reminders

---
+++ db/migrate/20180624133409_create_deliveries_for_existing_reminders.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateDeliveriesForExistingReminders < ActiveRecord::Migration[5.1]
+  def up
+    Cluster.all.each do |cluster|
+      ActsAsTenant.with_tenant(cluster) do
+        Work::Reminder.all.each(&:create_or_update_deliveries)
+      end
+    end
+    execute("UPDATE work_reminder_deliveries SET delivered ='t' WHERE deliver_at < NOW()")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180624032649) do
+ActiveRecord::Schema.define(version: 20180624133409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

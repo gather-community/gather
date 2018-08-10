e8d3d6193365380ca452b0c953a0e7271bfd6724
Tom Smyth <tom@sassafras.coop>
2018-08-10 15:52:31 -0400

    8736: Use staggering info in decorator if present

--- app/decorators/work/synopsis_decorator.rb
+++ app/decorators/work/synopsis_decorator.rb
@@ -15,11 +15,15 @@ module Work
         chunks = data[who].map { |i| chunk_for_item(i) }
         safe_str << h.t("work.synopsis.#{who}") << " " << join_chunks(chunks)
       end
-      sentences << (data[:done] ? h.content_tag(:i, h.t("work.synopsis.done")) : nil)
+      sentences << (data[:done] ? youre_all_set : staggering_plan)
       @to_s = h.safe_join(sentences.compact, " ")
     end
 
     private
+
+    def youre_all_set
+      h.content_tag(:i, h.t("work.synopsis.done"))
+    end
 
     def chunk_for_item(item)
       got = round_next_half(item[:got])
@@ -37,6 +41,31 @@ module Work
 
     def item_count
       @item_count ||= data[:self].size == 1 ? :single : :multiple
+    end
+
+    def staggering_plan
+      return unless staggering.present? && staggering[:prev_limit].present?
+      if staggering[:prev_limit].zero?
+        h.t("work.synopsis.start_choosing", at_or_on_time: next_round_time)
+      elsif staggering[:next_limit].nil?
+        h.t("work.synopsis.round_limit_until",
+          prev: staggering[:prev_limit], until_time: next_round_time(preposition: "until"))
+      else # prev_limit and next_limit both present
+        h.t("work.synopsis.round_limit_with_next",
+          prev: staggering[:prev_limit], next: staggering[:next_limit], at_or_on_time: next_round_time)
+      end
+    end
+
+    def staggering
+      data[:staggering]
+    end
+
+    def next_round_time(preposition: nil)
+      return if staggering[:next_starts_at].nil?
+      preposition ||= staggering[:next_starts_at].today? ? "at" : "on"
+      format = preposition == "on" ? :datetime_no_yr : :regular_time
+      time = h.l(staggering[:next_starts_at], format: format)
+      h.t("common.time_with_preposition.#{preposition}", t: time).strip.gsub("  ", " ")
     end
   end
 end
--- spec/decorators/work/synopsis_decorator_spec.rb
+++ spec/decorators/work/synopsis_decorator_spec.rb
@@ -3,30 +3,30 @@
 require "rails_helper"
 
 describe Work::SynopsisDecorator do
-  let(:builder) { described_class.new(double(summary: summary)) }
+  let(:synopsis) { described_class.new(double(data: data)) }
   let(:regular) { OpenStruct.new(title: "regular") }
-  subject { builder.to_s }
+  subject { synopsis.to_s }
 
   context "by_user quota" do
     context "no full community jobs" do
       context "zero signups" do
-        let(:summary) { {self: [{bucket: regular, got: 0, ttl: 28.43, ok: false}], done: false} }
+        let(:data) { {self: [{bucket: regular, got: 0, ttl: 28.43, ok: false}], done: false} }
         it do
-          is_expected.to eq "You have signed up for <b>0/28.5</b> hours."
+          is_expected.to eq("You have signed up for <b>0/28.5</b> hours.")
         end
       end
 
       context "partially complete" do
-        let(:summary) { {self: [{bucket: regular, got: 9.0, ttl: 28.43, ok: false}], done: false} }
+        let(:data) { {self: [{bucket: regular, got: 9.0, ttl: 28.43, ok: false}], done: false} }
         it do
-          is_expected.to eq "You have signed up for <b>9/28.5</b> hours."
+          is_expected.to eq("You have signed up for <b>9/28.5</b> hours.")
         end
       end
 
       context "complete" do
-        let(:summary) { {self: [{bucket: regular, got: 29, ttl: 28.43, ok: true}], done: true} }
+        let(:data) { {self: [{bucket: regular, got: 29, ttl: 28.43, ok: true}], done: true} }
         it do
-          is_expected.to eq "You have signed up for <i>29/28.5</i> hours. <i>You&#39;re all set!</i>"
+          is_expected.to eq("You have signed up for <i>29/28.5</i> hours. <i>You&#39;re all set!</i>")
         end
       end
     end
@@ -35,38 +35,38 @@ describe Work::SynopsisDecorator do
       let(:job) { create(:work_job, title: "Foo Bar", slot_type: "full_multiple") }
 
       context "zero signups" do
-        let(:summary) do
+        let(:data) do
           {self: [{bucket: regular, got: 0, ttl: 28.43, ok: false},
                   {bucket: job, got: 0, ttl: 6, ok: false}],
            done: false}
         end
         it do
-          is_expected.to eq "You have signed up for <b>0/28.5</b> regular hours "\
-            "and <b>0/6</b> Foo Bar hours."
+          is_expected.to eq("You have signed up for <b>0/28.5</b> regular hours "\
+            "and <b>0/6</b> Foo Bar hours.")
         end
       end
 
       context "partially complete" do
-        let(:summary) do
+        let(:data) do
           {self: [{bucket: regular, got: 28.21, ttl: 28.43, ok: true},
                   {bucket: job, got: 2, ttl: 6, ok: false}],
            done: false}
         end
         it do
-          is_expected.to eq "You have signed up for <i>28.5/28.5</i> regular hours "\
-            "and <b>2/6</b> Foo Bar hours."
+          is_expected.to eq("You have signed up for <i>28.5/28.5</i> regular hours "\
+            "and <b>2/6</b> Foo Bar hours.")
         end
       end
 
       context "complete" do
-        let(:summary) do
+        let(:data) do
           {self: [{bucket: regular, got: 28.21, ttl: 28.43, ok: true},
                   {bucket: job, got: 6, ttl: 6, ok: true}],
            done: true}
         end
         it do
-          is_expected.to eq "You have signed up for <i>28.5/28.5</i> regular hours "\
-            "and <i>6/6</i> Foo Bar hours. <i>You&#39;re all set!</i>"
+          is_expected.to eq("You have signed up for <i>28.5/28.5</i> regular hours "\
+            "and <i>6/6</i> Foo Bar hours. <i>You&#39;re all set!</i>")
         end
       end
     end
@@ -76,7 +76,7 @@ describe Work::SynopsisDecorator do
       let(:job2) { create(:work_job, title: "Ba. Qux", slot_type: "full_single") }
 
       context "zero signups" do
-        let(:summary) do
+        let(:data) do
           {self: [
             {bucket: regular, got: 0, ttl: 28.43, ok: false},
             {bucket: job1, got: 3, ttl: 6, ok: false},
@@ -84,8 +84,8 @@ describe Work::SynopsisDecorator do
           ], done: false}
         end
         it do
-          is_expected.to eq "You have signed up for <b>0/28.5</b> regular hours, "\
-            "<b>3/6</b> Foo Bar hours, and <i>8/8</i> Ba. Qux hours."
+          is_expected.to eq("You have signed up for <b>0/28.5</b> regular hours, "\
+            "<b>3/6</b> Foo Bar hours, and <i>8/8</i> Ba. Qux hours.")
         end
       end
     end
@@ -95,7 +95,7 @@ describe Work::SynopsisDecorator do
     let(:job) { create(:work_job, title: "Foo Bar", slot_type: "full_multiple") }
 
     context "zero signups" do
-      let(:summary) do
+      let(:data) do
         {self:      [{bucket: regular, got: 0, ttl: 28.43, ok: false},
                      {bucket: job, got: 0, ttl: 6, ok: false}],
          household: [{bucket: regular, got: 0, ttl: 56.86, ok: false},
@@ -103,14 +103,14 @@ describe Work::SynopsisDecorator do
          done:      false}
       end
       it do
-        is_expected.to eq "You have signed up for <b>0/28.5</b> regular hours "\
+        is_expected.to eq("You have signed up for <b>0/28.5</b> regular hours "\
           "and <b>0/6</b> Foo Bar hours. Your household has <b>0/57</b> regular hours "\
-          "and <b>0/12</b> Foo Bar hours."
+          "and <b>0/12</b> Foo Bar hours.")
       end
     end
 
     context "partially complete" do
-      let(:summary) do
+      let(:data) do
         {self:      [{bucket: regular, got: 4.2, ttl: 28.43, ok: false},
                      {bucket: job, got: 6, ttl: 6, ok: true}],
          household: [{bucket: regular, got: 4.2, ttl: 56.86, ok: false},
@@ -118,14 +118,14 @@ describe Work::SynopsisDecorator do
          done:      false}
       end
       it do
-        is_expected.to eq "You have signed up for <b>4.5/28.5</b> regular hours "\
+        is_expected.to eq("You have signed up for <b>4.5/28.5</b> regular hours "\
           "and <i>6/6</i> Foo Bar hours. Your household has <b>4.5/57</b> regular hours "\
-          "and <b>6/12</b> Foo Bar hours."
+          "and <b>6/12</b> Foo Bar hours.")
       end
     end
 
     context "household complete even though user not" do
-      let(:summary) do
+      let(:data) do
         {self:      [{bucket: regular, got: 25, ttl: 28.43, ok: true},
                      {bucket: job, got: 6, ttl: 6, ok: true}],
          household: [{bucket: regular, got: 58, ttl: 56.86, ok: true},
@@ -133,9 +133,72 @@ describe Work::SynopsisDecorator do
          done:      true}
       end
       it do
-        is_expected.to eq "You have signed up for <i>25/28.5</i> regular hours "\
+        is_expected.to eq("You have signed up for <i>25/28.5</i> regular hours "\
           "and <i>6/6</i> Foo Bar hours. Your household has <i>58/57</i> regular hours "\
-          "and <i>12/12</i> Foo Bar hours. <i>You&#39;re all set!</i>"
+          "and <i>12/12</i> Foo Bar hours. <i>You&#39;re all set!</i>")
+      end
+    end
+  end
+
+  context "with staggering" do
+    let(:buckets) { [{bucket: regular, got: 0, ttl: 28.43, ok: false}] }
+    let(:done) { false }
+    let(:data) { {self: buckets, done: done, staggering: staggering} }
+    let(:next_starts_at) { Time.zone.parse("2018-08-15 19:30") }
+    let(:now) { next_starts_at - 2.hours }
+
+    around do |example|
+      Timecop.freeze(now) { example.run }
+    end
+
+    context "with next_limit and zero prev_limit" do
+      let(:staggering) { {prev_limit: 0, next_limit: 5, next_starts_at: next_starts_at} }
+
+      context "on choosing day" do
+        it "gives time only " do
+          is_expected.to eq("You have signed up for <b>0/28.5</b> hours. "\
+            "You can start choosing jobs at 7:30pm.")
+        end
+      end
+
+      context "on other day" do
+        let(:now) { next_starts_at - 2.days }
+        it "gives date and time" do
+          is_expected.to eq("You have signed up for <b>0/28.5</b> hours. "\
+            "You can start choosing jobs on Wed Aug 15 7:30pm.")
+        end
+      end
+
+      context "if already set" do
+        let(:buckets) { [{bucket: regular, got: 30, ttl: 28.43, ok: true}] }
+        let(:done) { true }
+
+        it "doesn't add staggering info" do
+          is_expected.to eq("You have signed up for <i>30/28.5</i> hours. <i>You&#39;re all set!</i>")
+        end
+      end
+    end
+
+    context "with next_limit and prev_limit" do
+      let(:staggering) { {prev_limit: 15, next_limit: 20, next_starts_at: next_starts_at} }
+      it do
+        is_expected.to eq("You have signed up for <b>0/28.5</b> hours. "\
+          "Your round limit is 15 hours and will rise to 20 at 7:30pm.")
+      end
+    end
+
+    context "with prev_limit only" do
+      let(:staggering) { {prev_limit: 15, next_limit: nil, next_starts_at: next_starts_at} }
+      it do
+        is_expected.to eq("You have signed up for <b>0/28.5</b> hours. "\
+          "Your round limit is 15 hours until 7:30pm.")
+      end
+    end
+
+    context "with no limit" do
+      let(:staggering) { {prev_limit: nil, next_limit: nil, next_starts_at: nil} }
+      it do
+        is_expected.to eq("You have signed up for <b>0/28.5</b> hours.")
       end
     end
   end

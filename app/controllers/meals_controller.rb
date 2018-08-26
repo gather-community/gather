28f81524e8e590c4f3c760d9549193c7cbc7f026
Tom Smyth <tom@sassafras.coop>
2018-08-26 13:11:27 -0400

    7335: Remove old finalize code

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -105,39 +105,6 @@ class MealsController < ApplicationController
     redirect_to(meals_path)
   end
 
-  def finalize
-    @meal = Meal.find(params[:id])
-    authorize @meal
-    @meal.build_cost
-    @dupes = []
-  end
-
-  def do_finalize
-    @meal = Meal.find(params[:id])
-    authorize @meal, :finalize?
-
-    # We assign finalized here so that the meal/signup validations don't complain about no spots left.
-    @meal.assign_attributes(finalize_params.merge(status: "finalized"))
-
-    if (@dupes = @meal.duplicate_signups).any?
-      flash.now[:error] = "There are duplicate signups. "\
-        "Please correct by adding numbers for each diner type."
-      render(:finalize)
-    elsif @meal.valid?
-      # Run the save and signup in a transaction in case the finalize operation fails.
-      # Save the meal first so that any signups marked for deletion are deleted.
-      Meal.transaction do
-        @meal.save!
-        Meals::Finalizer.new(@meal).finalize!
-      end
-      flash[:success] = "Meal finalized successfully"
-      redirect_to(meals_path(finalizable: 1))
-    else
-      set_validation_error_notice(@meal)
-      render(:finalize)
-    end
-  end
-
   def reopen
     @meal = Meal.find(params[:id])
     authorize @meal

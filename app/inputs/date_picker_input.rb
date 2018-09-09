676594c6f04ccb4e8095aaf7bfd35e39a8ff1ff6
Tom Smyth <tom@sassafras.coop>
2018-09-09 08:58:38 -0400

    4561: Fix dirty checking for timepicker

--- app/inputs/date_picker_input.rb
+++ app/inputs/date_picker_input.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DatePickerInput < SimpleForm::Inputs::StringInput
   def input(wrapper_options)
     @dow = options.delete(:include_day_of_week)
@@ -5,61 +7,67 @@ class DatePickerInput < SimpleForm::Inputs::StringInput
     set_html_options
     set_value_html_option
 
-    template.content_tag :div, class: 'input-group date datetimepicker' do
+    template.content_tag :div, class: "input-group date datetimepicker" do
       input = super(wrapper_options) # leave StringInput do the real rendering
       input + input_button
     end
   end
 
   def input_html_classes
-    super.push ''   # 'form-control'
+    super.push "" # 'form-control'
   end
 
   private
 
   def input_button
-    template.content_tag :span, class: 'input-group-btn' do
-      template.content_tag :button, class: 'btn btn-default', type: 'button' do
-        template.content_tag :span, '', class: 'fa fa-calendar'
+    template.content_tag :span, class: "input-group-btn" do
+      template.content_tag :button, class: "btn btn-default", type: "button" do
+        template.content_tag :span, "", class: "fa fa-calendar"
       end
     end
   end
 
   def set_html_options
-    input_html_options[:type] = 'text'
+    input_html_options[:type] = "text"
     input_html_options[:data] ||= {}
     input_html_options[:data].merge!(
       date_options: date_options,
-      initial_value: value.try(:iso8601)
+      initial_value: initial_value
     )
   end
 
+  def initial_value
+    # This value is used for dirty checking.
+    # The moment library requires a standardized format.
+    value&.iso8601
+  end
+
   def set_value_html_option
-    return unless value.present?
+    return if value.blank?
     input_html_options[:value] ||= I18n.localize(value, format: display_pattern)
   end
 
   def value
-    object.send(attribute_name) if object.respond_to? attribute_name
+    object.send(attribute_name) if object.respond_to?(attribute_name)
   end
 
   def display_pattern
-    (@dow ? "%a " : "") << I18n.t('datepicker.dformat', default: '%d/%m/%Y')
+    (@dow ? "%a " : "") << I18n.t("datepicker.dformat", default: "%d/%m/%Y")
   end
 
   def picker_pattern
-    (@dow ? "ddd " : "") << I18n.t('datepicker.pformat', default: 'DD/MM/YYYY')
+    (@dow ? "ddd " : "") << I18n.t("datepicker.pformat", default: "DD/MM/YYYY")
   end
 
   def date_view_header_format
-    I18n.t('dayViewHeaderFormat', default: 'MMMM YYYY')
+    I18n.t("dayViewHeaderFormat", default: "MMMM YYYY")
   end
 
   def date_options_base
     {
-        locale: I18n.locale.to_s,
-        format: picker_pattern,
-        dayViewHeaderFormat: date_view_header_format
+      locale: I18n.locale.to_s,
+      format: picker_pattern,
+      dayViewHeaderFormat: date_view_header_format
     }
   end
 
@@ -67,5 +75,4 @@ class DatePickerInput < SimpleForm::Inputs::StringInput
     custom_options = input_html_options[:data][:date_options] || {}
     date_options_base.merge!(custom_options)
   end
-
 end
--- app/inputs/datetime_picker_input.rb
+++ app/inputs/datetime_picker_input.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class DatetimePickerInput < DatePickerInput
   private
 
   def display_pattern
-    super << ' ' << I18n.t('timepicker.dformat', default: '%R')
+    super << " " << I18n.t("timepicker.dformat", default: "%R")
   end
 
   def picker_pattern
-    super << ' ' << I18n.t('timepicker.pformat', default: 'HH:mm')
+    super << " " << I18n.t("timepicker.pformat", default: "HH:mm")
   end
 end

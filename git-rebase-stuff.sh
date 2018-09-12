2815e1f9eee401116023b56ec82583700f7f343d
Steve Gaarder <gaarder1@ecovillage.ithaca.ny.us>
2018-09-12 15:05:11 -0400

    resolved conflicts

--- git-rebase-stuff.sh
+++
@@ -1,7 +0,0 @@
-git add spec/policies/user_policy_spec.rb 
-git add spec/models/people/exporter_spec.rb
-git add spec/expectations/users.csv 
-git add db/schema.rb
-git add app/models/people/exporter.rb 
-git rebase --continue
-# git status

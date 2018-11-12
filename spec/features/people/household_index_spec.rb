2f83bb4b96ddc36d2700f513aaa09c07ad26c9ee
Tom Smyth <tom@sassafras.coop>
2018-11-11 19:38:33 -0500

    6597: Combine household index feature specs

--- spec/features/people/household_index_spec.rb
+++ spec/features/people/household_index_spec.rb
@@ -12,16 +12,23 @@ feature "household index", js: true do
   end
 
   let(:actor) { create(:user) }
-  let!(:household1) { create(:household, name: "Alpha", unit_num: 10) }
-  let!(:household2) { create(:household, name: "Bravo", unit_num: 2) }
+  let!(:household1) { create(:household, name: "Alpha", unit_num_and_suffix: "2DEF") }
+  let!(:household2) { create(:household, name: "Bravo", unit_num_and_suffix: "2-ABC") }
+  let!(:household3) { create(:household, name: "Charlie", unit_num_and_suffix: "10 ABC") }
+  let!(:household4) { create(:household, name: "Delta", unit_num_and_suffix: "1") }
 
   scenario "sort lens" do
     visit(households_path)
     expect(page).to have_content("Alpha")
     expect(page).to have_content("Bravo")
+    expect(page).to have_content("2-DEF")
+    expect(page).to have_content("10-ABC")
+    expect(page).to have_content("2-ABC")
     expect(page.body.index("Alpha")).to be < page.body.index("Bravo")
+    expect(page.body.index("Delta")).to be > page.body.index("Charlie")
     select_lens_and_wait(:sort, "By Unit")
     expect(page.body.index("Alpha")).to be > page.body.index("Bravo")
+    expect(page.body.index("Delta")).to be < page.body.index("Charlie")
   end
 
   scenario "search lens" do

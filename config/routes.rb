1dc2c62d7c30579caeba6d130914163ea8016efa
Tom Smyth <tom@sassafras.coop>
2018-11-25 09:52:48 -0500

    4559: Removed sessions controller override

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    omniauth_callbacks: "users/omniauth_callbacks",
-    sessions: "users/sessions"
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   resources :users do

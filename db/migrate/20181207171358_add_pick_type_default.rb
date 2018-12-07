e88ae4b0590c95bc2690c89648d17d37d304fb4e
Tom Smyth <tom@sassafras.coop>
2018-12-07 15:06:24 -0500

    Linting

--- db/migrate/20181207171358_add_pick_type_default.rb
+++ db/migrate/20181207171358_add_pick_type_default.rb
@@ -2,7 +2,7 @@
 
 # Helps for now while form doesn't have these fields, fine to keep it after then too.
 class AddPickTypeDefault < ActiveRecord::Migration[5.1]
-  def change
+  def up
     change_column_default(:work_periods, :pick_type, "free_for_all")
   end
 end

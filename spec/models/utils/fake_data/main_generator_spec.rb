820f1e58e0f98fddbffb13ca7b93472f644a837f
Tom Smyth <tom@sassafras.coop>
2018-12-21 09:44:15 -0500

    9144: Linting

--- spec/models/utils/fake_data/main_generator_spec.rb
+++ spec/models/utils/fake_data/main_generator_spec.rb
@@ -8,14 +8,14 @@ describe Utils::FakeData::MainGenerator do
   let(:cluster) { community.cluster }
 
   before do
-    FileUtils.rm_rf(Rails.root.join("public/system/test"))
+    FileUtils.rm_rf(Rails.root.join("public", "system", "test"))
   end
 
   it "should run and destroy cleanly" do
     described_class.new(community: community, photos: true).generate
 
     expect(User.count).to be > 10
-    expect(Dir[Rails.root.join("public/system/test/**/*.jpg")].size).to be > 10
+    expect(Dir[Rails.root.join("public", "system", "test", "**", "*.jpg")].size).to be > 10
 
     Utils::DataRemover.new(cluster.id).remove
     community.destroy
@@ -24,6 +24,6 @@ describe Utils::FakeData::MainGenerator do
       expect(klass.count).to eq(0), "Expected to find no #{klass.name.pluralize}"
     end
 
-    expect(Dir[Rails.root.join("public/system/test/**/*.*")].size).to eq(0)
+    expect(Dir[Rails.root.join("public", "system", "test", "**", "*.*")].size).to eq(0)
   end
 end

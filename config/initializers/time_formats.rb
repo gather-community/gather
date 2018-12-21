d2ee8785add920263b7b710cac4bfe3a9907796d
Tom Smyth <tom@sassafras.coop>
2018-12-21 11:43:58 -0500

    9219: Remove zone from serialized times for calendars

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -2,3 +2,4 @@
 
 # Only language time formats that should not differ by locale, such as machine-readable formats
 # should be in this file.
+Time::DATE_FORMATS[:iso8601_no_zone] = "%Y-%m-%dT%H:%M:%S"

43bd8e3675837da9fe3e2c1530a1f99471baf34d
Tom Smyth <tom@sassafras.coop>
2019-01-09 08:50:01 -0500

    9188: Added form

--- app/controllers/work/job_templates_controller.rb
+++ app/controllers/work/job_templates_controller.rb
@@ -15,6 +15,42 @@ module Work
       @templates = policy_scope(JobTemplate).in_community(current_community).by_title
     end
 
+    def new
+      @template = JobTemplate.new
+      authorize(@template)
+      prep_form_vars
+    end
+
+    def edit
+      @template = JobTemplate.find(params[:id])
+      authorize(@template)
+      prep_form_vars
+    end
+
+    def create
+      @template = JobTemplate.new(job_template_params.merge(community_id: current_community))
+      authorize(@template)
+      if @template.save
+        flash[:success] = "Template created successfully."
+        redirect_to(work_job_templates_path)
+      else
+        prep_form_vars
+        render(:new)
+      end
+    end
+
+    def update
+      @template = JobTemplate.find(params[:id])
+      authorize(@template)
+      if @template.update(job_template_params)
+        flash[:success] = "Template updated successfully."
+        redirect_to(work_job_templates_path)
+      else
+        prep_form_vars
+        render(:edit)
+      end
+    end
+
     protected
 
     def klass
@@ -22,6 +58,10 @@ module Work
     end
 
     private
+
+    def prep_form_vars
+      @requesters = People::Group.in_community(current_community).by_name
+    end
 
     def sample_template
       JobTemplate.new(community: current_community)

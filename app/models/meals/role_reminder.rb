967c4c8ce0a227189ff64f09969eec408e637b10
Tom Smyth <tom@sassafras.coop>
2019-01-13 10:44:24 -0500

    9188: Add normalization and validation for role reminders

--- app/models/meals/role_reminder.rb
+++ app/models/meals/role_reminder.rb
@@ -7,5 +7,9 @@ module Meals
     REL_UNIT_SIGN_OPTIONS = %i[days_before days_after hours_before hours_after].freeze
 
     belongs_to :role, class_name: "Meals::Role", inverse_of: :reminders
+
+    validates :rel_magnitude, presence: true
+
+    normalize_attributes :note
   end
 end

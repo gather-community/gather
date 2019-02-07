e9bb46db59f70fbb8ab596b70352377e4e3c2c8e
Tom Smyth <tom@sassafras.coop>
2019-02-07 09:09:01 -0500

    9188: Refactor and add to job reminder delivery spec

--- spec/models/work/job_reminder_delivery_spec.rb
+++ spec/models/work/job_reminder_delivery_spec.rb
@@ -87,28 +87,49 @@ describe Work::JobReminderDelivery do
   describe "maintenance" do
     let(:job) { create(:work_job, shift_count: 2, hours: 2) }
     let!(:reminder) { create_work_job_reminder(job, 1, "hours_after") }
-    subject(:deliveries) { Work::ReminderDelivery.all.to_a }
+    let(:shift1) { job.shifts[0] }
+    let(:shift1_delivery) { deliveries.detect { |d| d.shift == shift1 } }
+    subject(:deliveries) { Work::JobReminderDelivery.all.to_a }
 
     before do
       # Ensure job knows about its reminders.
       job.reload
     end
 
-    it "creates deliveries on new shift additions" do
-      job.shifts << build(:work_shift, job: job, starts_at: Time.zone.now + 7.days, hours: 2)
-      job.shifts << build(:work_shift, job: job, starts_at: Time.zone.now + 8.days, hours: 2)
-      job.save!
-      expect(deliveries.size).to eq(4)
-      expect(deliveries.map(&:shift)).to match_array(job.shifts)
-      expect(deliveries.map(&:reminder)).to match_array([reminder] * 4)
+    context "on new shift additions" do
+      before do
+        job.shifts << build(:work_shift, job: job, starts_at: Time.zone.now + 7.days, hours: 2)
+        job.shifts << build(:work_shift, job: job, starts_at: Time.zone.now + 8.days, hours: 2)
+        job.save!
+      end
+
+      it "creates deliveries" do
+        expect(deliveries.size).to eq(4)
+        expect(deliveries.map(&:shift)).to match_array(job.shifts)
+        expect(deliveries.map(&:reminder)).to match_array([reminder] * 4)
+      end
     end
 
-    it "updates deliveries on shift changes" do
-      shift = job.shifts.first
-      shift.update!(starts_at: shift.starts_at + 1.hour, ends_at: shift.ends_at + 1.hour)
-      expect(deliveries.size).to eq(2)
-      delivery = deliveries.index_by(&:shift)[shift]
-      expect(delivery.deliver_at).to eq(shift.reload.starts_at + 1.hour)
+    context "on shift changes" do
+      before do
+        shift1.update!(starts_at: shift1.starts_at + 1.hour, ends_at: shift1.ends_at + 1.hour)
+      end
+
+      it "updates deliveries" do
+        expect(deliveries.size).to eq(2)
+        expect(shift1_delivery.deliver_at).to eq(shift1.reload.starts_at + 1.hour)
+      end
+    end
+
+    context "on reminder changes" do
+      before do
+        reminder.update!(rel_unit_sign: "hours_before")
+      end
+
+      it "updates deliveries" do
+        expect(deliveries.size).to eq(2)
+        expect(shift1_delivery.deliver_at).to eq(shift1.starts_at - 1.hour)
+      end
     end
   end
 end

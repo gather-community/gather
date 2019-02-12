a7b7c40a62b04029981a8b768293c04ea21accbd
Tom Smyth <tom@sassafras.coop>
2019-02-12 09:03:31 -0500

    9188: Hide reminder fields for meal role jobs

--- app/models/work/job.rb
+++ app/models/work/job.rb
@@ -10,6 +10,7 @@ module Work
 
     belongs_to :period, class_name: "Work::Period", inverse_of: :jobs
     belongs_to :requester, class_name: "People::Group"
+    belongs_to :meal_role, class_name: "Meals::Role"
 
     has_many :shifts, -> { by_time }, class_name: "Work::Shift", inverse_of: :job, dependent: :destroy
     has_many :reminders, -> { canonical_order }, class_name: "Work::JobReminder", inverse_of: :job,

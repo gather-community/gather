045ce622814a73c3b09894a2f9d13dcc99bdf1db
Tom Smyth <tom@sassafras.coop>
2019-03-07 22:01:18 -0500

    3159: Add option for paypal email

--- app/decorators/billing/account_decorator.rb
+++ app/decorators/billing/account_decorator.rb
@@ -21,10 +21,6 @@ module Billing
       end
     end
 
-    def payment_settings
-      community.settings.billing.payment_methods
-    end
-
     def billing_contact
       community.settings.billing.contact
     end
@@ -36,31 +32,6 @@ module Billing
       respond_to?(link_method) ? send(link_method, image) : image
     end
 
-    def pay_with_paypal?
-      payment_settings.paypal_me
-    end
-
-    def pay_with_check?
-      payment_settings.check_payee || payment_settings.check_address || payment_settings.check_dropoff
-    end
-
-    def pay_with_cash?
-      payment_settings.cash_dropoff
-    end
-
-    def pay_with_online_bill_pay?
-      payment_settings.check_payee && payment_settings.check_address
-    end
-
-    def no_payment_instructions?
-      payment_settings.paypal_me.nil? &&
-        payment_settings.check_payee.nil? &&
-        payment_settings.check_address.nil? &&
-        payment_settings.check_dropoff.nil? &&
-        payment_settings.cash_dropoff.nil? &&
-        payment_settings.additional_info.nil?
-    end
-
     def number_padded
       @number_padded ||= id.to_s.rjust(6, "0")
     end
@@ -70,13 +41,6 @@ module Billing
         ActionLink.new(object, :edit, icon: "pencil", path: h.edit_account_path(object)),
         ActionLink.new(object, :add_txn, icon: "plus", path: h.new_account_transaction_path(object))
       )
-    end
-
-    private
-
-    def paypal_link(image)
-      link_to(image, "#{methods.paypal_me}/#{number_with_precision(account.balance_due, precision: 2)}",
-        data: {confirm: "Please **do not** select 'Paying for goods or a service?' if prompted."})
     end
   end
 end

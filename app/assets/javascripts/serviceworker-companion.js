cbc61456ddb9d24c871aecba39f20a14cf535fae
Tom Smyth <tom@sassafras.coop>
2019-03-12 21:15:38 -0400

    9430: Tweak serviceworker root

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,5 +1,5 @@
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+  navigator.serviceWorker.register('/serviceworker.js', { scope: '/' })
     .then(function(reg) {
       console.log('[Companion]', 'Service worker registered!');
     });

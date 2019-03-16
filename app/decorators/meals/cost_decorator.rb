a1851997fd9a363a28f7a51e5b0e0921c46aa47f
Tom Smyth <tom@sassafras.coop>
2019-03-16 14:34:40 -0400

    9381: Use Cost object if present, else use calculator

--- app/decorators/meals/cost_decorator.rb
+++ app/decorators/meals/cost_decorator.rb
@@ -4,13 +4,11 @@ module Meals
   class CostDecorator < ApplicationDecorator
     delegate_all
 
-    (Signup::SIGNUP_TYPES + %i[ingredient_cost pantry_cost]).each do |attrib|
+    %i[ingredient_cost pantry_cost].each do |attrib|
       define_method("#{attrib}_nice") do
         (num = self[attrib]).blank? ? "?" : h.number_to_currency(num)
       end
-    end
 
-    %i[ingredient_cost pantry_cost].each do |attrib|
       define_method("#{attrib}_decimals") do
         (num = self[attrib]).blank? ? nil : h.number_with_precision(num, precision: 2)
       end

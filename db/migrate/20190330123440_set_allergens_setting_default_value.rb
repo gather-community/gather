f609905a21a4ea709d5fb761d85d76ba9e47e75d
Tom Smyth <tom@sassafras.coop>
2019-04-24 08:35:10 -0400

    Fix allergen migration

--- db/migrate/20190330123440_set_allergens_setting_default_value.rb
+++ db/migrate/20190330123440_set_allergens_setting_default_value.rb
@@ -4,6 +4,6 @@ class SetAllergensSettingDefaultValue < ActiveRecord::Migration[5.1]
   def up
     value = "Gluten, Shellfish, Soy, Corn, Dairy, Eggs, Peanuts, Almonds, "\
       "Tree Nuts, Pineapple, Bananas, Tofu, Eggplant"
-    execute(%(UPDATE communities SET settings = settings || '{"meals":{"allergens":"#{value}"}}'))
+    execute(%(UPDATE communities SET settings = jsonb_insert(settings, '{meals, allergens}', '"#{value}"')))
   end
 end

bf58e1322771901379b77b807dc6fa7ff9d05f3d
Tom Smyth <tom@sassafras.coop>
2019-05-06 22:50:28 -0400

    9665: Add to user creation feature specs

--- spec/features/people/user_form_spec.rb
+++ spec/features/people/user_form_spec.rb
@@ -53,9 +53,29 @@ feature "user form", js: true do
       fill_in("Mobile", with: "5556667777")
       click_button("Save")
 
-      expect_success
+      expect(page).to have_alert("User created successfully.")
       expect(page).to have_title(/Foo Barre/)
       expect_photo(/cooper/)
+
+      emails = email_sent_by { process_queued_job }
+      expect(emails).to be_empty
+      expect(User.find_by(email: "foo@example.com")).not_to be_confirmed
+    end
+
+    scenario "new user with invite" do
+      visit(new_user_path)
+      fill_in("First Name", with: "Foo")
+      fill_in("Last Name", with: "Barre")
+      fill_in("Email", with: "foo@example.com")
+      select2("Ginger", from: "#user_household_id")
+      fill_in("Mobile", with: "5556667777")
+      click_on("Save & Invite")
+
+      expect(page).to have_alert("User created and invited successfully.")
+
+      emails = email_sent_by { process_queued_job }
+      expect(emails.map(&:subject)).to eq(["Instructions for Signing in to Gather"])
+      expect(User.find_by(email: "foo@example.com")).not_to be_confirmed
     end
 
     scenario "editing household" do

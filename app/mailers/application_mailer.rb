993a9bd557e3d627461b0e0c709659323348665f
Tom Smyth <tom@sassafras.coop>
2019-05-08 21:45:40 -0400

    9665: Add guard for unconfirmed users when sending emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -11,12 +11,14 @@ class ApplicationMailer < ActionMailer::Base
 
   # Overrides default mail method.
   #
-  # Allows email addresses, users, or households in the to field.
+  # Allows users or households in the `to` field.
   # It is up to the mailer system, not the job system, to figure out how to send mail to a household or user,
   # and whether users have opted out of a given type of mail.
   # Mails to households should be addressed to all the household users, not to each user separately.
   #
   # Also filters out fake users so that emails are not sent to their fake addresses.
+  # Also filters out unconfirmed email addresses for security purposes (note that confirmation/password/
+  # invite emails are sent with the AuthMailer which doesn't inherit from this class).
   #
   # If self.community returns a non-nil value, sets the appropriate subdomain.
   def mail(params)
@@ -35,17 +37,20 @@ class ApplicationMailer < ActionMailer::Base
       if recipient.is_a?(User)
         resolve_user_email(recipient)
       elsif recipient.is_a?(Household)
-        recipient.users.reject(&:fake?).map(&:email)
+        recipient.users.flat_map { |u| resolve_user_email(u, via_household: true) }
       else
-        recipient
+        raise ArgumentError("Invalid recipient type: #{recipient}")
       end
-    end.flatten.compact
+    end.flatten.uniq.compact
   end
 
-  def resolve_user_email(user)
-    if user.fake?
+  def resolve_user_email(user, via_household: false)
+    if user.fake? || !user.confirmed?
       nil
-    elsif user.child? && user.email.blank?
+    elsif user.child? && user.email.blank? && !via_household
+      # We don't map emails to guardians if we're going via household because not all guardians
+      # of children in a household live in that household. So sending an email addressed to household X
+      # to guardians Y and Z of child C, even though Z doesn't live in household X, could be awkward.
       user.guardians.map(&:email)
     else
       user.email

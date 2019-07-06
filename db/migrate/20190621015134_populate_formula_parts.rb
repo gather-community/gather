33aef66942245882ea1b7744bc6c59ae2e8f3c10
Tom Smyth <tom@sassafras.coop>
2019-07-06 09:07:14 -0230

    73: Rework ranks in migration

--- db/migrate/20190621015134_populate_formula_parts.rb
+++ db/migrate/20190621015134_populate_formula_parts.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class PopulateFormulaParts < ActiveRecord::Migration[5.1]
-  TYPES = %i[adult_meat adult_veg big_kid_meat big_kid_veg little_kid_meat little_kid_veg
-             senior_meat senior_veg teen_meat teen_veg].freeze
+  TYPES = %i[adult_meat adult_veg senior_meat senior_veg teen_meat teen_veg
+             big_kid_meat big_kid_veg little_kid_meat little_kid_veg].freeze
 
   def up
     Cluster.all.each do |cluster|

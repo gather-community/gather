012cde759370e607c5fcbd34a71f3bfe40bdf925
Tom Smyth <tom@sassafras.coop>
2019-07-11 08:47:58 -0400

    72: Add ajax lookup for select2

---
+++ app/controllers/meals/types_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Meals
+  class TypesController < ApplicationController
+    # Only AJAX/JSON for now
+    def index
+      authorize(sample_type)
+      types = policy_scope(Meals::Type).in_community(current_community).matching(params[:search]).by_name
+      render(json: types, root: "results")
+    end
+
+    private
+
+    def sample_type
+      Meals::Type.new(community: current_community)
+    end
+  end
+end

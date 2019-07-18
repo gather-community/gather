be20dd044814463d42c458781f432c5b5be22526
Tom Smyth <tom@sassafras.coop>
2019-07-18 08:26:36 -0400

    73: Tidy spec wrappers and add comment

--- spec/support/config/002_cluster_setup.rb
+++ spec/support/config/002_cluster_setup.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.configure do |config|
   # We have to set a default tenant to avoid NoTenantSet errors.
   # We also reset TZ to default in case previous spec changed it.
@@ -6,6 +8,8 @@ RSpec.configure do |config|
     with_tenant(Defaults.cluster) do
       Defaults.community
       example.run
+      # Note that if there is an error in a let! block, this line may never be reached. If errors result,
+      # focus on fixing the let! block.
       Defaults.reset
     end
   end

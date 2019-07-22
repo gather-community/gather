f7961582c065825226127ba8ab824a47951fc8dd
Tom Smyth <tom@sassafras.coop>
2019-07-22 09:24:21 -0400

    73: Add portion size to show page

--- app/decorators/meals/formula_part_decorator.rb
+++ app/decorators/meals/formula_part_decorator.rb
@@ -4,7 +4,7 @@ module Meals
   class FormulaPartDecorator < ApplicationDecorator
     delegate_all
 
-    PORTION_SIZE_SELECT_OPTIONS = {full: 1.0, three_qtr: 0.75, half: 0.5, one_qtr: 0.25}
+    PORTION_SIZE_SELECT_OPTIONS = {full: 1.0, three_qtr: 0.75, half: 0.5, one_qtr: 0.25}.freeze
 
     def share_formatted
       if fixed_meal?
@@ -16,6 +16,11 @@ module Meals
 
     def portion_size_select_options
       PORTION_SIZE_SELECT_OPTIONS.map { |n, p| [I18n.t("meals/formula_parts.portion_options.#{n}"), p.to_s] }
+    end
+
+    def portion_size_short_name
+      key = PORTION_SIZE_SELECT_OPTIONS.each { |k, v| break k if (portion_size - v).abs < 0.01 }
+      I18n.t("meals/formula_parts.portion_short_names.#{key}")
     end
   end
 end

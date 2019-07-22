51d5ab40f1c1281c33012b97d991b79310b764d7
Tom Smyth <tom@sassafras.coop>
2019-07-22 08:52:10 -0400

    72: Add support for nil-non-nil mix to portion count

--- spec/decorators/meals/portion_count_builder_spec.rb
+++ spec/decorators/meals/portion_count_builder_spec.rb
@@ -19,4 +19,42 @@ describe Meals::PortionCountBuilder do
 
     it { is_expected.to match("This meal will require approximately <strong>4</strong> portions.") }
   end
+
+  context "with categores" do
+    let(:formula) do
+      create(:meal_formula, meal_calc_type: "share",
+                            part_shares: [["100%", "Meat", 1], ["75%", "Meat", 0.5], ["100%", "Veg", 1]])
+    end
+    let(:meal) { create(:meal, formula: formula) }
+    let!(:signups) do
+      [
+        create(:meal_signup, meal: meal, diner_counts: [2, 3, 1]),
+        create(:meal_signup, meal: meal, diner_counts: [0, 3, 1])
+      ]
+    end
+
+    it do
+      is_expected.to match("This meal will require approximately: <strong>5 Meat</strong> portions, "\
+        "<strong>2 Veg</strong> portions.")
+    end
+  end
+
+  context "with mix of categores and no categories" do
+    let(:formula) do
+      create(:meal_formula, meal_calc_type: "share",
+                            part_shares: [["100%", "Meat", 1], ["75%", nil, 0.5], ["100%", "Veg", 1]])
+    end
+    let(:meal) { create(:meal, formula: formula) }
+    let!(:signups) do
+      [
+        create(:meal_signup, meal: meal, diner_counts: [2, 3, 1]),
+        create(:meal_signup, meal: meal, diner_counts: [0, 3, 1])
+      ]
+    end
+
+    it do
+      is_expected.to match("This meal will require approximately: <strong>2 Meat</strong> portions, "\
+        "<strong>2 Veg</strong> portions, <strong>3 other</strong> portions.")
+    end
+  end
 end

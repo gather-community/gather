43bb45d7c5191a490e84642215d918643edff45e
Tom Smyth <tom@sassafras.coop>
2019-07-27 14:36:57 -0400

    73: Update signups form spec

--- spec/features/meals/signups_form_spec.rb
+++ spec/features/meals/signups_form_spec.rb
@@ -4,7 +4,8 @@ require "rails_helper"
 
 feature "meal signups form", js: true do
   let(:actor) { create(:meals_coordinator) }
-  let!(:meal) { create(:meal, served_at: Time.current + 7.days) }
+  let(:formula) { create(:meal_formula, name: "Fmla A", part_shares: [1, 1, 1, 1]) }
+  let!(:meal) { create(:meal, formula: formula, served_at: Time.current + 7.days) }
   let!(:households) { create_list(:household, 2) }
 
   around do |example|
@@ -19,14 +20,14 @@ feature "meal signups form", js: true do
     # Try to submit duplicate signups
     visit edit_meal_path(meal)
     click_link("Edit Signups")
-    enter_signup(household: households[0], quantities: {"Adult (Meat)": 2, "Adult (Veg)": 3})
-    enter_signup(household: households[0], quantities: {"Teen (Veg)": 4})
+    enter_signup(household: households[0], quantities: {"Fmla A Type 1": 2, "Fmla A Type 2": 3})
+    enter_signup(household: households[0], quantities: {"Fmla A Type 3": 4})
     click_button("Save")
     expect(page).to have_content("Has already been taken")
 
     # Spot check one line
     expect(page).to have_select(all("select[id$=_count]")[2][:id], selected: "4")
-    expect(page).to have_select(all("select[id$=_type_id]")[2][:id], selected: "Teen (Veg)")
+    expect(page).to have_select(all("select[id$=_type_id]")[2][:id], selected: "Fmla A Type 3")
 
     # Fix and submit
     select2(households[1].name, from: all("select[id$=_household_id]")[1])
@@ -35,8 +36,8 @@ feature "meal signups form", js: true do
     # Remove a signup by zeroing out, and change the other one.
     visit edit_meal_path(meal)
     click_link("Edit Signups")
-    enter_signup(index: 0, quantities: {"Adult (Meat)": 0, "Adult (Veg)": 0})
-    enter_signup(index: 1, quantities: {"Teen (Meat)": 6})
+    enter_signup(index: 0, quantities: {"Fmla A Type 1": 0, "Fmla A Type 2": 0})
+    enter_signup(index: 1, quantities: {"Fmla A Type 4": 6})
     click_button("Save")
     expect_success
 
@@ -45,7 +46,7 @@ feature "meal signups form", js: true do
     click_link("Edit Signups")
     expect(all(".meals_meal_signups_signup").size).to eq(1)
     expect(page).to have_select(all("select[id$=_count]")[0][:id], selected: "6")
-    expect(page).to have_select(all("select[id$=_type_id]")[0][:id], selected: "Teen (Meat)")
+    expect(page).to have_select(all("select[id$=_type_id]")[0][:id], selected: "Fmla A Type 4")
   end
 
   def enter_signup(quantities:, household: nil, index: nil)

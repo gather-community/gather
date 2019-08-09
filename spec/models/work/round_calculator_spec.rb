d7ef10ac4c5c2ff84d4045fa388df77bbe6f29f1
Tom Smyth <tom@sassafras.coop>
2019-08-09 17:20:34 -0400

    Fix floating point issue with round calculator

--- spec/models/work/round_calculator_spec.rb
+++ spec/models/work/round_calculator_spec.rb
@@ -7,7 +7,6 @@ describe Work::RoundCalculator do
 
   describe "next_num, prev_limit, next_limit" do
     let(:time) { "18:55" }
-    let(:quota) { 17 }
     let(:priority) { [] }
     let!(:shares) do
       portions.each_with_index.map do |portion, i|
@@ -33,9 +32,9 @@ describe Work::RoundCalculator do
     end
 
     context "with lots of rounds" do
-      # Quota: 17
+      # Quota: 19.3
       # Max rounds per worker: 3
-      # Implied round max: 5.6
+      # Implied round max: 11.12
       # For given worker:
       #   Num rounds: ceil(Need / round max)
       #   Hours per round: Need / Num rounds
@@ -46,29 +45,30 @@ describe Work::RoundCalculator do
       #   2. Remaining need (descending)
       #   3. ID (ascending)
       #   and pick the top N
-      # Idx   Pre  Prtn  Need   1  2  3  4  5  6  7  8  9 10
-      # ----------------------------------------------------
-      #  0     0      1    17  11        5        0
-      #  1     2      1    15  10        5        0
-      #  2     2      1    15  10           5        0
-      #  3     2      1    15     10        5        0
-      #  4     6      1    11      5        0
-      #  5     9      1     8      4           0
-      #  6    10      1     7         3        0
-      #  7     2    0.5   6.5       3.5        0
-      #  8    11      1     6         3           0
-      #  9     3    0.5   5.5            0
-      # 10    20      1    -3                           0  At this point we can let all the
-      # 11    20      1    -3                           0  overachievers pick more jobs if they want.
-      # 12    20      1    -3                           0  There is no need to count workers per round.
-      # 13    21      1    -3                           0
-      # 14    21      1    -3                           0
+      # Idx   Pre  Prtn  Need      1    2    3    4    5    6    7    8    9
+      # --------------------------------------------------------------------
+      #  0     0      1    19.3  12.8            6.4            0
+      #  1     2      1    17.3  11.5            5.8            0
+      #  2     2      1    17.3  11.5                 5.8            0
+      #  3     2      1    17.3       11.5            5.8            0
+      #  4     7      1    12.3        6.2            0
+      #  5     9      1    10.3        5.2                 0
+      #  6    10      1     9.3             4.7            0
+      #  7     2      0.5   7.7             3.8            0
+      #  8    12.5    1     6.8             3.4                 0
+      #  9     3      0.25  1.8                  0
+      # 10    20      1    -0.7                                            0  At this point we can let all the
+      # 11    20      1    -0.7                                            0  overachievers pick more jobs if
+      # 12    20      1    -0.7                                            0  they want. There is no need to
+      # 13    21      1    -1.7                                            0  count workers per round.
+      # 14    21      1    -1.7                                            0
 
       # rubocop:disable Layout/ExtraSpacing
-      let(:pre_assign_totals) { [0, 2, 2, 2, 6, 9, 10,   2, 11,   3, 20, 20, 20, 21, 21] }
-      let(:portions) {          [1, 1, 1, 1, 1, 1,  1, 0.5,  1, 0.5,  1,  1,  1,  1,  1] }
+      let(:pre_assign_totals) { [0, 2, 2, 2, 6, 9, 10,   2, 12.5,    3, 20, 20, 20, 21, 21] }
+      let(:portions) {          [1, 1, 1, 1, 1, 1,  1, 0.5,    1, 0.25,  1,  1,  1,  1,  1] }
       # rubocop:enable Layout/ExtraSpacing
 
+      let(:quota) { 19.3 }
       let(:period) do
         create(:work_period, pick_type: "staggered", quota_type: "by_person", workers_per_round: 3,
                              round_duration: 5, max_rounds_per_worker: 3,
@@ -80,35 +80,35 @@ describe Work::RoundCalculator do
 
         it "has correct round schedule" do
           expect(calculator.rounds).to eq([
-            {starts_at: Time.zone.parse("2018-08-15 19:00"), limit: 6},
-            {starts_at: Time.zone.parse("2018-08-15 19:15"), limit: 12},
+            {starts_at: Time.zone.parse("2018-08-15 19:00"), limit: 7},
+            {starts_at: Time.zone.parse("2018-08-15 19:15"), limit: 13},
             {starts_at: Time.zone.parse("2018-08-15 19:30"), limit: nil}
           ])
         end
 
         context "at time before auto open" do
           let(:time) { "18:55" }
-          it { expect_round(prev_limit: 0, next_limit: 6, next_starts_at: "19:00") }
+          it { expect_round(prev_limit: 0, next_limit: 7, next_starts_at: "19:00") }
         end
 
         context "at time within round 1" do
           let(:time) { "19:01" }
-          it { expect_round(prev_limit: 6, next_limit: 12, next_starts_at: "19:15") }
+          it { expect_round(prev_limit: 7, next_limit: 13, next_starts_at: "19:15") }
         end
 
         context "at time within round 2" do
           let(:time) { "19:06" }
-          it { expect_round(prev_limit: 6, next_limit: 12, next_starts_at: "19:15") }
+          it { expect_round(prev_limit: 7, next_limit: 13, next_starts_at: "19:15") }
         end
 
         context "at time within round 4" do
           let(:time) { "19:16" }
-          it { expect_round(prev_limit: 12, next_limit: nil, next_starts_at: "19:30") }
+          it { expect_round(prev_limit: 13, next_limit: nil, next_starts_at: "19:30") }
         end
 
         context "at time right before round 7" do
           let(:time) { "19:29" }
-          it { expect_round(prev_limit: 12, next_limit: nil, next_starts_at: "19:30") }
+          it { expect_round(prev_limit: 13, next_limit: nil, next_starts_at: "19:30") }
         end
 
         context "at time within round 7" do
@@ -119,32 +119,28 @@ describe Work::RoundCalculator do
 
       context "worker with 2 preassigned hours, one of many, with large ID" do
         let(:target_share) { shares[3] }
-        let(:time) { "18:55" }
 
         before do
           target_share.update!(id: 1e9)
         end
 
-        it "starts in round 2" do
-          expect_round(prev_limit: 0, next_limit: 7, next_starts_at: "19:05")
-        end
-      end
-
-      context "worker with 6 preassigned hours" do
-        let(:target_share) { shares[4] }
-        let(:time) { "18:55" }
-
-        it "starts in round 2" do
-          expect_round(prev_limit: 0, next_limit: 12, next_starts_at: "19:05")
+        it "has correct round schedule" do
+          expect(calculator.rounds).to eq([
+            {starts_at: Time.zone.parse("2018-08-15 19:05"), limit: 8},
+            {starts_at: Time.zone.parse("2018-08-15 19:20"), limit: 14},
+            {starts_at: Time.zone.parse("2018-08-15 19:35"), limit: nil}
+          ])
         end
       end
 
       context "worker with 9 preassigned hours" do
         let(:target_share) { shares[5] }
-        let(:time) { "18:55" }
 
-        it "starts in round 2" do
-          expect_round(prev_limit: 0, next_limit: 13, next_starts_at: "19:05")
+        it "has correct round schedule" do
+          expect(calculator.rounds).to eq([
+            {starts_at: Time.zone.parse("2018-08-15 19:05"), limit: 15},
+            {starts_at: Time.zone.parse("2018-08-15 19:25"), limit: nil}
+          ])
         end
       end
 
@@ -159,23 +155,24 @@ describe Work::RoundCalculator do
         end
       end
 
-      context "worker with 11 preassigned hours" do
+      context "worker with 12.5 preassigned hours" do
         let(:target_share) { shares[8] }
 
         it "has correct round schedule" do
           expect(calculator.rounds).to eq([
-            {starts_at: Time.zone.parse("2018-08-15 19:10"), limit: 14},
+            {starts_at: Time.zone.parse("2018-08-15 19:10"), limit: 16},
             {starts_at: Time.zone.parse("2018-08-15 19:30"), limit: nil}
           ])
         end
       end
 
-      context "worker with 3 preassigned hours, half share" do
+      context "worker with 3 preassigned hours, quarter share" do
         let(:target_share) { shares[9] }
-        let(:time) { "18:55" }
 
-        it "starts in round 4" do
-          expect_round(prev_limit: 0, next_limit: nil, next_starts_at: "19:15")
+        it "has correct round schedule" do
+          expect(calculator.rounds).to eq([
+            {starts_at: Time.zone.parse("2018-08-15 19:15"), limit: nil}
+          ])
         end
       end
 
@@ -191,14 +188,15 @@ describe Work::RoundCalculator do
     end
 
     context "with one round per worker" do
-      # Pre  Prtn  Need   1  2  3  4  5  6  7  8  9 10
+      # Pre  Prtn  Need     1  2  3  4  5  6  7  8  9 10
       # ---------------------------------------------------
-      #   0     1    17   0
-      #   2     1    15   0
-      #   6     1    11      0
-      #   7     1    10      0
+      #   0     1    17.9   0
+      #   2     1    15.9   0
+      #   6     1    11.9      0
+      #   7     1    10.9      0
       let(:pre_assign_totals) { [0, 2, 6, 7] }
       let(:portions) { [1, 1, 1, 1] }
+      let(:quota) { 17.9 }
       let(:period) do
         create(:work_period, pick_type: "staggered", quota_type: "by_person", workers_per_round: 2,
                              round_duration: 5, max_rounds_per_worker: 1,
@@ -237,6 +235,7 @@ describe Work::RoundCalculator do
       let(:pre_assign_totals) { [0, 18, 6, 7, 7] }
       let(:portions) { [0.25, 1, 1, 1, 1] }
       let(:priority) { [true, true, false, false, false] }
+      let(:quota) { 17 }
       let(:period) do
         create(:work_period, pick_type: "staggered", quota_type: "by_person", workers_per_round: 3,
                              round_duration: 5, max_rounds_per_worker: 2,
@@ -306,6 +305,7 @@ describe Work::RoundCalculator do
       #   7     1    10   5  0
       let(:pre_assign_totals) { [0, 2, 6, 7] }
       let(:portions) { [1, 1, 1, 1] }
+      let(:quota) { 17 }
       let(:period) do
         create(:work_period, pick_type: "staggered", quota_type: "by_person", workers_per_round: 100,
                              round_duration: 5, max_rounds_per_worker: 2,

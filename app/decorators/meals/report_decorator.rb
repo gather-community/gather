f9facdfd9772beb1608b753d4d07e48d33298eb9
Tom Smyth <tom@sassafras.coop>
2019-08-14 21:07:15 -0400

    73: Refactor subhead ERB

--- app/decorators/meals/report_decorator.rb
+++ app/decorators/meals/report_decorator.rb
@@ -8,8 +8,9 @@ module Meals
       %w[first last].map { |m| l(range.send(m), format: :month_yr) }.join(" - ")
     end
 
-    def this_cmty_only
-      multi_community? ? ", #{community.name} meals only" : ""
+    def subhead(current_cmty_only: true)
+      cmty_str = h.multi_community? && current_cmty_only ? ", #{community.name} meals only" : ""
+      h.content_tag(:small, range_formatted << cmty_str)
     end
   end
 end

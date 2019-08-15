f85b58ad4c0e5aff82ad074b47af517ea79d221b
Tom Smyth <tom@sassafras.coop>
2019-08-14 21:52:11 -0400

    72: Add more zero handling for signups

--- app/models/meals/signup_part.rb
+++ app/models/meals/signup_part.rb
@@ -8,6 +8,8 @@ module Meals
     belongs_to :type
     belongs_to :signup, inverse_of: :parts
 
+    after_save :destroy_if_zero
+
     delegate :zero?, to: :count
     delegate :name, to: :type, prefix: true
     delegate :household_id, to: :signup
@@ -18,6 +20,12 @@ module Meals
         .joins("LEFT JOIN meal_formula_parts ON meal_formula_parts.formula_id = meals.formula_id
           AND meal_formula_parts.type_id = meal_signup_parts.type_id")
         .order("meal_formula_parts.rank")
+    end
+
+    private
+
+    def destroy_if_zero
+      destroy if zero?
     end
   end
 end

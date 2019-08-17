e0ee3c4321c9326acff8a6ff03fae0445d86c7d7
Tom Smyth <tom@sassafras.coop>
2019-08-17 17:07:35 -0400

    73: Remove old calculator specs

--- spec/models/meals/fixed_cost_calculator_spec.rb
+++ spec/models/meals/fixed_cost_calculator_spec.rb
@@ -4,22 +4,16 @@ require "rails_helper"
 
 describe Meals::FixedCostCalculator do
   let(:formula) do
-    build(:meal_formula,
-      meal_calc_type: "fixed",
-      pantry_calc_type: pantry_calc_type,
-      pantry_fee: pantry_fee,
-      adult_meat: 4,
-      adult_veg: 3,
-      little_kid_veg: 0)
+    create(:meal_formula, meal_calc_type: "fixed", pantry_calc_type: pantry_calc_type,
+                          pantry_fee: pantry_fee, parts_attrs: %w[4 3 0])
   end
+  let(:types) { formula.types }
   let(:meal) { build(:meal, formula: formula) }
   let(:calculator) { Meals::FixedCostCalculator.new(meal) }
 
   before do
     meal.build_cost
-    allow(Meals::Signup).to receive(:totals_for_meal).and_return(
-      "adult_meat" => 9, "adult_veg" => 3, "little_kid_veg" => 2
-    )
+    allow(meal).to receive(:signup_totals).and_return(types[0] => 9, types[1] => 3, types[2] => 2)
   end
 
   context "with fixed pantry_calc_type" do
@@ -28,15 +22,15 @@ describe Meals::FixedCostCalculator do
 
     describe "price_for" do
       it "should be correct" do
-        expect(calculator.price_for("adult_meat")).to eq(4.5)
-        expect(calculator.price_for("adult_veg")).to eq(3.5)
-        expect(calculator.price_for("little_kid_veg")).to eq(0)
+        expect(calculator.price_for(types[0])).to be_within(0.005).of(4.5)
+        expect(calculator.price_for(types[1])).to be_within(0.005).of(3.5)
+        expect(calculator.price_for(types[2])).to be_zero
       end
     end
 
     describe "max_ingredient_cost" do
       it "should be correct" do
-        expect(calculator.max_ingredient_cost).to eq(45)
+        expect(calculator.max_ingredient_cost_zzz).to be_within(0.005).of(45)
       end
     end
   end
@@ -47,9 +41,9 @@ describe Meals::FixedCostCalculator do
 
     describe "price_for" do
       it "should be correct" do
-        expect(calculator.price_for("adult_meat")).to eq(4.4)
-        expect(calculator.price_for("adult_veg")).to eq(3.3)
-        expect(calculator.price_for("little_kid_veg")).to eq(0)
+        expect(calculator.price_for(types[0])).to be_within(0.005).of(4.4)
+        expect(calculator.price_for(types[1])).to be_within(0.005).of(3.3)
+        expect(calculator.price_for(types[2])).to be_zero
       end
     end
   end

040c7c3624d2394454235c77a4c5451042d621be
Tom Smyth <tom@sassafras.coop>
2019-08-17 17:07:35 -0400

    72: Remove old calculator specs

--- spec/models/meals/share_cost_calculator_spec.rb
+++ spec/models/meals/share_cost_calculator_spec.rb
@@ -1,25 +1,20 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 describe Meals::ShareCostCalculator do
-
   let(:formula) do
-    build(:meal_formula,
-      meal_calc_type: "share",
-      pantry_calc_type: pantry_calc_type,
-      pantry_fee: pantry_fee,
-      adult_meat: 1,
-      adult_veg: 0.75,
-      little_kid_veg: 0
-    )
+    create(:meal_formula, meal_calc_type: "share", pantry_calc_type: pantry_calc_type,
+                          pantry_fee: pantry_fee, parts_attrs: %w[100% 75% 0])
   end
+  let(:types) { formula.types }
   let(:meal) { build(:meal, formula: formula) }
   let(:calculator) { Meals::ShareCostCalculator.new(meal) }
 
   before do
     meal.build_cost
-    allow(Meals::Signup).to receive(:totals_for_meal).and_return(
-      "adult_meat" => 9, "adult_veg" => 3, "little_kid_veg" => 2 # 11.25 adult equivalents
-    )
+    # 11.25 adult equivalents
+    allow(meal).to receive(:signup_totals).and_return(types[0] => 9, types[1] => 3, types[2] => 2)
   end
 
   context "with regular meal" do
@@ -31,18 +26,18 @@ describe Meals::ShareCostCalculator do
 
       describe "price_for" do
         it "should be correct" do
-          expect(calculator.price_for("adult_meat")).to be_within(0.005).of(9.39)
-          expect(calculator.price_for("adult_veg")).to be_within(0.005).of(7.17)
-          expect(calculator.price_for("little_kid_veg")).to eq 0
+          expect(calculator.price_for(types[0])).to be_within(0.005).of(9.39)
+          expect(calculator.price_for(types[1])).to be_within(0.005).of(7.17)
+          expect(calculator.price_for(types[2])).to be_zero
         end
       end
 
-      describe "max_ingredient_cost_for_full_price_of" do
+      describe "max_ingredient_cost_for_full_price_of_zzz" do
         it "should be correct" do
-          # 7.00 = (ingredient_cost / adult_equivs) + 0.50
-          # 6.50 * adult_equivs = ingredient_cost
-          expect(calculator.max_ingredient_cost_for_full_price_of(7)).to be_within(0.005).of(73.13)
-          expect(calculator.max_ingredient_cost_for_full_price_of(8)).to be_within(0.005).of(84.38)
+          # 7.00 = (ingredient_cost / full_price) + 0.50
+          # 6.50 * full_price = ingredient_cost
+          expect(calculator.max_ingredient_cost_for_full_price_of_zzz(7)).to be_within(0.005).of(73.13)
+          expect(calculator.max_ingredient_cost_for_full_price_of_zzz(8)).to be_within(0.005).of(84.38)
         end
       end
     end
@@ -53,18 +48,18 @@ describe Meals::ShareCostCalculator do
 
       describe "price_for" do
         it "should be correct" do
-          expect(calculator.price_for("adult_meat")).to be_within(0.005).of(9.78)
-          expect(calculator.price_for("adult_veg")).to be_within(0.005).of(7.33)
-          expect(calculator.price_for("little_kid_veg")).to eq 0
+          expect(calculator.price_for(types[0])).to be_within(0.005).of(9.78)
+          expect(calculator.price_for(types[1])).to be_within(0.005).of(7.33)
+          expect(calculator.price_for(types[2])).to be_zero
         end
       end
 
-      describe "max_ingredient_cost_for_full_price_of" do
+      describe "max_ingredient_cost_for_full_price_of_zzz" do
         it "should be correct for target price of 4.50" do
-          # 4.50 = (ingredient_cost / adult_equivs) * 1.1
-          # (4.50 / 1.1) * adult_equivs = ingredient_cost
-          expect(calculator.max_ingredient_cost_for_full_price_of(4.50)).to be_within(0.005).of(46.02)
-          expect(calculator.max_ingredient_cost_for_full_price_of(3.50)).to be_within(0.005).of(35.80)
+          # 4.50 = (ingredient_cost / full_price) * 1.1
+          # (4.50 / 1.1) * full_price = ingredient_cost
+          expect(calculator.max_ingredient_cost_for_full_price_of_zzz(4.50)).to be_within(0.005).of(46.02)
+          expect(calculator.max_ingredient_cost_for_full_price_of_zzz(3.50)).to be_within(0.005).of(35.80)
         end
       end
     end
@@ -76,12 +71,12 @@ describe Meals::ShareCostCalculator do
     before { meal.cost = build(:meal_cost, meal: meal, ingredient_cost: 0, pantry_cost: 15) }
 
     before do
-      allow(Meals::Signup).to receive(:totals_for_meal).and_return("adult_meat" => 0)
+      allow(meal).to receive(:signup_totals).and_return(types[0] => 0)
     end
 
     describe "price_for" do
       it "should be zero" do
-        expect(calculator.price_for("adult_meat")).to eq 0
+        expect(calculator.price_for(types[0])).to be_zero
       end
     end
   end

ba6085939ed7a626b38340e2790bdc0465a25366
Tom Smyth <tom@sassafras.coop>
2019-08-24 09:11:38 -0400

    Fix jobs people lens

--- app/lenses/people/user_lens.rb
+++ app/lenses/people/user_lens.rb
@@ -12,7 +12,7 @@ module People
         onchange: "this.form.submit();",
         "data-param-name": param_name,
         data: {
-          "select2-src": "users",
+          "select2-src": "/users",
           "select2-prompt": I18n.t("select2.prompts.user"),
           "select2-variable-width": "true",
           "select2-context": "lens"

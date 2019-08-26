77c8113ce27bd2962f13f96633e4145f773c7690
Tom Smyth <tom@sassafras.coop>
2019-08-26 07:53:53 -0400

    642: Fix cluster spec setup

--- spec/support/config/002_cluster_setup.rb
+++ spec/support/config/002_cluster_setup.rb
@@ -3,13 +3,17 @@
 RSpec.configure do |config|
   # We have to set a default tenant to avoid NoTenantSet errors.
   # But if the without_tenant flag is set to true, don't do this.
-  config.around(without_tenant: false) do |example|
-    with_tenant(Defaults.cluster) do
-      Defaults.community
+  config.around do |example|
+    if example.metadata[:without_tenant]
       example.run
-      # Note that if there is an error in a let! block, this line may never be reached. If errors result,
-      # focus on fixing the let! block.
-      Defaults.reset
+    else
+      with_tenant(Defaults.cluster) do
+        Defaults.community
+        example.run
+        # Note that if there is an error in a let! block, this line may never be reached. If errors result,
+        # focus on fixing the let! block.
+        Defaults.reset
+      end
     end
   end
 end

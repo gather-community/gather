903f1cdc15b9ccdb27881932dbe7d58a6211030f
Tom Smyth <tom@gather.coop>
2019-10-24 23:24:07 -0400

    416: Add header checking

--- app/models/meals/csv_importer.rb
+++ app/models/meals/csv_importer.rb
@@ -5,6 +5,8 @@ require "csv"
 module Meals
   # Imports meals from CSV.
   class CsvImporter
+    BASIC_HEADERS = %i[served_at resource_ids formula_id community_ids].freeze
+
     attr_accessor :file, :errors
 
     def initialize(file)
@@ -13,11 +15,34 @@ module Meals
     end
 
     def import
-      rows = CSV.new(file).read
+      rows = CSV.new(file, converters: ->(f) { f&.strip }).read
       if rows.empty?
         errors[0] = ["File is empty"]
         return
+      else
+        return unless parse_headers(rows[0])
       end
+    end
+
+    private
+
+    def parse_headers(row)
+      bad_headers = []
+      row.each do |cell|
+        next if header_dict[cell]
+        if (match_data = cell.match(/\A#{I18n.t("csv.headers.meal.role")}(\d+)\z/))
+          next if Role.find_by(id: match_data[1])
+        end
+        bad_headers << cell
+      end
+      return true if bad_headers.empty?
+      errors[1] = ["Invalid column headers: #{bad_headers.join(', ')}"]
+      false
+    end
+
+    # Returns a dictionary for looking up a header symbol based on the provided human-readable string.
+    def header_dict
+      @header_dict ||= BASIC_HEADERS.map { |h| [I18n.t("csv.headers.meal.#{h}"), h] }.to_h
     end
   end
 end
--- spec/models/meals/csv_importer_spec.rb
+++ spec/models/meals/csv_importer_spec.rb
@@ -3,13 +3,22 @@
 require "rails_helper"
 
 describe Meals::CsvImporter do
-  let(:importer) { described_class.new(file).tap(&:import) }
+  subject(:importer) { described_class.new(file).tap(&:import) }
 
   context "with empty file" do
     let(:file) { prepare_expectation("empty.csv") }
 
     it "returns error" do
       expect(importer.errors).to eq(0 => ["File is empty"])
+    end
+  end
+
+  context "with unrecognized headers including valid headers in other locale" do
+    let!(:roles) { create_list(:meal_role, 2) }
+    let(:file) { prepare_expectation("meals/import/bad_headers.csv", role_id: roles.map(&:id)) }
+
+    it "returns error listing all bad headers" do
+      expect(importer.errors).to eq(1 => ["Invalid column headers: Junk, Heure, Role999999999999"])
     end
   end
 end

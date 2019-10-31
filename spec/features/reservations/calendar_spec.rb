557af54e16c1375b56b137b220df055eedec4c8d
Tom Smyth <tom@gather.coop>
2019-10-31 09:00:18 -0400

    Fix time bug in calendar spec

--- spec/features/reservations/calendar_spec.rb
+++ spec/features/reservations/calendar_spec.rb
@@ -15,13 +15,12 @@ feature "reservation calendar", js: true do
     let!(:resource1) { create(:resource, name: "Foo Room") }
     let!(:resource2) { create(:resource, name: "Bar Room") }
     let(:time) { Time.current.midnight + 9.hours }
+    let(:time2) { (time + 2.weeks).at_beginning_of_month }
+    let(:time2_ymd) { time2.strftime("%Y-%m-%d") }
+    let(:time2_my) { time2.strftime("%B %Y") }
     let!(:meal) { create(:meal, :with_menu, title: "Yum", served_at: time + 9.hours, resources: [resource1]) }
     let!(:reservation) do
       create(:reservation, resource: resource1, starts_at: time, ends_at: time + 1.hour, name: "Funtimes")
-    end
-
-    around do |example|
-      Timecop.freeze("2019-10-20 12:00") { example.run }
     end
 
     before do
@@ -43,21 +42,21 @@ feature "reservation calendar", js: true do
       find(".fc-month-button").click
 
       # Test permalink and resource links update correctly.
-      permalink_url = "/reservations?resource_id=#{resource1.id}&view=month&date=2019-11-01"
+      permalink_url = "/reservations?resource_id=#{resource1.id}&view=month&date=#{time2_ymd}"
       expect(page).to have_css(%(a#permalink[href="#{permalink_url}"]))
-      other_resource_url = "/reservations?resource_id=#{resource2.id}&view=month&date=2019-11-01"
+      other_resource_url = "/reservations?resource_id=#{resource2.id}&view=month&date=#{time2_ymd}"
       expect(page).to have_css(%(a.resource-link[href="#{other_resource_url}"]), text: "Bar Room")
 
       # Test params respected on page load.
       click_link("Bar Room")
       expect(page).to have_echoed_url(other_resource_url)
       expect(page).to have_css(".fc-month-button.fc-state-active")
-      expect(page).to have_css(".fc-header-toolbar h2", text: "November 2019")
+      expect(page).to have_css(".fc-header-toolbar h2", text: time2_my)
 
       # Test storage of calendar params in localStorage
       visit(reservations_path(resource_id: resource2.id))
       expect(page).to have_css(".fc-month-button.fc-state-active")
-      expect(page).to have_css(".fc-header-toolbar h2", text: "November 2019")
+      expect(page).to have_css(".fc-header-toolbar h2", text: time2_my)
     end
   end
 end

49f84ef6def9eb8ac7e49fd0f38efcf715b68499
Tom Smyth <tom@gather.coop>
2019-11-03 07:11:11 -0500

    Fix frozen string bug

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -9,7 +9,7 @@ class UserDecorator < ApplicationDecorator
 
   def link(highlight: nil, show_cmty_if_foreign: false, show_inactive: false)
     name = full_name(show_inactive: show_inactive)
-    name << " (#{community_name})" if show_cmty_if_foreign && community != h.current_community
+    name = "#{name} (#{community_name})" if show_cmty_if_foreign && community != h.current_community
     name = h.content_tag(:mark, name) if id == highlight.to_i
     h.link_to(name, h.user_url(object), class: "user-link")
   end

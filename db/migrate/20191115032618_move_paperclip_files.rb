61e5656c34c12e13bd7a3a1c5918fb214b541921
Tom Smyth <tom@gather.coop>
2019-11-14 22:33:32 -0500

    691: Add migrations

---
+++ db/migrate/20191115032618_move_paperclip_files.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class MovePaperclipFiles < ActiveRecord::Migration[6.0]
+  def up
+    ActsAsTenant.without_tenant do
+      ActiveStorage::Attachment.find_each do |attachment|
+        name = attachment.name
+
+        source = attachment.record.send(name).path
+        dest_dir = File.join(
+          "storage",
+          attachment.blob.key.first(2),
+          attachment.blob.key.first(4).last(2))
+        dest = File.join(dest_dir, attachment.blob.key)
+
+        FileUtils.mkdir_p(dest_dir)
+        puts "Copying #{source} to #{dest}"
+        FileUtils.cp(source, dest)
+      end
+    end
+  end
+end

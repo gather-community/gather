65a5bce95b1b44ced55b016cc15d484afc8eccfc
Steve Gaarder <gaarder1@ecovillage.ithaca.ny.us>
2019-12-14 02:34:36 +0000

    clean up feature spec

--- spec/features/nav_menu_spec.rb
+++ spec/features/nav_menu_spec.rb
@@ -3,75 +3,44 @@
 require "rails_helper"
 
 feature "nav menu" do
-  let(:actor) { create(:admin) }
+  let(:community) { create(:community) }
+  let(:actor) { create(:user, community: community) }
 
   around { |ex| with_user_home_subdomain(actor) { ex.run } }
 
   before do
+    community.settings.main_nav_customizations = setting
+    community.save!
     login_as(actor, scope: :user)
   end
 
   context "basic" do
-    scenario "index" do
-      # no customizations
-      main_nav_test(setting: "",
-                    have: {"People" => "/users",
-                           "Meals" => "/meals",
-                           "Work" => "/work/signups",
-                           "Reservations" => "/reservations",
-                           "Wiki" => "/wiki"},
-                    not_have: [])
-      # change one link and add another
-      main_nav_test(setting: "[wiki](http://wikipedia.org) [google](http://google.com)",
-                    have: {"People" => "/users",
-                           "Meals" => "/meals",
-                           "Work" => "/work/signups",
-                           "Reservations" => "/reservations",
-                           "Wiki" => "http://wikipedia.org",
-                           "Google" => "http://google.com"},
-                    not_have: [])
-      # add two links
-      main_nav_test(setting: "[Yahoo](http://yahoo.com) [google](http://google.com)",
-                    have: {"People" => "/users",
-                           "Meals" => "/meals",
-                           "Work" => "/work/signups",
-                           "Reservations" => "/reservations",
-                           "Wiki" => "/wiki",
-                           "Yahoo" => "http://yahoo.com",
-                           "Google" => "http://google.com"},
-                    not_have: [])
-      # disable one link
-      main_nav_test(setting: "[wiki](none)",
-                    have: {"People" => "/users",
-                           "Meals" => "/meals",
-                           "Work" => "/work/signups",
-                           "Reservations" => "/reservations"},
-                    not_have: ["wiki"])
-      # no valid data 
-      main_nav_test(setting: "irrelevant gobbledygook",
-                    have: {"People" => "/users",
-                           "Meals" => "/meals",
-                           "Work" => "/work/signups",
-                           "Reservations" => "/reservations",
-                           "Wiki" => "/wiki"},
-                    not_have: [])
-      # complicated url
-      main_nav_test(setting: "[Somewhere out there](http://groucho:swordfish@somewhere.com?foo=bar&baz=frobozz+and+more%21)",
-                    have: {"People" => "/users",
-                           "Meals" => "/meals",
-                           "Work" => "/work/signups",
-                           "Reservations" => "/reservations",
-                           "Wiki" => "/wiki",
-                           "Somewhere out there" => "http://groucho:swordfish@somewhere.com?foo=bar&baz=frobozz+and+more%21"},
-                    not_have: [])
+    let(:setting) { "" }
+    scenario "no customizations" do
+      main_nav_test(match: [["People", "/users"],
+                            ["Meals", "/meals"],
+                            ["Work", "/work/signups"],
+                            ["Reservations", "/reservations"],
+                            ["Wiki", "/wiki"]])
+    end
+  end
+  context "changes" do
+    let(:setting) { "[Reservations](none)[Wiki](http://wikipedia.org) [google](http://google.com)" }
+    scenario "change one link, disable one, add another" do
+      main_nav_test(match: [["People", "/users"],
+                            ["Meals", "/meals"],
+                            ["Work", "/work/signups"],
+                            ["Wiki", "http://wikipedia.org"],
+                            ["Google", "http://google.com"]])
     end
   end
 
-  def main_nav_test(setting: "", have: {}, not_have: [])
-    visit("/admin/settings/community")
-    fill_in("Top Menu Customizations", with: setting)
-    click_button("Save")
-    have.each { |txt, linkto| expect(page).to have_link(txt, href: linkto) }
-    not_have.each { |txt| expect(page).not_to have_selector("td.main-nav ul.nav li", text: txt) }
+  def main_nav_test(match: [])
+    # go to a page that will have the main nav menu on it
+    visit("/users")
+    # Get all of the links in the main nav menu
+    # map them into an array of arrays
+    # and compare them with what they are supposed to be
+    expect(match).to eql(all("td.main-nav ul.nav li a").map { |element| [element.text, element["href"]] })
   end
 end

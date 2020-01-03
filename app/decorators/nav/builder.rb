2862f21f0c5beaab247a6cf3a14469f08adab941
Tom Smyth <tom@gather.coop>
2020-01-03 09:35:39 -0500

    66: Add groups feature flag

--- app/decorators/nav/builder.rb
+++ app/decorators/nav/builder.rb
@@ -118,7 +118,7 @@ module Nav
               name: :groups,
               parent: :people,
               path: h.groups_groups_path,
-              permitted: h.policy(sample_group).index?,
+              permitted: h.policy(sample_group).index? && !Settings.disable_groups,
               icon: "users"
             }, {
               name: :households,

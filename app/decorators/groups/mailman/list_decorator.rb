fa8d10633baffbbb1522417e0d82299e4f6d2170
Tom Smyth <tom@gather.coop>
2020-07-25 10:59:24 -0400

    Handle a lot of additional senders

--- app/decorators/groups/mailman/list_decorator.rb
+++ app/decorators/groups/mailman/list_decorator.rb
@@ -10,17 +10,22 @@ module Groups
       end
 
       def additional_senders_ul
-        membership_ul(additional_senders)
+        # We don't want to show all of these as there can be quite a few and it's not as sensitive
+        # as additional members.
+        membership_ul(additional_senders, max: 10)
       end
 
       private
 
-      def membership_ul(memberships)
+      def membership_ul(memberships, max: nil)
+        overflow = max.nil? ? 0 : memberships.size - max
+        memberships = memberships[0...max] if overflow > 0
         items = memberships.sort_by { |m| m.name_or_email.downcase }.map do |membership|
           text = membership.name_or_email
           text << "*" if membership.moderation_action == "hold"
           h.content_tag(:li, h.link_to(text, membership.email))
         end
+        items << h.content_tag(:li, "+#{overflow} more") if overflow > 0
         h.content_tag(:ul, h.safe_join(items), class: "no-bullets")
       end
     end
--- spec/system/groups/mailing_list_spec.rb
+++ spec/system/groups/mailing_list_spec.rb
@@ -22,32 +22,58 @@ describe "mailing lists", js: true do
 
   context "with domains" do
     let!(:domain) { create(:domain, name: "fluff.com") }
-    let(:api_response) do
-      {
-        entries: [
-          {email: "a@a.com", role: "nonmember", moderation_action: "hold", display_name: "A Tuzz"},
-          {email: "b@b.com", role: "nonmember", moderation_action: "accept", display_name: "Bo Fuzz"}
-        ]
-      }
+
+    context "with few add'l senders" do
+      let(:api_response) do
+        {
+          entries: [
+            {email: "a@a.com", role: "nonmember", moderation_action: "hold", display_name: "A Tuzz"},
+            {email: "b@b.com", role: "nonmember", moderation_action: "accept", display_name: "Bo Fuzz"}
+          ]
+        }
+      end
+
+      scenario "show, edit, delete list" do
+        with_env("STUB_MAILMAN" => api_response.to_json) do
+          visit(groups_group_path(group))
+          click_link("Edit")
+          fill_in("groups_group_mailman_list_attributes_name", with: "knitting")
+          expect(page).to have_content("List members are synchronized")
+          click_button("Save")
+
+          click_link("Knitting Club")
+          expect(page).to have_content("knitting@fluff.com")
+          expect(page).to have_content(/These additional senders.*A Tuzz\*\nBo Fuzz\n/m)
+          click_link("Edit")
+
+          check("Delete this list?")
+          accept_alert { click_button("Save") }
+          click_link("Knitting Club")
+          expect(page).to have_content("This group has no attached email list")
+        end
+      end
     end
 
-    scenario "show, edit, delete list" do
-      with_env("STUB_MAILMAN" => api_response.to_json) do
-        visit(groups_group_path(group))
-        click_link("Edit")
-        fill_in("groups_group_mailman_list_attributes_name", with: "knitting")
-        expect(page).to have_content("List members are synchronized")
-        click_button("Save")
+    context "with many add'l senders" do
+      let(:api_response) do
+        {
+          entries: 15.times.map do |i|
+            {email: "#{i}@a.com", role: "nonmember", moderation_action: "accept", display_name: "Lad #{i}"}
+          end
+        }
+      end
 
-        click_link("Knitting Club")
-        expect(page).to have_content("knitting@fluff.com")
-        expect(page).to have_content(/These additional senders.*A Tuzz\*\nBo Fuzz\n/m)
-        click_link("Edit")
-
-        check("Delete this list?")
-        accept_alert { click_button("Save") }
-        click_link("Knitting Club")
-        expect(page).to have_content("This group has no attached email list")
+      scenario "show, edit, delete list" do
+        with_env("STUB_MAILMAN" => api_response.to_json) do
+          visit(groups_group_path(group))
+          click_link("Edit")
+          fill_in("groups_group_mailman_list_attributes_name", with: "knitting")
+          click_button("Save")
+          click_link("Knitting Club")
+          expect(page).to have_content("knitting@fluff.com")
+          expect(page).to have_content(/These additional senders.*Lad 0\nLad 1\n/m)
+          expect(page).to have_content("+5 more")
+        end
       end
     end
   end

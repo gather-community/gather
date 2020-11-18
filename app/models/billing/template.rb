84a6cfa47f9f7cab5c0257927666faf8d1b2a4b0
Tom Smyth <tom@gather.coop>
2020-11-17 19:04:45 -0500

    782: Normalization and validation

--- app/models/billing/template.rb
+++ app/models/billing/template.rb
@@ -8,5 +8,11 @@ module Billing
     has_many :template_member_types, class_name: "Billing::TemplateMemberType",
                                      inverse_of: :template, dependent: :destroy
     has_many :member_types, class_name: "People::MemberType", through: :template_member_types
+
+    normalize_attributes :code, :description
+
+    validates :code, presence: true
+    validates :description, presence: true
+    validates :amount, presence: true, numericality: {greater_than: 0}
   end
 end

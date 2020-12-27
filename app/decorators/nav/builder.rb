379d954f490b9a8d424bf681d16a092288e6d73b
Tom Smyth <tom@gather.coop>
2020-12-27 16:44:46 -0500

    782: Add billing nav link for mobile

--- app/decorators/nav/builder.rb
+++ app/decorators/nav/builder.rb
@@ -11,16 +11,19 @@ module Nav
       self.context = {}
     end
 
-    def main_items
-      sample_household = Household.new(community: h.current_community)
+    def main_items(display:)
+      sample_household = Household.new(community: community)
       sample_user = User.new(household: sample_household)
-      sample_group = Groups::Group.new(communities: [h.current_community])
-      sample_period = Work::Period.new(community: h.current_community)
+      sample_meal = Meals::Meal.new(community: community)
+      sample_group = Groups::Group.new(communities: [community])
+      sample_period = Work::Period.new(community: community)
       sample_job = Work::Job.new(period: sample_period)
       sample_shift = Work::Shift.new(job: sample_job)
-      sample_resource = Reservations::Resource.new(community: h.current_community)
+      sample_resource = Reservations::Resource.new(community: community)
       sample_reservation = Reservations::Reservation.new(resource: sample_resource)
-      customizer = Utils::Nav::CustomizationParser.new(h.current_community&.settings&.main_nav_customizations)
+      sample_wiki_page = Wiki::Page.new(community: community)
+      sample_account = Billing::Account.new(community: community)
+      customizer = Utils::Nav::CustomizationParser.new(community&.settings&.main_nav_customizations)
 
       items = []
 
@@ -40,7 +43,7 @@ module Nav
       items << customizer.filter_item(
         name: :meals,
         path: lens_path_if_present("meals/meals", index_path: h.meals_path),
-        permitted: h.policy(Meals::Meal.new(community: h.current_community)).index?,
+        permitted: h.policy(sample_meal).index?,
         icon: "cutlery"
       )
       items << customizer.filter_item(
@@ -58,13 +61,20 @@ module Nav
       items << customizer.filter_item(
         name: :wiki,
         path: "/wiki",
-        permitted: h.policy(Wiki::Page.new(community: h.current_community)).show?,
+        permitted: h.policy(sample_wiki_page).show?,
         icon: "info-circle"
+      )
+      items << customizer.filter_item(
+        name: :billing,
+        path: h.accounts_path,
+        permitted: h.policy(sample_account).show?,
+        icon: "dollar",
+        display: :mobile
       )
 
       # Add all the new menu items defined as customizations
       items.concat(customizer.extra_items).compact!
-      filter_and_set_active_items(items, type: :main, active: context[:main])
+      filter_and_set_active_items(items, type: :main, display: display, active: context[:main])
     end
 
     def sub_items(main_item = nil)
@@ -72,7 +82,7 @@ module Nav
       items =
         case main_item
         when :meals
-          policy = h.policy(Meals::Meal.new(community: h.current_community))
+          policy = h.policy(Meals::Meal.new(community: community))
           [
             {
               name: :meals,
@@ -96,26 +106,26 @@ module Nav
               name: :formulas,
               parents: :meals,
               path: h.meals_formulas_path,
-              permitted: h.policy(Meals::Formula.new(community: h.current_community)).index?,
+              permitted: h.policy(Meals::Formula.new(community: community)).index?,
               icon: "calculator"
             }, {
               name: :types,
               parents: :meals,
               path: h.meals_types_path,
-              permitted: h.policy(Meals::Type.new(community: h.current_community)).index?,
+              permitted: h.policy(Meals::Type.new(community: community)).index?,
               icon: "cubes"
             }, {
               name: :roles,
               parents: :meals,
               path: h.meals_roles_path,
-              permitted: h.policy(Meals::Role.new(community: h.current_community)).index?,
+              permitted: h.policy(Meals::Role.new(community: community)).index?,
               icon: "user-circle-o"
             }
           ]
         when :people
-          sample_household = Household.new(community: h.current_community)
+          sample_household = Household.new(community: community)
           sample_user = User.new(household: sample_household)
-          sample_vehicle = People::Vehicle.new(household: Household.new(community: h.current_community))
+          sample_vehicle = People::Vehicle.new(household: Household.new(community: community))
           sample_memorial = People::Memorial.new(user: sample_user)
           [
             {
@@ -152,13 +162,13 @@ module Nav
               name: :settings,
               parents: :people,
               path: h.edit_people_settings_path,
-              permitted: People::SettingsPolicy.new(h.current_user, h.current_community).edit?,
+              permitted: People::SettingsPolicy.new(user, community).edit?,
               icon: "gear"
             }
           ]
         when :groups
           sample_user = User.new(household: sample_household)
-          sample_group = Groups::Group.new(communities: [h.current_community])
+          sample_group = Groups::Group.new(communities: [community])
           [
             {
               name: :groups,
@@ -181,24 +191,24 @@ module Nav
               parents: :reservations,
               path: h.reservations_path,
               permitted: h.policy(Reservations::Reservation.new(resource:
-                Reservations::Resource.new(community: h.current_community))).index?,
+                Reservations::Resource.new(community: community))).index?,
               icon: "calendar"
             }, {
               name: :resources,
               parents: :reservations,
               path: h.reservations_resources_path,
-              permitted: h.policy(Reservations::Resource.new(community: h.current_community)).index?,
+              permitted: h.policy(Reservations::Resource.new(community: community)).index?,
               icon: "bed"
             }, {
               name: :protocols,
               parents: :reservations,
               path: h.reservations_protocols_path,
-              permitted: h.policy(Reservations::Protocol.new(community: h.current_community)).index?,
+              permitted: h.policy(Reservations::Protocol.new(community: community)).index?,
               icon: "cogs"
             }
           ]
         when :work
-          sample_period = Work::Period.new(community: h.current_community)
+          sample_period = Work::Period.new(community: community)
           sample_job = Work::Job.new(period: sample_period)
           sample_shift = Work::Shift.new(job: sample_job)
           [
@@ -229,14 +239,14 @@ module Nav
             }
           ]
         when :billing
-          sample_account = Billing::Account.new(community: h.current_community)
+          sample_account = Billing::Account.new(community: community)
           [
             {
               name: :accounts,
               parents: :billing,
               path: h.accounts_path,
               permitted: h.policy(sample_account).index?,
-              icon: "dollar"
+              icon: "book"
             }, {
               name: :templates,
               parents: :billing,
@@ -255,7 +265,7 @@ module Nav
     # the hamburger menu.
     def sub_sub_items
       return @sub_sub_items if defined?(@sub_sub_items)
-      sample_member_type = People::MemberType.new(community: h.current_community)
+      sample_member_type = People::MemberType.new(community: community)
       items =
         case [context[:main], context[:sub_item]]
         when %i[people settings]
@@ -263,7 +273,7 @@ module Nav
             name: :general,
             parents: %i[people settings],
             path: h.edit_people_settings_path,
-            permitted: People::SettingsPolicy.new(h.current_user, h.current_community).edit?,
+            permitted: People::SettingsPolicy.new(user, community).edit?,
           },{
             name: :member_types,
             parents: %i[people settings],
@@ -275,13 +285,13 @@ module Nav
     end
 
     def personal_items
-      sample_export = Calendars::Exports::Export.new(user: h.current_user)
+      sample_export = Calendars::Exports::Export.new(user: user)
       items =
         [
           {
             name: :profile,
-            path: h.user_url(h.current_user),
-            permitted: h.policy(h.current_user).show?,
+            path: h.user_url(user),
+            permitted: h.policy(user).show?,
             icon: "vcard"
           }, {
             name: :accounts,
@@ -292,12 +302,12 @@ module Nav
           }, {
             name: :calendars,
             path: h.calendar_exports_path,
-            permitted: Calendars::ExportPolicy.new(h.current_user, sample_export).index?,
+            permitted: Calendars::ExportPolicy.new(user, sample_export).index?,
             icon: "calendar"
           }, {
             name: :change_passwd,
-            path: h.people_password_change_path(h.current_user),
-            permitted: UserPolicy.new(h.current_user, h.current_user).update?,
+            path: h.people_password_change_path(user),
+            permitted: UserPolicy.new(user, user).update?,
             icon: "asterisk"
           }, {
             name: :sign_out,
@@ -328,21 +338,31 @@ module Nav
     end
 
     def lens_path_if_present(controller, index_path: nil)
-      storage = Lens::Storage.new(session: h.session, community_id: h.current_community.id,
+      storage = Lens::Storage.new(session: h.session, community_id: community.id,
                                   controller_path: controller, action_name: "index")
       Lens::PathSaver.new(storage: storage).read || index_path || h.send("#{controller.tr('/', '_')}_path")
     end
 
     protected
 
-    def filter_and_set_active_items(items, type:, active: nil)
+    def filter_and_set_active_items(items, type:, display: nil, active: nil)
       return [] if items.blank?
-      items.select! { |i| i[:permitted] }
+      items.select! { |i| i[:permitted] && display.nil? || i[:display].nil? || i[:display] == display }
       items.each do |i|
         i[:type] = type
         i[:active] = true if active && i[:name] == active
       end
       items
+    end
+
+    private
+
+    def user
+      h.current_user
+    end
+
+    def community
+      h.current_community
     end
   end
 end

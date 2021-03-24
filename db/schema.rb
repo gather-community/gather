715f35bf4508bde14bbf11304ba10dddf3492bc0
Tom Smyth <tom@gather.coop>
2021-03-24 19:33:52 -0400

    821: Migrate activestorage attachment types

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_202824) do
+ActiveRecord::Schema.define(version: 2021_03_24_232811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

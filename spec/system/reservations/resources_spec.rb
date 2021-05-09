1f2904392705f505a5023d32588b0438657bd8c1
Tom Smyth <tom@gather.coop>
2021-05-09 13:25:39 -0400

    Fix label casing in spec

--- spec/system/reservations/resources_spec.rb
+++ spec/system/reservations/resources_spec.rb
@@ -42,7 +42,7 @@ describe "resources", js: true do
       expect_image_upload(state: :existing, path: /cooper/)
       fill_in("Name", with: "Foo Bar")
       fill_in("Abbreviation", with: "Bar")
-      select("Yes", from: "Can host meals?")
+      select("Yes", from: "Can Host Meals?")
       select("Month", from: "Calendar View")
       fill_in("Guidelines", with: "Don't do bad stuff")
       click_button("Save")

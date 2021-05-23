1e1e6e8e41f27762901ac3b5037f50029fbbc9bd
Tom Smyth <tom@gather.coop>
2021-05-23 09:28:54 -0400

    821: Fix select2 label attr for signup households

--- app/decorators/meals/signup_decorator.rb
+++ app/decorators/meals/signup_decorator.rb
@@ -26,7 +26,7 @@ module Meals
       else
         form.select(:household_id, household&.decorate&.selected_option_tag || "", {},
                     {class: "form-control", data: {"select2-src" => h.households_path,
-                                                   "select2-label-attr" => "name_with_prefix",
+                                                   "select2-label-attr" => "nameWithPrefix",
                                                    "select2-prompt" => t("select2.prompts.household"),
                                                    "select2-placeholder" => t("select2.placeholders.household"),
                                                    "select2-context" => "meal_form",

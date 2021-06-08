c6a9c94a9ac53054ff33dac8eb2e56efa5649a7d
Tom Smyth <tom@gather.coop>
2021-06-08 09:54:08 -0400

    821: Populate general calendars

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_013510) do
+ActiveRecord::Schema.define(version: 2021_06_08_132208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

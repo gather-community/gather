d3d1d0bf9cad72c7ee08bdd40175c0c476a78cfa
Tom Smyth <tom@gather.coop>
2021-07-07 09:24:19 -0400

    842: Set tenant in CloseMealsJob

--- app/jobs/meals/close_meals_job.rb
+++ app/jobs/meals/close_meals_job.rb
@@ -5,8 +5,17 @@ module Meals
   class CloseMealsJob < ApplicationJob
     def perform
       ActsAsTenant.without_tenant do
-        Meal.open.with_min_age(Settings.meals.close_cutoff_age.hours).find_each(&:close!)
-        Meal.open.with_past_auto_close_time.find_each(&:close!)
+        Meal.open.with_min_age(Settings.meals.close_cutoff_age.hours).find_each { |m| close_meal(m) }
+        Meal.open.with_past_auto_close_time.find_each { |m| close_meal(m) }
+      end
+    end
+
+    private
+
+    def close_meal(meal)
+      # We have to set the tenant in case associated records get created by listeners.
+      ActsAsTenant.with_tenant(meal.cluster) do
+        meal.close!
       end
     end
   end

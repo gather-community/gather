592c34493470ad8251a8d8c16c3fdf69100969f7
Tom Smyth <tom@gather.coop>
2021-07-16 15:53:37 -0400

    836: Add meal role reminders to jobs show page

--- app/decorators/meals/role_reminder_decorator.rb
+++ app/decorators/meals/role_reminder_decorator.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 module Meals
-  class RoleReminderDecorator < ApplicationDecorator
+  class RoleReminderDecorator < ReminderDecorator
     delegate_all
-
-    def rel_magnitude_to_i_or_f
-      return nil if rel_magnitude.blank?
-      to_int_if_no_fractional_part(rel_magnitude)
-    end
   end
 end

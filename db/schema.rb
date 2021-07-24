550c59909914374fca39685b879de9e2deb43917
Tom Smyth <tom@gather.coop>
2021-07-24 09:25:41 -0400

    836: Migration to seed role work hours

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_110608) do
+ActiveRecord::Schema.define(version: 2021_07_24_110223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

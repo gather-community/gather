c0c5815bc3261ffa931b4353506f56f1eb2cfa0d
Tom Smyth <tom@gather.coop>
2021-10-03 10:06:18 -0400

    858: Add migration to populate all day system calendars

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_164839) do
+ActiveRecord::Schema.define(version: 2021_10_02_132242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

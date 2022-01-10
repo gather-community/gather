0b792cf0a9bce360c021741690bb22dfd5d8ca2f
Tom Smyth <tom@gather.coop>
2022-01-10 15:53:43 -0500

    857: Clear out old content from export page

--- app/decorators/calendars/export_decorator.rb
+++ app/decorators/calendars/export_decorator.rb
@@ -8,30 +8,5 @@ module Calendars
       self.community = community
       self.user = user
     end
-
-    def calendar_link(type, personalized: true)
-      h.content_tag(:div) do
-        url = calendar_url(type, personalized)
-        h.link_to(h.icon_tag("calendar"), url) << " " <<
-          h.link_to(Exports::Factory.build(type: type, user: user).calendar_name, url) << h.tag(:br) <<
-          h.link_to("Copy Link", url, class: "copy", onclick: "copyTextToClipboard('#{url}'); return false")
-      end
-    end
-
-    private
-
-    def calendar_url(type, personalized)
-      if personalized
-        method = :calendars_personalized_exports_url
-        token = user.calendar_token
-      else
-        method = :calendars_community_exports_url
-        token = community.calendar_token
-      end
-
-      # If we don't set port to nil then it will be included in the
-      # webcal link which some clients don't like.
-      h.send(method, type.tr("_", "-"), calendar_token: token, format: :ics, protocol: :webcal, port: nil)
-    end
   end
 end

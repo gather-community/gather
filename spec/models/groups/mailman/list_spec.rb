f0b22b5409f2a1654771465365fec7bdfb59f474
Tom Smyth <tom@gather.coop>
2022-09-16 09:54:04 -0400

    941: Respect opt out when determining list memberships via mailman group

--- spec/models/groups/mailman/list_spec.rb
+++ spec/models/groups/mailman/list_spec.rb
@@ -27,8 +27,8 @@ describe Groups::Mailman::List do
     let!(:manager1) { create(:user, email: "l@l.com", first_name: "Lu", last_name: "Smith") }
     let!(:manager2) { create(:user, email: "m@m.com", first_name: "Mu", last_name: "Smith") }
     let!(:mm_user1) { create(:group_mailman_user, user: joiner1, remote_id: "xyz") }
-    let!(:group) { create(:group, availability: "open") }
-    let!(:decoy) { create(:user, email: "i@i.com", first_name: "Iu", last_name: "Smith") }
+    let!(:non_joiner) { create(:user, email: "i@i.com", first_name: "Iu", last_name: "Smith") }
+    let(:managers_can_admin_mod) { false }
     let!(:list) do
       build(:group_mailman_list, group: group,
                                  managers_can_administer: managers_can_admin_mod,
@@ -36,35 +36,18 @@ describe Groups::Mailman::List do
                                  remote_id: "foo.bar.com")
     end
 
-    before do
-      group.memberships.create!(group: group, user: joiner1, kind: "joiner")
-      group.memberships.create!(group: group, user: joiner2, kind: "joiner")
-      group.memberships.create!(group: group, user: manager1, kind: "manager")
-      group.memberships.create!(group: group, user: manager2, kind: "manager")
-    end
+    context "for regular group" do
+      let!(:group) { create(:group, availability: "open") }
 
-    context "when managers can't administer or moderate" do
-      let(:managers_can_admin_mod) { false }
-
-      it "returns correct list" do
-        expect(list.list_memberships.map(&:list_id).uniq).to eq(["foo.bar.com"])
-        expect(summaries(list.list_memberships)).to contain_exactly(
-          [false, mod1.id, nil, "e@e.com", "Eu Smith", "owner"],
-          [false, mod2.id, nil, "f@f.com", "Fu Smith", "owner"],
-          [false, mod1.id, nil, "e@e.com", "Eu Smith", "moderator"],
-          [false, mod2.id, nil, "f@f.com", "Fu Smith", "moderator"],
-          [false, owner1.id, nil, "g@g.com", "Gu Smith", "owner"],
-          [false, owner2.id, nil, "h@h.com", "Hu Smith", "owner"],
-          [true, joiner1.id, "xyz", "j@j.com", "Ju Smith", "member"],
-          [false, joiner2.id, nil, "k@k.com", "Ku Smith", "member"],
-          [false, manager1.id, nil, "l@l.com", "Lu Smith", "member"],
-          [false, manager2.id, nil, "m@m.com", "Mu Smith", "member"]
-        )
+      before do
+        # No opt out memberships since those aren't valid
+        group.memberships.create!(group: group, user: joiner1, kind: "joiner")
+        group.memberships.create!(group: group, user: joiner2, kind: "joiner")
+        group.memberships.create!(group: group, user: manager1, kind: "manager")
+        group.memberships.create!(group: group, user: manager2, kind: "manager")
       end
 
-      context "when managers can administer and moderate" do
-        let(:managers_can_admin_mod) { true }
-
+      context "when managers can't administer or moderate" do
         it "returns correct list" do
           expect(list.list_memberships.map(&:list_id).uniq).to eq(["foo.bar.com"])
           expect(summaries(list.list_memberships)).to contain_exactly(
@@ -77,13 +60,69 @@ describe Groups::Mailman::List do
             [true, joiner1.id, "xyz", "j@j.com", "Ju Smith", "member"],
             [false, joiner2.id, nil, "k@k.com", "Ku Smith", "member"],
             [false, manager1.id, nil, "l@l.com", "Lu Smith", "member"],
-            [false, manager1.id, nil, "l@l.com", "Lu Smith", "moderator"],
-            [false, manager1.id, nil, "l@l.com", "Lu Smith", "owner"],
-            [false, manager2.id, nil, "m@m.com", "Mu Smith", "member"],
-            [false, manager2.id, nil, "m@m.com", "Mu Smith", "moderator"],
-            [false, manager2.id, nil, "m@m.com", "Mu Smith", "owner"]
+            [false, manager2.id, nil, "m@m.com", "Mu Smith", "member"]
           )
         end
+
+        context "when managers can administer and moderate" do
+          let(:managers_can_admin_mod) { true }
+
+          it "returns correct list" do
+            expect(list.list_memberships.map(&:list_id).uniq).to eq(["foo.bar.com"])
+            expect(summaries(list.list_memberships)).to contain_exactly(
+              [false, mod1.id, nil, "e@e.com", "Eu Smith", "owner"],
+              [false, mod2.id, nil, "f@f.com", "Fu Smith", "owner"],
+              [false, mod1.id, nil, "e@e.com", "Eu Smith", "moderator"],
+              [false, mod2.id, nil, "f@f.com", "Fu Smith", "moderator"],
+              [false, owner1.id, nil, "g@g.com", "Gu Smith", "owner"],
+              [false, owner2.id, nil, "h@h.com", "Hu Smith", "owner"],
+              [true, joiner1.id, "xyz", "j@j.com", "Ju Smith", "member"],
+              [false, joiner2.id, nil, "k@k.com", "Ku Smith", "member"],
+              [false, manager1.id, nil, "l@l.com", "Lu Smith", "member"],
+              [false, manager1.id, nil, "l@l.com", "Lu Smith", "moderator"],
+              [false, manager1.id, nil, "l@l.com", "Lu Smith", "owner"],
+              [false, manager2.id, nil, "m@m.com", "Mu Smith", "member"],
+              [false, manager2.id, nil, "m@m.com", "Mu Smith", "moderator"],
+              [false, manager2.id, nil, "m@m.com", "Mu Smith", "owner"]
+            )
+          end
+        end
+      end
+    end
+
+    context "for everybody list" do
+      let!(:group) { create(:group, availability: "everybody") }
+      let!(:opt_outer) { create(:user, email: "o@o.com", first_name: "Ou", last_name: "Smith") }
+
+      before do
+        # No opt out memberships since those aren't valid
+        group.memberships.create!(group: group, user: joiner1, kind: "joiner")
+        group.memberships.create!(group: group, user: joiner2, kind: "joiner")
+        group.memberships.create!(group: group, user: manager1, kind: "manager")
+        group.memberships.create!(group: group, user: manager2, kind: "manager")
+        group.memberships.create!(group: group, user: opt_outer, kind: "opt_out")
+      end
+
+      it "respects opt-outs" do
+        expect(list.list_memberships.map(&:list_id).uniq).to eq(["foo.bar.com"])
+        expect(summaries(list.list_memberships)).to contain_exactly(
+          [false, mod1.id, nil, "e@e.com", "Eu Smith", "owner"],
+          [false, mod1.id, nil, "e@e.com", "Eu Smith", "moderator"],
+          [false, mod1.id, nil, "e@e.com", "Eu Smith", "member"], # Also a member b/c everybody group
+          [false, mod2.id, nil, "f@f.com", "Fu Smith", "owner"],
+          [false, mod2.id, nil, "f@f.com", "Fu Smith", "moderator"],
+          [false, mod2.id, nil, "f@f.com", "Fu Smith", "member"], # Also a member b/c everybody group
+          [false, owner1.id, nil, "g@g.com", "Gu Smith", "owner"],
+          [false, owner1.id, nil, "g@g.com", "Gu Smith", "member"], # Also a member b/c everybody group
+          [false, owner2.id, nil, "h@h.com", "Hu Smith", "owner"],
+          [false, owner2.id, nil, "h@h.com", "Hu Smith", "member"], # Also a member b/c everybody group
+          [true, joiner1.id, "xyz", "j@j.com", "Ju Smith", "member"],
+          [false, joiner2.id, nil, "k@k.com", "Ku Smith", "member"],
+          [false, manager1.id, nil, "l@l.com", "Lu Smith", "member"],
+          [false, manager2.id, nil, "m@m.com", "Mu Smith", "member"],
+          [false, ownerX.id, nil, "n@n.com", "Nu Smith", "member"],
+          [false, non_joiner.id, nil, "i@i.com", "Iu Smith", "member"]
+        )
       end
     end
 

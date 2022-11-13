42effbbebcbf113db9382e2fc80f57cdcf28ffe9
Tom Smyth <tom@gather.coop>
2022-11-13 12:18:38 -0500

    Sprockets 4

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,4 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .scss
+//= link_directory ../javascripts .js
+//= link_directory ../javascripts .json
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,4 @@
 /* eslint-disable */
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery2
 //= require jquery_ujs
 //= require bootstrap-sprockets
@@ -35,7 +23,7 @@
 //= require backbone/backbone
 //= require i18n
 //= require i18n/translations
-//= require serviceworker-companion
+//= require serviceworker/companion
 //= require_tree .
 /* eslint-enable */
 

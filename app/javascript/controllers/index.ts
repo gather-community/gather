76d9d05c99b62811f80429b158e7a8fcc2d824db
Tom Smyth <tom@gather.coop>
2023-02-11 22:25:40 -0500

    957: Rename to PaymentController

--- app/javascript/controllers/index.ts
+++ app/javascript/controllers/index.ts
@@ -7,5 +7,5 @@ import { application } from "./application"
 import Gdrive__RootPickerController from "./gdrive/root_picker_controller"
 application.register("gdrive--root-picker", Gdrive__RootPickerController)
 
-import SubscriptionController from "./subscription_controller"
-application.register("subscription", SubscriptionController)
+import PaymentController from "./payment_controller"
+application.register("payment", PaymentController)

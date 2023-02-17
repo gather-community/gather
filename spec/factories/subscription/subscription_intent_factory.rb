0a7e6db2b2b39078a89dda33179b5fd57398acb0
Tom Smyth <tom@gather.coop>
2023-02-16 21:33:11 -0500

    957: Fix subscription intent factory

--- spec/factories/subscription/subscription_intent_factory.rb
+++ spec/factories/subscription/subscription_intent_factory.rb
@@ -12,5 +12,7 @@ FactoryBot.define do
     address_line1 { "123 Little Pond Rd" }
     address_city { "Ann Arbor" }
     address_country { "US" }
+    payment_method_types { %w[us_bank_account card] }
+    tier { "standard" }
   end
 end

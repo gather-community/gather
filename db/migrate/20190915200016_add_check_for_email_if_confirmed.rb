80a5931b04f18a5c7faa8b6cec1d00c239278881
Tom Smyth <tom@gather.coop>
2023-03-04 21:52:43 -0500

    964: Add name to old check constraints

--- db/migrate/20190915200016_add_check_for_email_if_confirmed.rb
+++ db/migrate/20190915200016_add_check_for_email_if_confirmed.rb
@@ -2,6 +2,6 @@
 
 class AddCheckForEmailIfConfirmed < ActiveRecord::Migration[5.1]
   def change
-    add_check_constraint(:users, :unconfirmed_if_no_email, "email IS NOT NULL OR confirmed_at IS NULL")
+    add_check_constraint(:users, "email IS NOT NULL OR confirmed_at IS NULL", name: :unconfirmed_if_no_email)
   end
 end

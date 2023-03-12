0833809050605aadfab3a998d00c61802ab34d36
Tom Smyth <tom@gather.coop>
2023-03-12 13:39:36 -0400

    964: Tidy up missing column migration

--- db/migrate/20230309124919_change_gdrive_items_active_to_missing.rb
+++
@@ -1,6 +0,0 @@
-class ChangeGDriveItemsActiveToMissing < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :gdrive_items, :active, :missing
-    change_column_default :gdrive_items, :missing, from: true, to: false
-  end
-end

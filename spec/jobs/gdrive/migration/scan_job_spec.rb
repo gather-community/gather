1bf0a73cfe830a4a8212dbc4ee50c29acbdea5fc
Tom Smyth <tom@gather.coop>
2023-07-06 08:06:04 -0400

    965: Add test for unwritable file

--- spec/jobs/gdrive/migration/scan_job_spec.rb
+++ spec/jobs/gdrive/migration/scan_job_spec.rb
@@ -6,13 +6,12 @@ describe GDrive::Migration::ScanJob do
   include_context "jobs"
 
   let!(:main_config) { create(:gdrive_main_config, org_user_id: "drive.admin@gocoho.net") }
-  let!(:token) { create(:gdrive_token, gdrive_config: main_config, google_user_id: main_config.org_user_id, access_token: "ya29.a0AbVbY6NcC4BtSBJNPImEKwhMKJFpXKW_4jSbotxJ8E49GqWPLbXD2NyTqlwVbJP8pC3yWHE3UfhUIhbOFIMxkXmD_HFYyODpai1A8sEJs3ER8l3Te4MCUR-clHiaP6T054enWkv5zgNozx-wXsSQ0Ptv88LIaCgYKAeASARESFQFWKvPl14ksQIJ63x4y-QHtFV7Myw0163") }
+  let!(:token) { create(:gdrive_token, gdrive_config: main_config, google_user_id: main_config.org_user_id, access_token: "ya29.a0AbVbY6NnplfOPO7T1LtXlCVGjFVDA3M9fOekX8V2dm2IqvtgBM-ebCCVRFN0OFdNJ6kK3RQSfT186uICC7y26PR0kWgCwZfrDCzITw7D9JTQ-GFTYJUm8RBaz-ewv_nNktFmhAuA93J49-jINmcZq9hocl5XaCgYKAW8SARESFQFWKvPlSK2jbSqH5seNBkh6PO33ag0163") }
   let!(:migration_config) { create(:gdrive_migration_config) }
   let!(:operation) do
     create(:gdrive_migration_operation, config: migration_config, dest_folder_id: "0AEFsHNu6aSRGUk9PVA",
       filename_tag: "TS", src_folder_id: "1FBirfPXk-5qaMO1BkvlyhaC8JARE_FRq")
   end
-  let!(:scan_task) { operation.scan_tasks.create!(folder_id: operation.src_folder_id) }
   let!(:existing_file) do
     # This file already exists and shouldn't get re-tagged or cause unique key collisions.
     create(:gdrive_migration_file, operation: operation, name: "File Root.1",
@@ -23,6 +22,8 @@ describe GDrive::Migration::ScanJob do
   end
 
   describe "happy path" do
+    let!(:scan_task) { operation.scan_tasks.create!(folder_id: operation.src_folder_id) }
+
     before do
       stub_const("#{described_class.name}::PAGE_SIZE", 4)
     end
@@ -48,6 +49,25 @@ describe GDrive::Migration::ScanJob do
         .to contain_exactly("File Root.1", "File Root.2", "Test A", "Test B")
 
       expect(operation.reload.status).to eq("in_progress")
+    end
+  end
+
+  describe "when encounters unwritable file" do
+    let!(:scan_task) { operation.scan_tasks.create!(folder_id: "1nqlV0TWp5e78WCVmSuLdtQ2KYV2S8hsV") }
+
+    it "increments error count" do
+      VCR.use_cassette("gdrive/migration/scan_job/single_error") do
+        perform_job
+      end
+
+      expect { scan_task.reload }.to raise_error(ActiveRecord::RecordNotFound)
+
+      unwritable_file = GDrive::Migration::File.find_by(external_id: "1StDG48lKzbEnlmE5BboPBQCfhSlqxDrReceXiKes1mo")
+      expect(unwritable_file.status).to eq("error")
+      expect(unwritable_file.error_type).to eq("cant_edit")
+      expect(unwritable_file.error_message).to eq("drive.admin@gocoho.net did not have edit permission")
+
+      expect(operation.reload.error_count).to eq(1)
     end
   end
 end

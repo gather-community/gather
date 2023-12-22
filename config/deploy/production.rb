3cfff7775d38d1bc6b43d7a1c5e78716ecea9f5a
Tom Smyth <tom@gather.coop>
2023-12-22 09:46:08 -0500

    Update web server IPs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 set :deploy_to, "/home/deploy/gather"
-role :app, %w[deploy@134.122.120.173]
-role :web, %w[deploy@134.122.120.173]
-role :db,  %w[deploy@134.122.120.173]
+role :app, %w[deploy@143.244.212.80]
+role :web, %w[deploy@143.244.212.80]
+role :db, %w[deploy@143.244.212.80]
 set :branch, "master"
 set :rails_env, "production"
 set :linked_files, fetch(:linked_files, []).push(".rbenv-vars")

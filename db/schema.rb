62e708886fc7f233bb603eb486397ce72dfa6225
Tom Smyth <tom@gather.coop>
2024-01-21 10:45:40 -0500

    Remove api_key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_21_123438) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_21_125551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -265,7 +265,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_21_123438) do
   end
 
   create_table "gdrive_configs", force: :cascade do |t|
-    t.string "api_key", null: false
     t.string "client_id", null: false
     t.string "client_secret", null: false
     t.bigint "cluster_id", null: false

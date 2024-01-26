cf4ac9d294da89df0ddc982fe8152fe794b901ea
Tom Smyth <tom@gather.coop>
2024-01-26 08:24:50 -0500

    Handle auth errors on settings page

--- app/controllers/gdrive/settings_controller.rb
+++ app/controllers/gdrive/settings_controller.rb
@@ -7,6 +7,8 @@ module GDrive
     def show
       authorize(current_community, :setup?, policy_class: SetupPolicy)
       @config = MainConfig.find_by!(community: current_community)
+      return render_not_found unless @config
+
       @items_by_kind = {}
       @items_by_kind[:drive] = []
       @items_by_kind[:folder] = []
@@ -15,9 +17,22 @@ module GDrive
         @items_by_kind[item.kind.to_sym] << item
       end
 
-      config = MainConfig.find_by(community: current_community)
-      wrapper = Wrapper.new(config: config, google_user_id: config.org_user_id)
-      ItemSyncer.new(wrapper, @items_by_kind.values.flatten).sync
+      wrapper = Wrapper.new(config: @config, google_user_id: @config.org_user_id)
+      if wrapper.has_credentials?
+        ItemSyncer.new(wrapper, @items_by_kind.values.flatten).sync
+      else
+        set_auth_error
+      end
+    rescue Google::Apis::AuthorizationError
+      set_auth_error
+    end
+
+    private
+
+    def set_auth_error
+      @authorization_error = true
+      flash.now[:error] = "We encountered an error connecting to Google. The information below may be out of date. " \
+        "Please return to the main Google Drive page to re-establish a connection."
     end
   end
 end

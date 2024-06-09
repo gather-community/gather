311cc4b6ccf326cb098487535326a1a4a632ca42
Tom Smyth <tom@gather.coop>
2024-06-09 17:35:45 -0400

    Comment on deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,10 @@
 set :deploy_to, "/home/deploy/gather"
 role :app, %w[deploy@198.211.97.159 deploy@157.230.81.136]
 role :web, %w[deploy@198.211.97.159 deploy@157.230.81.136]
+
+# This is not the db server. It's just the server we use to run the migrations.
 role :db, %w[deploy@198.211.97.159]
+
 set :branch, "master"
 set :rails_env, "production"
 set :linked_files, fetch(:linked_files, []).push(".rbenv-vars")

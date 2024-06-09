38262b99b8bcc5322bee8b39312a9edef18682f2
Tom Smyth <tom@gather.coop>
2024-06-09 17:20:59 -0400

    Update prod deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 set :deploy_to, "/home/deploy/gather"
-role :app, %w[deploy@143.244.212.80]
-role :web, %w[deploy@143.244.212.80]
-role :db, %w[deploy@143.244.212.80]
+role :app, %w[deploy@198.211.97.159 deploy@157.230.81.136]
+role :web, %w[deploy@198.211.97.159 deploy@157.230.81.136]
+role :db, %w[deploy@198.211.97.159]
 set :branch, "master"
 set :rails_env, "production"
 set :linked_files, fetch(:linked_files, []).push(".rbenv-vars")

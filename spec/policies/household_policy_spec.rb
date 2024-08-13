d016aafedad21857f8d869ac21d36143813e00b2
Tom Smyth <tom@gather.coop>
2024-08-13 07:45:06 -0400

    Dont show inactive households to non-admins

--- spec/policies/household_policy_spec.rb
+++ spec/policies/household_policy_spec.rb
@@ -184,7 +184,7 @@ describe HouseholdPolicy do
 
     # We need to list all the users that will be used in the spec here, otherwise they will get
     # created too late and their households will show up in `permitted` but not in our expectation.
-    let!(:objs_in_community) { [user, other_user, admin, cluster_admin].map(&:household) }
+    let!(:objs_in_community) { [user, other_user, inactive_user, admin, cluster_admin].map(&:household) }
     let!(:objs_in_cluster) { [userB.household] }
     let!(:inactive_household) { inactive_user.household }
 
@@ -192,7 +192,7 @@ describe HouseholdPolicy do
       context "for regular users" do
         let(:actor) { user }
         it "returns all households except inactive ones" do
-          is_expected.to match_array(objs_in_community + objs_in_cluster)
+          is_expected.to match_array(objs_in_community + objs_in_cluster - [inactive_household])
         end
       end
 

31781892effa3c34db153f51130d6f6fab9cc335
Tom Smyth <tom@gather.coop>
2024-09-10 10:44:09 -0400

    Fix test

--- spec/system/people/auth/signed_in_password_change_spec.rb
+++ spec/system/people/auth/signed_in_password_change_spec.rb
@@ -13,14 +13,14 @@ describe "changing password when already signed in", js: true do
     full_sign_in_as(actor)
     visit(people_password_change_path)
 
-    click_on("Save")
+    click_on("Set Password")
     expect_validation_message(/can't be blank/)
 
     fill_in("New Password", with: "foo")
     expect_validation_message(/Too weak/)
     fill_in("Re-type New Password", with: "fo")
     expect_validation_message(/Doesn't match/)
-    click_on("Save")
+    click_on("Set Password")
 
     expect_validation_error(/was too weak/)
     expect_validation_error(/Didn't match/)
@@ -28,7 +28,7 @@ describe "changing password when already signed in", js: true do
     fill_in("New Password", with: "48hafeirafar42")
     expect_validation_message(/Good/)
     fill_in("Re-type New Password", with: "48hafeirafar42")
-    click_on("Save")
+    click_on("Set Password")
 
     expect_success
 

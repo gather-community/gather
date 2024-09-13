99508ee0343c4afb036fd5d558e79c36619d8fa0
Tom Smyth <tom@gather.coop>
2024-09-13 12:31:08 -0400

    Fix anniversary calendars timezone bug

--- app/models/calendars/system/user_anniversaries_calendar.rb
+++ app/models/calendars/system/user_anniversaries_calendar.rb
@@ -12,13 +12,21 @@ module Calendars
         lower = range.first
         upper = range.last
         users = User.in_community(community).active.where.not(attrib => nil)
+
+        # Try the anniversaries for each year
         events = (lower.year..upper.year).flat_map do |year|
           users.map do |user|
             month = user[attrib].month
             day = user[attrib].day
-            feb29 = !Date.leap?(year) && month == 2 && day == 29
-            candidate = feb29 ? Date.new(year, 2, 28) : Date.new(year, month, day)
-            range.first <= candidate && range.last >= candidate ? event_for(candidate, user) : nil
+
+            # If the anniversary is on feb 29 and it's not a leap year, use feb 28
+            day = 28 if !Date.leap?(year) && month == 2 && day == 29
+
+            candidate_day_start = Time.zone.local(year, month, day, 0, 0)
+            candidate_day_end = Time.zone.local(year, month, day, 11, 59)
+
+            # We match if the range overlaps _any part of_ the candidate day
+            range.first <= candidate_day_end && range.last >= candidate_day_start ? event_for(Date.new(year, month, day), user) : nil
           end
         end
         events.compact.sort_by(&:starts_at)

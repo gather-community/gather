3559de3788d5f1a831284dd8af5274d18a238bcc
Tom Smyth <tom@gather.coop>
2024-09-20 07:48:27 -0400

    Fix mail to inactive users (#1063)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -23,7 +23,7 @@ class ApplicationMailer < ActionMailer::Base
   #
   # If self.community returns a non-nil value, sets the appropriate subdomain.
   def mail(params)
-    include_inactive = params.delete(:include_inactive) || false
+    include_inactive = params.delete(:include_inactive)
     params[:to] = resolve_recipients(params[:to], include_inactive: include_inactive)
     return if params[:to].empty?
     raise "@community must be set or community method overridden to send mail" unless community
@@ -35,38 +35,53 @@ class ApplicationMailer < ActionMailer::Base
   private
 
   def resolve_recipients(recipients, include_inactive:)
-    Array.wrap(recipients).map do |recipient|
+    users_or_strings = Array.wrap(recipients).map do |recipient|
       if recipient.is_a?(User)
-        resolve_user_email(recipient, include_inactive: include_inactive)
+        resolve_user(recipient)
       elsif recipient.is_a?(Household)
         recipient.users.flat_map do |u|
-          resolve_user_email(u, via_household: true, include_inactive: include_inactive)
+          resolve_user(u, via_household: true)
         end
       elsif recipient.is_a?(String)
-        recipient
+        # Convert any empty strings to nil so they'll get removed.
+        recipient.presence
       else
         raise ArgumentError, "Invalid recipient type: #{recipient}"
       end
     end.flatten.uniq.compact
+
+    users_or_strings = if include_inactive == :always
+      users_or_strings
+    elsif include_inactive == :if_no_active
+      # If all results are inactive users, send to all. Else just send to active users and raw email addresses.
+      inactive_users, active_users_or_strings = users_or_strings.partition { |us| us.is_a?(User) && us.inactive? }
+      emails = if active_users_or_strings.empty?
+        inactive_users
+      else
+        active_users_or_strings
+      end
+    else
+      users_or_strings.reject { |us| us.is_a?(User) && us.inactive? }
+    end
+
+    users_or_strings.map { |us| us.is_a?(User) ? us.email : us }
   end
 
-  def resolve_user_email(user, include_inactive:, via_household: false)
-    # Don't send to fake users or unconfirmed full_access users.
-    # It's ok to send emails to unconfirmed non-full_access users
-    # because they can't log in, and they may still want to get emails.
-    # It would probably even be ok to send to unconfirmed full_access users but just to be safe we don't.
-    # Inactive users may have blank emails, but if they do, they can't be confirmed,
-    # so we exclude them anyway.
-    if user.fake? || (user.full_access? && !user.confirmed?) || (user.inactive? && !include_inactive)
-      nil
+  def resolve_user(user, via_household: false)
     # For children with no emails, we send to their guardians, except
     # we don't map emails to guardians if we're going via household because not all guardians
     # of children in a household live in that household. So sending an email addressed to household X
     # to guardians Y and Z of child C, even though Z doesn't live in household X, could be awkward.
-    elsif user.child? && user.email.blank? && !via_household
-      user.guardians.map(&:email)
+    users = if user.child? && user.email.blank? && !via_household
+      user.guardians
     else
-      user.email
+      [user]
     end
+
+    # Don't send to fake users or unconfirmed full_access users.
+    # It's ok to send emails to unconfirmed non-full_access users
+    # because they can't log in, and they may still want to get emails.
+    # It would probably even be ok to send to unconfirmed full_access users but just to be safe we don't.
+    users.reject { |u| u.email.blank? || u.fake? || (u.full_access? && !u.confirmed?) }
   end
 end
--- spec/mailers/general_mailer_spec.rb
+++ spec/mailers/general_mailer_spec.rb
@@ -3,19 +3,6 @@
 require "rails_helper"
 
 describe "general mailer" do
-  describe "mail to household with no emails" do
-    let(:statement) { create(:statement, total_due: 9.99) }
-    let(:mail) { AccountMailer.statement_notice(statement).deliver_now }
-
-    before do
-      statement.household.users.each { |u| u.email = "" }
-    end
-
-    it "doesn't send" do
-      expect(mail).to be_nil
-    end
-  end
-
   describe "mail to inactive user in active household" do
     let(:statement) { create(:statement, total_due: 9.99) }
     let(:user) { create(:user).tap(&:deactivate) }

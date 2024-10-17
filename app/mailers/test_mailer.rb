45a872e3a92657e1511e5a61b32338df7297730f
Tom Smyth <tom@gather.coop>
2024-10-16 23:09:36 -0400

    Add from address mail test (#1079)

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -2,6 +2,8 @@
 
 # Sends test emails that are then checked for by the system.
 class TestMailer < ActionMailer::Base
+  default from: Settings.email.from, reply_to: Settings.email.no_reply
+
   SUBJECT = "Gather Mail Reliability Test"
 
   def test_mail(counter:)

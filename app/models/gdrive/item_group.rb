f26a62510e0b3769049c002db45e53d157410546
Tom Smyth <tom@gather.coop>
2025-05-08 09:13:33 -0400

    Disallow organizer permission

--- app/models/gdrive/item_group.rb
+++ app/models/gdrive/item_group.rb
@@ -7,24 +7,21 @@ module GDrive
 
     acts_as_tenant :cluster
 
-    ACCESS_LEVELS = %i[reader commenter writer fileOrganizer organizer].freeze
+    # In the GDrive UI sharing dialog, there are several permission levels, and these:
+    # map to values in the GDrive API.
+    # - Viewer => reader
+    # - Commenter => commenter
+    # - Contributor (Add, edit, and share) => writer
+    # - Content manager (Add, edit, move, delete, and share) => fileOrganizer
+    # - Manager (Manage content, people, and settings) => organizer
+    #
+    # We want to allow admins to pick from any of these except "Manager", for any item.
+    ACCESS_LEVELS = %i[reader commenter writer fileOrganizer].freeze
 
     belongs_to :item, class_name: "GDrive::Item", inverse_of: :item_groups
     belongs_to :group, class_name: "Groups::Group", inverse_of: :gdrive_item_groups
 
     delegate :external_id, to: :item, prefix: true
     delegate :community, to: :item
-
-    def self.access_levels_for_kind(kind)
-      # We want to allow the "Add, edit, move, delete, and share" permission for all items
-      # but not the "Manage content, people, and settings" one.
-      # The former is "organizer" for folders/files and "fileOrganizer" for drives.
-      # The latter is "organizer" for drives.
-      if kind.to_sym == :drive
-        ACCESS_LEVELS - [:organizer]
-      else
-        ACCESS_LEVELS - [:fileOrganizer]
-      end
-    end
   end
 end

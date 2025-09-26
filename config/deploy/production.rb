70baaf9b75223c23d8ed1ae3d4dada3bbdee4732
Tom Smyth <tom@gather.coop>
2025-09-26 09:08:25 -0400

    Add new web node to production deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 set :deploy_to, "/home/deploy/gather"
 
-role :app, %w[deploy@10.136.121.104 deploy@10.136.121.106 deploy@10.136.121.112]
-role :web, %w[deploy@10.136.121.104 deploy@10.136.121.106 deploy@10.136.121.112]
+role :app, %w[deploy@10.136.121.104 deploy@10.136.121.106 deploy@10.136.121.112 10.136.121.115]
+role :web, %w[deploy@10.136.121.104 deploy@10.136.121.106 deploy@10.136.121.112 10.136.121.115]
 role :bg, %w[deploy@10.136.121.113]
 
 # This is not the db server. It's just the server we use to run the migrations.

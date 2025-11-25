fb0d32b8d73088f4e32e39a2974075cb4067e8d0
Jill Dimond <jill@sassafras.coop>
2025-11-25 16:14:27 -0500

    Removed check constraint and backfill

--- db/migrate/20251103224150_add_check_constraint_on_meals_signup_part.rb
+++
@@ -1,5 +0,0 @@
-class AddCheckConstraintOnMealsSignupPart < ActiveRecord::Migration[7.0]
-  def change
-    add_check_constraint :meal_signup_parts, "(user_id IS NULL) != (guest_id IS NULL)", name: "affirm_user_with_signup"
-  end
-end

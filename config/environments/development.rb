8f9be81108f9480a2b28dc335b4be72abaa0d04e
Nicholas Molen <60625073+robotastronaut@users.noreply.github.com>
2026-01-09 09:23:40 -0500

    Add Dev Container support and modernize development setup (#1134)
    * Initial devcontainer
    
    * First pass at a configuration script
    
    * Switch to using thor
    
    * gcloud config, host config, etc
    
    * Back to bash for more global usage.
    
    * Launch configs, ssl fixes, etc
    
    * Updated some configs, switched to bash+starship for simplicity
    
    * Fix compose template being gitignored
    
    * Refactored to simplify the whole process
    
    * Update README with new setup and config instructions
    
    * Remove mise init from devcontainer init script since it's being handled in the dockerfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,6 +21,10 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # Allow gatherdev.org for local development
+  config.hosts << "gatherdev.org"
+  config.hosts << ".gatherdev.org"
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp", "caching-dev.txt").exist?

81ef09221d6e0feefd3cf2f976e771d0c542766c
Jill Dimond <jill@sassafras.coop>
2026-04-20 15:29:06 -0400

    more schema fixes

--- db/schema.rb
+++ db/schema.rb
@@ -360,7 +360,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_07_130000) do
     t.index ["group_id"], name: "index_gdrive_item_groups_on_group_id"
     t.index ["item_id", "group_id"], name: "index_gdrive_item_groups_on_item_id_and_group_id", unique: true
     t.index ["item_id"], name: "index_gdrive_item_groups_on_item_id"
-    t.check_constraint "access_level::text = ANY (ARRAY['reader'::character varying, 'commenter'::character varying, 'writer'::character varying, 'fileOrganizer'::character varying]::text[])", name: "access_level_enum"
+    t.check_constraint "access_level::text = ANY (ARRAY['reader'::character varying::text, 'commenter'::character varying::text, 'writer'::character varying::text, 'fileOrganizer'::character varying::text])", name: "access_level_enum"
   end
 
   create_table "gdrive_items", force: :cascade do |t|
@@ -498,6 +498,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_07_130000) do
     t.datetime "updated_at", null: false
     t.index ["cluster_id"], name: "index_gdrive_migration_scans_on_cluster_id"
     t.index ["operation_id"], name: "index_gdrive_migration_scans_on_operation_id"
+    t.check_constraint "scope::text = ANY (ARRAY['full'::character varying::text, 'changes'::character varying::text, 'file_drop'::character varying::text])", name: "scope_enum"
     t.check_constraint "status::text = ANY (ARRAY['new'::character varying::text, 'in_progress'::character varying::text, 'cancelled'::character varying::text, 'complete'::character varying::text])", name: "status_enum"
   end
 

1f8462a80a45dedffe839a727e917d56dafc7f3b
Tom Smyth <tom@tomsmyth.ca>
2026-04-29 23:20:58 +0000

    Fix logger

--- lib/gather/structured_logger.rb
+++ lib/gather/structured_logger.rb
@@ -1,33 +1,29 @@
 # frozen_string_literal: true
 
+require "delegate"
+
 module Gather
-  # Wraps another logger class to allow passing structured data.
-  class StructuredLogger < ActiveSupport::Logger
-    attr_accessor :base_logger
-
-    def initialize(base_logger)
-      self.base_logger = base_logger
-      self.level = base_logger.level
-    end
-
+  # Wraps another logger to allow passing structured data as a second argument.
+  # Uses SimpleDelegator so Rails-internal methods like broadcast_to are preserved.
+  class StructuredLogger < SimpleDelegator
     def debug(message_or_progname = nil, data = nil, &block)
       message_or_progname = "#{message_or_progname} #{data.to_json}" unless data.nil?
-      base_logger.debug(message_or_progname, &block)
+      __getobj__.debug(message_or_progname, &block)
     end
 
     def info(message_or_progname = nil, data = nil, &block)
       message_or_progname = "#{message_or_progname} #{data.to_json}" unless data.nil?
-      base_logger.info(message_or_progname, &block)
+      __getobj__.info(message_or_progname, &block)
     end
 
     def warn(message_or_progname = nil, data = nil, &block)
       message_or_progname = "#{message_or_progname} #{data.to_json}" unless data.nil?
-      base_logger.warn(message_or_progname, &block)
+      __getobj__.warn(message_or_progname, &block)
     end
 
     def error(message_or_progname = nil, data = nil, &block)
       message_or_progname = "#{message_or_progname} #{data.to_json}" unless data.nil?
-      base_logger.error(message_or_progname, &block)
+      __getobj__.error(message_or_progname, &block)
     end
   end
 end
